e8ba6ff066f87e1ae2705d6b8533027cce244c5d
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 23:31:35 +0200

    Update Peruzeli_salingak.ts

--- data/Taner_Murat/Peruzeli_salingak.ts
+++ data/Taner_Murat/Peruzeli_salingak.ts
@@ -1,4 +1,2 @@
 export const peruzeliSalingakEntries: string = `
-# Peruzeli salıncaq — The turquoise swing
-# Bağçada bir salıncaq, peruzeden yapılğan. — A swing in the garden, made of turquoise.
 `;
