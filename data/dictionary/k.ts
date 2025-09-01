779f5833ec75a3288fd81b13be4c540b974c8396
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 14:30:28 +0200

    Update k.ts

--- data/dictionary/k.ts
+++ data/dictionary/k.ts
@@ -1,1 +1,3 @@
-export const rawDictionaryText = ``;
+export const rawDictionaryText = `
+Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
+`;
