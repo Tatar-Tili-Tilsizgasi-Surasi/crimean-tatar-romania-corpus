388c8b0efc1027f4608f9d028b1d77eb26fd3a4c
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 16:23:23 +0200

    Update Charles_Baudelaire.ts

--- data/Charles_Baudelaire.ts
+++ data/Charles_Baudelaire.ts
@@ -1,6 +1,6 @@
 export const charlesBaudelaireEntries: string = `
- Yamanlîgîñ şeşeklerí — Lesfleurs du mal
- Saylangan sonetler — Sonnets choisis
- Fransîzğa-kîrîm tatarğa ekítíllí baskî — Edition bilingue français-tatar criméen
- Kîrîm Tatarğasî Taner Murat'îñ kaytarmasîndan — Traduction en tatar criméen par Taner Murat
+# Yamanlîgîñ şeşeklerí — Lesfleurs du mal
+# Saylangan sonetler — Sonnets choisis
+# Fransîzğa-kîrîm tatarğa ekítíllí baskî — Edition bilingue français-tatar criméen
+# Kîrîm Tatarğasî Taner Murat'îñ kaytarmasîndan — Traduction en tatar criméen par Taner Murat
 `;
