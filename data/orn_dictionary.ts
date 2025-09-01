0e7975bf6fbfa84c2e433377359cdc4bf23a93ba
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 17:33:31 +0200

    Add files via upload

---
+++ data/orn_dictionary.ts
@@ -0,0 +1,26 @@
+// Ornithological Dictionary
+export const ornDictionaryRawText: string = `
+Aves
+•quş
+Passer
+domesticus
+•torğay
+•üy torğayî
+Corvus
+corone
+•qarğa
+•kara qarğa
+Columba
+livia
+•kökşin
+•kókşin
+Luscinia
+megarhynchos
+•búlbúl
+Aquila
+chrysaetos
+•kartal
+Bubo
+bubo
+•bayquş
+`;
