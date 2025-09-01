9829edf84d871d6b1e0ed89fc1eea13f00e05e67
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 23:30:56 +0200

    Update Ute_Karsun.ts

--- data/Ute_Karsun.ts
+++ data/Ute_Karsun.ts
@@ -1,4 +1,2 @@
 export const uteKarsunEntries: string = `
-# Kók júzúnde yulduzlar yiltiray. — Stars are shining in the sky.
-# Qış keldi, er yer appaq qar boldi. — Winter came, and everywhere turned white with snow.
 `;
