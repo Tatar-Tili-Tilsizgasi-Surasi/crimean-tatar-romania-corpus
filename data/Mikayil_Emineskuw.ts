624fa36e322a336b35f38b693551bd16354e6c9b
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 22:39:53 +0200

    Update Mikayil_Emineskuw.ts

--- data/Mikayil_Emineskuw.ts
+++ data/Mikayil_Emineskuw.ts
@@ -6,4 +6,18 @@ Gúzel heş bolmaganday. — A fost odată ca-n povești,
 A fost ca niciodată,
 Din rude mari împărătești,
 O prea frumoasă fată.
+# Tek kîz eken babasînda
+Herşiy men maktalganday,
+Erden, veliy arasînda,
+Yîldîzlarga da ayday. — Şi era una la părinţi
+Şi mândră-n toate cele,
+Cum e Fecioara între sfinţi
+Şi luna între stele.
+# Yúğe kubbe kólgesínde
+Kîz añîlîp pîsar,
+Penğíreníñ kóşesínde
+Şolpan beklep turar. — Din umbra falnicelor bolţi
+Ea pasul şi-l îndreaptă
+Lângă fereastră, unde-n colţ
+Luceafărul aşteaptă.
 `;
