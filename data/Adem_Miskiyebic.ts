2930cffa1b204b01fa4a8bce0a36d17cc97bacbe
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 23:21:56 +0200

    Update Adem_Miskiyebic.ts

--- data/Adem_Miskiyebic.ts
+++ data/Adem_Miskiyebic.ts
@@ -1,4 +1,5 @@
 export const ademMiskiyebicEntries: string = `
-# Litvaniya! Vetanım menim! sen sawlıq kibi bir şeysiñ. — Lithuania! My fatherland! you are like health.
-# Yurtqa bolğan sawgin, anadan miras. — Love for the homeland is inherited from the mother.
+# El-Kîrîm Sónetlerí — Sonnets from the Crimea
+# Adem Miskiyebiç — Adam Mickiewicz
+# Ingilizğe terğumesi Edna Wortliy Underwut — translated into English by Edna Worthley Underwood
 `;
