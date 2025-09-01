1ee811c7989d555474dffa609e65ea8b337c30a1
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 12:03:33 +0200

    Update Taner_Murat.ts

--- data/Taner_Murat.ts
+++ data/Taner_Murat.ts
@@ -9,4 +9,55 @@ Men şanak uzatmam, karamam merhamet,
 Topraklar bízímkí, hakkîmîz ğet-beğet,
 Fiat Justitia, pereat mundus, et,
 Ya batsîn dúniya, ya bolsîn adalet!`,
+  `Bayrak kúní
+Yígírím-altîsî búgún, haziran,
+Minare-minare sús taktî, Bayrak.
+Semalar ap-aydîn, mawusî berrak,
+Kîrîm'dan dúnyaga okîla ezan.
+
+Ğaş kîzlar kózínde ğîltîray felek,
+Kîrîm'îñ-Kîrîm'îñ hawasî sîğak.
+Bír bala sallanîp ğelpíldiy Bayrak,
+Sîradan kún tuwul, búgún múbarek.
+
+Kórmedím bonday tañ, bonday bír şafak,
+Tatarlar-Tatarlar dórt ğónden aga.
+Búgúnní yaşattîñ, temenna saga,
+Kartlarîñ-ğaşlarîñ kolînda, Bayrak!
+
+Biñ yaşa Bayragîm, ela zenger hakk,
+Som altîn-som altîn tamgalî mañlay!
+Káyinat rengínden ayîrgan Kuday,
+Tom, dokîz bírleşken milletke Bayrak.
+(Kóstenğe, 26 haziran 2011)`,
+  `Ay ğîlar
+Neday rutubet
+Kurganîñ ústí, baárde.
+Ay ğîlar, ebet.`,
+  `Batîrîmîz Bekmambet yúz yaşînda
+Alí Osman kartbabay, batîrîmîz Bekmambet,
+Niyaziy kayday edí? - bízge de añlat, úyret.
+Bíz de sendiy yaşlanîp yúzúmúzge kelgende
+Sóz etermíz Alí'ní keleğek nesíllerge.`,
+  `Bar mî sízde?
+Bastîlar ğurt, tandîr, oşak,
+Ğaktîlar úy, yuwa, ğatak,
+Bízden ístep altîn-ğuman.
+Bar mî sízge ottay dúşman?
+
+Asîp bízní azbar-azbar
+Ğîktîlar kór, kabír, mezar.
+Bízge dogrî hukuk kerek,
+Bar mî sízde onday ğúrek?
+
+Bízní toplap haywan gibí
+Súrgún attîlar perşembí.
+Bo soy kurutmasî tuwul…
+Bar mî sízde tora makul?
+
+Bízden kîrslap mutabakat
+Yazdîlar yalanğî lugat.
+Bo mî eren, insaniyet?
+Bar mî sízde medeniyet?
+(10 ewlúl 2011, Kóstenğe)`,
 ];
