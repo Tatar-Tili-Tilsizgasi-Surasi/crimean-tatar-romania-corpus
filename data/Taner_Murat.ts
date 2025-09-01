925caf0aae94b1f923a9cd4034b63806e38e6336
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 11:47:44 +0200

    Update Taner_Murat.ts

--- data/Taner_Murat.ts
+++ data/Taner_Murat.ts
@@ -1,13 +1,4 @@
 export const tanerMuratEntries: string[] = [
-  'Fiat Justitia
-Fiat Justitia, pereat mundus, et!
-Ya batsîn dúniya, ya bolsîn adalet,
-Ya pítsín hakksîzlîk, ya kelsín felaket,
-Fiat Justitia, pereat mundus, et!
-
-Men şanak uzatmam, karamam merhamet,
-Topraklar bízímkí, hakkîmîz ğet-beğet,
-Fiat Justitia, pereat mundus, et,
-Ya batsîn dúniya, ya bolsîn adalet!',
+  'Fiat Justitia Fiat Justitia, pereat mundus, et! Ya batsîn dúniya, ya bolsîn adalet, Ya pítsín hakksîzlîk, ya kelsín felaket, Fiat Justitia, pereat mundus, et! Men şanak uzatmam, karamam merhamet, Topraklar bízímkí, hakkîmîz ğet-beğet, Fiat Justitia, pereat mundus, et, Ya batsîn dúniya, ya bolsîn adalet!',
   
 ];
