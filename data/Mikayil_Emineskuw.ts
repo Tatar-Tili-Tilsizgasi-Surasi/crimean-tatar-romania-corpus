dbc89a4c2fb9aeba0d7e932d8a3805d53a2c0d5d
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 22:37:25 +0200

    Update Mikayil_Emineskuw.ts

--- data/Mikayil_Emineskuw.ts
+++ data/Mikayil_Emineskuw.ts
@@ -2,7 +2,7 @@ export const mikayilEmineskuwEntries: string = `
 # Bír bar eken dep aytîla
 Kópten ótken masalday,
 Kamuk kîzî, ónder soyda,
-Gúzel heş bolmaganday. / A fost odată ca-n povești,
+Gúzel heş bolmaganday. — A fost odată ca-n povești,
 A fost ca niciodată,
 Din rude mari împărătești,
 O prea frumoasă fată.
