02d2ef006a4c2ead0cd42e41f45b7a35add25ca1
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 17:48:17 +0200

    Update bot_dictionary.ts

--- data/bot_dictionary.ts
+++ data/bot_dictionary.ts
@@ -1,72 +1,123 @@
 // Botanical Dictionary
 export const botDictionaryRawText: string = `
-Abe lmoschus
-esculentus
-•bamiye
-•kelínparmagî
-Ab ies
-alba
-•ak-narus
-•ak-ládin
-•ak-şîrşî
-Abies
-cephalonica
-•Rum-narusî
-•Rum-ládiní
-•Rum-şîrşîsî
-Abies
-concolor
-•gúmúş-narus
-•gúmúş-ládin
-•gúmúş-şîrşî
-Abies
-firma
-•Japon-narusî
-•Japon-ládiní
-•Japon-şîrşîsî
-Abies
-grandis
-•úyken-narus
-•úyken-ládin
-•úyken-şîrşî
-Abies
-lasiocarpa
-•dagaşasî-narusî
-•dagaşasî-ládiní
-•dagaşasî-şîrşîsî
-Abies
-magnifica
-•kîrmîzî-narus
-•kîrmîzî-ládin
-•kîrmîzî-şîrşî
-Abies
-nordmanniana
-•Kawkaz-narusî
-•Kawkaz-ládiní
-•Kawkaz-şîrşîsî
-Abies
-procera
-•soylî-narus
-•soylî-ládin
-•soylî-şîrşî
-Abies
-sibirica
-•Sebír-narusî
-•Sebír-ládiní
-•Sebír-şîrşîsî
-Abutilon
-theophrasti
-•tereksîrgasî
-Acacia
-•akasiya
-Acanthopanax
-senticosus
-•tayga
-Acanthus
-longifolius
-•kenger
-Acer
-campestre
-•kîr-úreñgesí
+Abe lmoschus 
+escu lentus 
+•bamiye 
+•kelínparmagî 
+Ab ies a lba 
+•ak-narus 
+•ak-ládin 
+•ak-şîrşî 
+Ab ies cepha lon ica 
+•Rum-narusî 
+•Rum-ládiní 
+•Rum-şîrşîsî 
+Ab ies conco lo r 
+•gúmúş-narus 
+•gúmúş-ládin 
+•gúmúş-şîrşî 
+Ab ies f irm a 
+•Japon-narusî 
+•Japon-ládiní 
+•Japon-şîrşîsî 
+Ab ies grand is 
+•úyken-narus 
+•úyken-ládin 
+•úyken-şîrşî 
+Ab ies las iocarpa 
+•dagaşasî-narusî 
+•dagaşasî-ládiní 
+•dagaşasî-şîrşîsî 
+Ab ies magn if i ca 
+•kîrmîzî-narus 
+•kîrmîzî-ládin 
+•kîrmîzî-şîrşî 
+Ab ies nordmann iana 
+•Kawkaz-narusî 
+•Kawkaz-ládiní 
+•Kawkaz-şîrşîsî 
+Ab ies procera 
+•soylî-narus 
+•soylî-ládin 
+•soylî-şîrşî 
+Ab ies s ib i rica 
+•Sebír-narusî 
+•Sebír-ládiní 
+•Sebír-şîrşîsî 
+Abuti lon theophrasti 
+•tereksîrgasî 
+Acaci a 
+•akasiya 
+Acanthopanax 
+senticosus 
+•tayga 
+Acanthus longifo lius 
+•kenger 
+Acer campestre 
+•kîr-úreñgesí 
 •kîr-akşateregí
+Acer mon spessu lanum
+•Frengístan-úreñgesí 
+•Frengístan-akşateregí 
+Acer negundo 
+•Amerika-akşateregí 
+Acer p latano ides 
+•akşaterek 
+Acer pseudop latanus 
+•yalanğî-şarîn 
+•isfender 
+Acer s acchar inum
+•gúmúş-akşaterek 
+Acer s accharum
+•şeker-akşateregí 
+Acer tatari cum
+•akşa-kayîñ 
+•úreñge 
+Aceras 
+anthropophorum
+•insan-salebí 
+Acetosa praten s is 
+•ekşí-atkulak 
+Ach i l lea m i l lefo lium
+•ğiwanperşemí 
+•biñbíryaprak 
+Ach i l lea ptarm ica 
+•hoğaakayhapşîrdî 
+Achusa of f icina lis 
+•sîyîrtílí 
+Acon itum anthora 
+•sarî-buwmaotî 
+•sarî-kaşkîrbuwgan 
+Acon itum f irmum
+•kattî-buwmaotî 
+•kattî-kaşkîrbuwgan 
+Acon itum nape l lus 
+•mawî-buwmaotî 
+•mawî-kaşkîrbuwgan 
+Acon itum orienta le 
+•Kawkaz-buwmaotî 
+•Kawkaz-kaşkîrbuwgan 
+Acon itum sp . 
+•buwmaotî 
+•kaşkîrbuwgan 
+Acon itum tox icum
+•kók-buwmaotî 
+•kók-kaşkîrbuwgan 
+Acon itum vari egatum
+•alaşa-buwmaotî 
+•alaşa-kaşkîrbuwgan 
+Acon itum vu lparia 
+•túlkí-buwmaotî 
+•túlkí-kaşkîrbuwganî 
+Acorus ca lamus 
+•egír 
+Actaea sp icata 
+•ğîlan-şelegí 
+Actin id ia ch inen s is 
+•kiwiy 
+Adenophora li li ifo lia 
+•kelín-şañşeşegí 
+Ad iantum cap i l lus -
+veneris 
+•baldîrîkara
 `;
