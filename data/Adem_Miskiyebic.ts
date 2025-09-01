913e09157215c7644af4c634ba78821af8fdbf45
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-01 23:09:07 +0200

    Add files via upload

---
+++ data/Adem_Miskiyebic.ts
@@ -0,0 +1,4 @@
+export const ademMiskiyebicEntries: string = `
+# Litvaniya! Vetanım menim! sen sawlıq kibi bir şeysiñ. — Lithuania! My fatherland! you are like health.
+# Yurtqa bolğan sawgin, anadan miras. — Love for the homeland is inherited from the mother.
+`;
---
+++ data/Geriy_Bek.ts
@@ -0,0 +1,4 @@
+export const geriyBekEntries: string = `
+# Tarih kitapları, kelecekke bir ders. — History books are a lesson for the future.
+# Tınç bir keşe, ay ışıgı astında. — A quiet night, under the moonlight.
+`;
