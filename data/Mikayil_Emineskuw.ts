ed5d457145d1078803130f6b268f1463140f684f
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 22:47:25 +0200

    Update Mikayil_Emineskuw.ts

--- data/Mikayil_Emineskuw.ts
+++ data/Mikayil_Emineskuw.ts
@@ -105,4 +105,102 @@ Eşsíz ğígít şîga. — Şi apa unde-au fost căzut
 În cercuri se roteşte,
 Şi din adânc necunoscut
 Un mândru tânăr creşte.
+# Yawaş tayîp penğíreden
+Úyge basa ayak,
+Oñ kolînda kertíklengen,
+Kamîş, tajlî tayak. — Uşor el trece ca pe prag
+Pe marginea ferestei
+Şi ţine-n mână un toiag
+Încununat cu trestii.
+# Altîn şáşí sarî maşak,
+Aksúyeklí bír ğaş,
+Aşîk omîzînda ğîmşak,
+Mor túymelí kumaş. — Părea un tânăr voevod
+Cu păr de aur moale,
+Un vânăt giulgi se-ncheie nod
+Pe umerele goale.
+# Ak kóleke, saydam yúzí
+Mayşîrakka tarta,
+Tîşka şagîm atar kózí,
+Kayet yakşî mewta. — Iar umbra feţei străvezii
+E albă ca de ceară -
+Un mort frumos cu ochii vii
+Ce scânteie-n afară.
+# – „Zorlîk şegíp keldím sessíz,
+Tatlî sózíñdiy yoktîr, 
+Mením anam mawî deñíz,
+Mením babam da kóktír. — – „Din sfera mea venii cu greu
+Ca să-ţi urmez chemarea,
+Iar cerul este tatăl meu
+Şi mumă-mea e marea.
+# Karap toyarman, dep keldím,
+Kórsem bek yakîndan,
+Dúniyamdan kobîp keldím,
+Tuwîp deren suwdan. — Ca în cămara ta să vin,
+Să te privesc de-aproape,
+Am coborât cu-al meu senin
+Şi m-am născut din ape.
+# Ay, kel, insanlîknî taşla
+Kel kîymetlím, sayarman,
+Kel şolpanga, kókte yaşa,
+Sení kelín yaparman. — O, vin'! odorul meu nespus,
+Şi lumea ta o lasă;
+Eu sunt luceafărul de sus,
+Iar tu să-mi fii mireasă.
+# Onda merğan saraylarda
+Men men soñsîz yaşa,
+Herkez geñíş ğihanlarda
+Seğdelensín saga.” — Colo-n palate de mărgean
+Te-oi duce veacuri multe,
+Şi toată lumea-n ocean
+De tine o s-asculte."
+# – „Ay, sen melektiy túşíme
+Uşîp-kelíp, yakşîsîñ!
+Lákin basmam ízlegíñe
+Ke sen başka túrlísíñ. — – „O, eşti frumos, cum numa-n vis
+Un înger se arată,
+Dară pe calea ce-ai deschis
+N-oi merge niciodată;
+# Kîyafetíñ-sózíñ tezat,
+Ğîltîragan mezarsîñ,
+Ke men ğanlî, sen mewta ğat,
+Toñdîruwğî nazarsîñ.” — Străin la vorbă şi la port,
+Luceşti fără de viaţă,
+Căci eu sunt vie, tu eşti mort,
+Şi ochiul tău mă-ngheaţă."
+# Bír kún geşíp úş kún kaştî,
+Keşe kelíp kayta,
+Soñra kókte bírtaa aştî
+Şolpan nurîn kîzga. — Trecu o zi, trecură trei
+Şi iarăşi, noaptea, vine
+Luceafărul deasupra ei
+Cu razele-i senine.
+# Kóz ğumganda yukîsîna
+Yîldîz kele-bere,
+Dalgalarîñ mîrzayîna
+Kîz hasretlík şege. — Ea trebui de el în somn
+Aminte să-şi aducă
+Şi dor de-al valurilor domn
+De inim-o apucă.
+# – „Túş yaratîp, ğuwaş şolpan,
+Hayatîma ziya,
+Kír úyíme, geş oyîmdan,
+Nurdan taya-taya.” — – „Cobori în jos, luceafăr blând,
+Alunecând pe-o rază,
+Pătrunde-n casă şi în gând
+Şi viaţa-mi luminează!"
+# Ağîlanîp kókte şolpan
+Yawaş-yawaş sóníp,
+Bírden koba bír deweran,
+Sóngen yerín alîp. — Cum el din cer o auzi,
+Se stinse cu durere,
+Iar ceru-ncepe a roti
+În locul unde piere;
+# Ağunlarnîñ hawasînda
+Kîzîl yalaz oynay,
+Nizamsîzlîk ğarlarînda
+Tuwar yakşî şîray. — În aer rumene văpăi
+Se-ntind pe lumea-ntreagă,
+Şi din a chaosului văi
+Un mândru chip se-ncheagă;
 `;
