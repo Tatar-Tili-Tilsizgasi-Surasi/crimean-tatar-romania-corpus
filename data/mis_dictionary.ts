4f84af1200a8ba0fbe5f63f4afddd71b308ca2b2
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 12:42:01 +0200

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -2,4 +2,5 @@ export const misDictionaryRawText: string = `
 Ğadîlar Bayramî s. Halloween
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
 Şînğa s. (topon.) Xinjiang, China.
+Virjiniye s. (topon.) Virginia
 `;
