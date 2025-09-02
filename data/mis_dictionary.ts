f0172ab663029e41fb3b3bbe2e6c73b49f8d0a91
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 13:14:22 +0200

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -1,17 +1,7 @@
 export const misDictionaryRawText: string = `
-Awustin s. (topon.) Austin, Texas.
-Çikago s. (topon.) Chicago.
 Ğadîlar Bayramî s. Halloween.
-Ğorj s. (antrop. m.) George.
-Henriy s. (antrop. m.) Henry.
 Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
-Minísk s. (topon.) Minsk.
-Moskaw s. (topon.) Moscow.
-Petersburk s. (topon.) Petersburg.
 Şînğa s. (topon.) Xinjiang, China.
-Teksas s. (topon.) Texas.
-Vilne s. (topon.) Vilnius.
 Virjiniye s. (topon.) Virginia.
-Warşaw s. (topon.) Warsaw.
 `;
