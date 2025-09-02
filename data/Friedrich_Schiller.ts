fd6f26b7931b81c5c13984db6a6816db9cc11368
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:52:54 +0200

    Update Friedrich_Schiller.ts

--- data/Friedrich_Schiller.ts
+++ data/Friedrich_Schiller.ts
@@ -1,5 +1,289 @@
 export const friedrichSchillerEntries: string = `
-# Kaside kunakka — An die Freude
-# Kîrîm Tatarğasî
-Taner Murat'în terğúmesínde
+# Kaside kunakka
+Kunak, hoş Tañrî şagîmî,
+Kók kîzî gúlistanda,
+Bíz kíremíz álewleníp
+Semaviy mehrabîña.
+Tîlsîmîñ bírleştíredír
+Kuy kîlîşî ayîrsa,
+Tílenşí mîrza man kardaş
+Kanatîñ ğuwaş kaksa.
+
+Kor
+
+Kuşaklaşsîn miliyonlar!
+Bo óbúw dúniyaga!
+Kardaşlar – súygen ata
+Yîldîz án-yagînda yaşar.
+
+Úyken-geñíş kîsmetlíler
+Bolîp dost arkadaşka,
+Súygen, sadîk biykelíler,
+Koşîlîr kuwanîşka!
+Ya – kímíñ tek ğanî bolsa
+Tek ózíñ káyinatta!
+Beğermegen ğîlasa da
+Yerí yok ortaklîkta.
+
+Kor
+
+Bo keñ şemberdekíler
+Húrmet eter hoşlîkka!
+Aşîp ğol yîldîzlîkka
+Şo evrende ógíysírer.
+
+Kunak íşer bútún ğanlî
+Tuwa kókíregínde,
+Yakşîsî da, yamanî da,
+Gúllí ğollardan kete.
+O óbúw berdí, şarap ta,
+Ólímgeşík dostman der,
+Hewesní berdí kurtka da,
+Tañrîga tík melekler.
+
+Kor
+
+Ğúgúndúñ mí, miliyonlîk?
+Álem, sezdíñ mí, Kuday?
+O yîldîz ústí yaşay
+Kubbe ústúnden karañîz.
+
+Kunak sebebíñ atîdîr
+Ebediy tuwalarda.
+Kunak, kunak, onîñ kúşí
+Úyken sáátní kaytara.
+O şîgara şeşek kózden
+Kúneş-kuyaş kubbede,
+O kúreler aylandîra
+Dúrbún barmaz yerlerde.
+
+Kor
+
+Kuwanîp, şemsler uşkanday
+Kógíñ ela yúzúnde
+Şabuw şabîñîz, kardaş,
+Tîpkî batîrday zaferde.
+
+Hakkîñ ateş aynasîndan
+Kúlúmsúriy aydînga.
+Faziletíñ sarplîgîndan
+Şegíşkenge ğol aşa.
+Iman nurlî daklarînda
+Bayragîn sallar ğelge
+Tabît-kapak arasîndan
+Koşar ses meleklerge.
+
+Kor
+
+Şegíşíñíz, miliyonlar,
+Túzeleğek álemge!
+Hakkîñîznî kubbede
+Úyken Alla kaytarağak.
+
+Tañrîlarday bírew yoktîr
+Hoş olarga uşamak.
+Ağî-Aşlîk kuwansînlar
+Kunakka bolîp koşak.
+Óş-aşuw unutulsunlar,
+Ottay ğaw bagîşlansîn,
+Endí yaşlar akmasînlar,
+Íşten kurt kemírmesín.
+
+Kor
+
+Sílínsín borîş defterí!
+Hakklaşsîn dúniyalar!
+Kardaşlar, yîldîz kubbede
+Tañrî bízdiy yargîlar.
+
+Kunak kaynay kadelerde
+Altîn yúzúm kanînda
+Yamyam íşer múlayemet
+Şáresízlík hamaset.
+Uş karadaşîm óz yeríñden
+Kadeler tolîp tursa
+Kókke kópíríp taşîsîn:
+Bo íşe tañrîlarga.
+
+Kor
+
+Yîldîzlardan alîr maktaw
+Meleklerden kaside
+Mína, oga bo kade
+Kubbe ústí ğuwaş tînga!
+
+Derman-ğúrek dert-azapka,
+Yardîm ğangan sábiyge,
+Meñgí súrsún yemín-antlar,
+Hakk dostka, hakk dúşmanga,
+Erlík-ğúrek meliklerge,
+Ğan man, kan man, kardaşlar,
+Taj berílsín hakketkenge
+Ğeñílsín yalanğîlar.
+
+Kor
+
+Taraytîlsîn veliy şewre 
+Bo al şarap ant bolsîn
+Yemín etíp íşílsín
+Yîldîz mahkemeğísínde.
+
+Zalim şînğîrîndan halás,
+Múruvvet yamanlîkka,
+Umut eğel tóşegínde,
+Insaf akîrzamanda!
+Ólíler de yaşayğaktîr
+Kardaşîm, íşíp eglen,
+Gúnalar sílíneğektír,
+Kapatîlağak ğennem.
+
+Kor
+
+Aydîn sáátte elvedalar!
+Tatlî yukî kefende!
+Kardaş, bagîşlagan karar,
+Awuz ğeza keskende. — An die Freude
+Freude, schoener Goetterfunken,
+Tochter aus Elysium,
+Wir betreten feuertrunken,
+Himmlische, dein Heiligtum.
+Deine Zauber binden wieder
+Was der Mode Schwert geteilt
+Bettler werden Fuerstenbrueder
+Wo dein sanfter Fluegel weilt.
+
+Chor.
+
+Seid umschlungen, Millionen!
+Diesen Kuss der ganzen Welt!
+Brueder - ueber'm Sternenzelt
+Muss ein lieber Vater wohnen.
+
+Wem der grosse Wurf gelungen,
+Eines Freundes Freund zu sein,
+Wer ein holdes Weib errungen,
+Mische seinen Jubel ein!
+Ja - wer auch nur eine Seele
+Sein nennt auf dem Erdenrund!
+Und wer's nie gekonnt, der stehle
+Weinend sich aus diesem Bund
+
+Chor.
+
+Was den grossen Ring bewohnet,
+Huldige der Sympathie!
+Zu den Sternen leitet sie,
+Wo der Unbekannte thronet.
+ 
+Freude trinken alle Wesen
+An den Bruesten der Natur,
+Alle Guten, alle Boesen,
+Folgen ihrer Rosenspur.
+Kuesse gab sie uns, und Reben,
+Einen Freund, geprueft im Tod,
+Wollust ward dem Wurm gegeben,
+Und der Cherub steht vor Gott.
+
+Chor.
+
+Ihr stuerzt nieder, Millionen?
+Ahndest du den Schoepfer, Welt?
+Such ihn ueberm Sternenzelt.
+Ueber Sternen muss er wohnen.
+
+Freude heisst die starke Feder
+In der ewigen Natur.
+Freude, Freude, treibt die Raeder
+In der grossen Weltenuhr.
+Blumen lockt sie aus den Keimen,
+Sonnen aus dem Firmament,
+Sphaeren rollt sie in den Raeumen,
+Die des Sehers Rohr nicht kennt.
+
+Chor.
+
+Froh, wie seine Sonnen fliegen
+Durch des Himmels praecht'gen Plan
+Laufet, Brueder, eure Bahn,
+Freudig wie ein Held zum Siegen.
+
+Aus der Wahrheit Feuerspiegel
+Laechelt sie den Forscher an.
+Zu der Tugend steilem Huegel
+Leitet sie des Dulders Bahn.
+Auf des Glaubens Sonnenberge
+Sieht man ihre Fahnen wehn,
+Durch den Riss gesprengter Saerge
+Sie im Chor der Engel stehn.
+
+Chor.
+
+Duldet mutig, Millionen!
+Duldet fuer die bess're Welt!
+Droben ueber'm Sternenzelt
+Wird ein grosser Gott belohnen.
+
+Goettern kann man nicht vergelten,
+Schoen ist's, ihnen gleich zu sein.
+Gram und Armut soll sich melden,
+Mit den Frohen sich erfreun.
+Groll und Rache sei vergessen,
+Unserm Todfeind sei verziehn,
+Keine Traene soll ihn pressen,
+Keine Reue nage ihn.
+
+Chor.
+
+Unser Schuldbuch sei vernichtet!
+Ausgesoehnt die ganze Welt!
+Brueder, ueber'm Sternenzelt
+Richtet Gott, wie wir gerichtet.
+
+Freude sprudelt in Pokalen,
+In der Traube goldnem Blut
+Trinken Sanftmut Kannibalen,
+Die Verzweiflung Heldenmut--
+Brueder, fliegt von euren Sitzen,
+Wenn der volle Roemer kreist,
+Lasst den Schaum zum Himmel sptruetzen:
+Dieses Glas dem guten Geist.
+
+Chor.
+
+Den der Sterne Wirbel loben
+Den des Seraphs Hymne preist,
+Dieses Glas dem guten Geist
+Ueber'm Sternenzelt dort oben!
+
+Festen Mut in schwerem Leiden,
+Huelfe, wo die Unschuld weint,
+Ewigkeit geschwornen Eiden,
+Wahrheit gegen Freund und Feind,
+Maennerstolz vor Koenigsthronen--
+Brueder, gaelt es Gut und Blut.--
+Dem Verdienste seine Kronen,
+Untergang der Luegenbrut!
+
+Chor.
+
+Schliesst den heilgen Zirkel dichter
+Schwoert bei diesem goldnen Wein:
+Dem Geluebde treu zu sein,
+Schwoert es bei dem Sternenrichter!
+
+Rettung von Tyrannenketten,
+Grossmut auch dem Boesewicht,
+Hoffnung auf den Sterbebetten,
+Gnade auf dem Hochgericht!
+Auch die Toten sollen leben!
+Brueder, trinkt und stimmet ein,
+Allen Suendern soll vergeben,
+Und die Hoelle nicht mehr sein.
+
+Chor.
+
+Eine heitre Abschiedsstunde!
+Suessen Schlaf im Leichentuch!
+Brueder--einen sanften Spruch
+Aus des Totenrichters Mund.
 `;
