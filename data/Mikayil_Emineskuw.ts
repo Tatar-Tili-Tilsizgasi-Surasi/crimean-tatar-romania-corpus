80db654261faa37b12102ddcb2eddfc3fcae7f95
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 13:38:23 +0200

    Update Mikayil_Emineskuw.ts

--- data/Mikayil_Emineskuw.ts
+++ data/Mikayil_Emineskuw.ts
@@ -21,4 +21,88 @@ Penğíreníñ kóşesínde
 Ea pasul şi-l îndreaptă
 Lângă fereastră, unde-n colţ
 Luceafărul aşteaptă.
+# Yîldîz, uzak deñízlerde
+Tuwîp ğarîk atar,
+Kîbîrdagan ízleklerde
+Kemí ğolî aşar. — Privea în zare cum pe mări
+Răsare şi străluce,
+Pe mişcătoarele cărări
+Corăbii negre duce.
+# Hergún kókke karay-karay
+Şo kîz aşîk bola,
+Şolpan da góñílín baylay,
+Karşîlîklî sewda. — Îl vede azi, îl vede mâni,
+Astfel dorinţa-i gata;
+El iar, privind de săptămâni,
+Îi cade dragă fata.
+# Tírsegíne başîn salîp
+Kîz túşlerge dala,
+Meraklanîp ta ğan atîp
+Góñíl-ğúrek tola. — Cum ea pe coate-şi răzima
+Visând ale ei tâmple
+De dorul lui şi inima
+Şi sufletu-i se împle.
+# Yîldîz da kîzîşîp ğanar
+Beklep akşam-akşam,
+Saray kólgesíne karar
+Kîz kóstersín endam. — Şi cât de viu s-aprinde el
+În orişicare sară,
+Spre umbra negrului castel
+Când ea o să-i apară.
+# Kîznî ízlep adîm-adîm
+Odasîna tayar,
+Ateşínden şagîm-şagîm
+Bír aw tokîştîrar. — Şi pas cu pas pe urma ei
+Alunecă-n odaie,
+Ţesând cu recile-i scântei
+O mreajă de văpaie.
+# Ğatagîna yukîmsîrap
+Kîz uzanîp ğatsa,
+Tatlî-tatlî kolîn sîypap,
+O, kírpígín ğuma. — Şi când în pat se-ntinde drept
+Copila să se culce,
+I-atinge mâinile pe piept,
+I-nchide geana dulce;
+# We aynadan taşîr nurlar
+Onîñ kewdesíne,
+Kaytîmlanîr ak ziyalar
+Kózí men betíne. — Şi din oglindă luminiş
+Pe trupu-i se revarsă,
+Pe ochii mari, bătând închişi
+Pe faţa ei întoarsă.
+# Kaltîragan piyalaga
+Kúlímsírep hoş kîz,
+Góñílíne yapîşmaga
+Túşer edí yîldîz. — Ea îl privea cu un surâs,
+El tremura-n oglindă,
+Căci o urma adânc în vis
+De suflet să se prindă.
+# Kîz, túşínde tartîp íşín,
+Úrkíp ğîlamsîray:
+– „Ne kelmiysíñ maga tatlîm,
+Túşímdekí ğanbay? — Iar ea vorbind cu el în somn,
+Oftând din greu suspină
+– „O, dulce-al nopţii mele domn,
+De ce nu vii tu? Vină!
+# Túş yaratîp, ğuwaş şolpan,
+Hayatîma ziya,
+Kír úyíme, geş oyîmdan,
+Nurdan taya-taya.” — Cobori în jos, luceafăr blând,
+Alunecând pe-o rază,
+Pătrunde-n casă şi în gând
+Şi viaţa-mi luminează!"
+# O kaltîrap kulak sala
+Taa bek ateş alîp,
+Bírden ózín atîp ala
+Deñízlerge batîp. — El asculta tremurător,
+Se aprindea mai tare
+Şi s-arunca fulgerător,
+Se cufunda în mare;
+# Burum-burum, dalgan suwî,
+Túm-túwerek bola,
+Deren suwdan kayet yakşî, 
+Eşsíz ğígít şîga. — Şi apa unde-au fost căzut
+În cercuri se roteşte,
+Şi din adânc necunoscut
+Un mândru tânăr creşte.
 `;
