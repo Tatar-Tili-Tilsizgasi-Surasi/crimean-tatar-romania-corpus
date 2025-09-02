4772306f21a5a5446d56a02efb8c2a8d007845a5
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:55:04 +0200

    Update Arthur_Rimbaud.ts

--- data/Arthur_Rimbaud.ts
+++ data/Arthur_Rimbaud.ts
@@ -1,2 +1,81 @@
 export const arthurRimbaudEntries: string = `
+# Síftí akşam
+- Kalgan edí bek şîpalak,
+Keñ terekler merak, kurnaz,
+Atîp terezege yaprak,
+Kelír edí yakîn, az-az.
+
+Mol koltîkta tutup mesken
+Yarî-ğalañ, kol tutuşa,
+Ğerde úrkeler hewesten
+Wak ayaklar, ğuka-ğuka. 
+
+- Bír kóz atsam, mum rengínde
+Kíşke bír nur, oynak, zerlek
+Dalgalana tebessúmde,
+Kókírekte – gúlde bóğek.
+
+- Inğe topîk man bílekler
+Óptím, kúldí ğîmşak, azgîn,
+Kesíp ğîltîr zengúleler
+Gúzel billúr kúlúşlerín.
+
+Ufak ayak kaşîp kete
+Kólek astî: "Pítír endí!"
+- Razî bolîp elk ğúretke
+Igí kúlúş ğeza berdí.
+
+Erínímde kaltîragan
+Sábiy kózín óbíp aldîm.
+- Başîn yawaş artka tartkan:
+"Endí fazla bo yapkanîñ!"
+
+"Bay, músaade, ekí lafka…"
+- Artkan óbúw kókírekke
+Atîlganda, kúlúp ala,
+Kúlúş ibraz heweslíkke.
+
+- Kalgan edí bek şîpalak,
+Keñ terekler merak, kurnaz,
+Atîp terezege yaprak,
+Kelír edí yakîn, az-az. — Première Soirée
+"- Elle était fort déshabillée
+Et de grands arbres indiscrets
+Aux vitres jetaient leur feuillée
+Malinement, tout près, tout près.
+
+Assise sur ma grande chaise,
+Mi-nue, elle joignait les mains.
+Sur le plancher frissonnaient d'aise
+Ses petits pieds si fins, si fins
+
+- Je regardai, couleur de cire
+Un petit rayon buissonnier
+Papillonner dans son sourire
+Et sur son sein, - mouche ou rosier
+
+- Je baisai ses fines chevilles.
+Elle eut un doux rire brutal
+Qui s'égrenait en claires trilles,
+Un joli rire de cristal
+
+Les petits pieds sous la chemise
+Se sauvèrent : "Veux-tu en finir !"
+- La première audace permise,
+Le rire feignait de punir !
+
+- Pauvrets palpitants sous ma lèvre,
+Je baisai doucement ses yeux :
+- Elle jeta sa tête mièvre
+En arrière : "Oh ! c'est encor mieux !...
+
+"Monsieur, j'ai deux mots à te dire..."
+- Je lui jetai le reste au sein
+Dans un baiser, qui la fit rire
+D'un bon rire qui voulait bien.....
+
+- Elle était fort déshabillée
+Et de grands arbres indiscrets
+Aux vitres jetaient leur feuillée
+Malinement, tout près, tout près.
 `;
