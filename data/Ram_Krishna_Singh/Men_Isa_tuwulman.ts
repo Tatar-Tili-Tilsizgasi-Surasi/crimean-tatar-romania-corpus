594860f519ef28050e0590f8e72c5e2903ec8aab
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:41:51 +0200

    Update Men_Isa_tuwulman.ts

--- data/Ram_Krishna_Singh/Men_Isa_tuwulman.ts
+++ data/Ram_Krishna_Singh/Men_Isa_tuwulman.ts
@@ -1,4 +1,2 @@
 export const menIsaTuwulmanEntries: string = `
-# Men Isa tuwulman, tek bir insanman. — I am not Jesus, I am just a human.
-# Er birimiz öz yolumıznı taba. — Each of us finds our own way.
 `;
