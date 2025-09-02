b4417b31e3b0cdec1988e9d3794b0ba89a73a85a
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:35:07 +0200

    Delete Ram_Krishna_Singh directory

--- Ram_Krishna_Singh/Kovid-19_hem_sessizlik_tolkini.ts
+++
@@ -1,4 +0,0 @@
-export const kovid19Entries: string = `
-# Kovid-19 vaqtında, sessizlik er yerni qapladı. — During Covid-19, silence covered everywhere.
-# Tolkınlar kibi, sessizlik de kele ve kete. — Like waves, silence also comes and goes.
-`;
