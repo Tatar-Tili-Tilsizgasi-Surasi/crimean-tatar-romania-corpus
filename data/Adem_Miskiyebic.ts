dabafdae8bb4e3849dc0913af3faee2e54536ad6
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 13:07:10 +0200

    Update Adem_Miskiyebic.ts

--- data/Adem_Miskiyebic.ts
+++ data/Adem_Miskiyebic.ts
@@ -2,4 +2,150 @@ export const ademMiskiyebicEntries: string = `
 # El-Kîrîm Sónetlerí — Sonnets from the Crimea
 # Adem Miskiyebiç — Adam Mickiewicz
 # Ingilizğe terğumesi Edna Wortliy Underwut — translated into English by Edna Worthley Underwood
+# Adem Miskiyebiç
+Bír terğúmiyhál taslagî
+ 
+ 
+Sade siyasiy kóríşínden tuwul, edebiyat man sanat kóríşínden de onsekízínğí 
+yuzîl Urusiye men Polagîstan úşún emiyetlí bír kaytîm boldî. Dewlet, 
+ğurtsúygen, şayir men yazuwğîlarnîñ tuwumîna paya boldî. Bo aytuwlî 
+ísímler men faaliyetlerníñ Polagîstanînda kelgendír dúniyaga 1798’de 
+pîkareleşken eskí soylî bír korantanîñ balasî Adem Miskiebiç. Tuwumîndan 
+úş sene ewel atağurtîñ úşúnğí we soñ bólmesí yer alîp imzalî belgeler ağele-
+ağele Úyken Katerina’nîñ tuwum kúnín taa muzaffer etmege Petersburk'ka 
+yollangan edí.
+Bondan sade bírkaş sene ewel (1792’de), kîrk beş biñ Polak ğígítníñ aldîna 
+şîgîp ezúwğî sayîda Kazak man Urus ordîsîn karşîlap Kosciusko batîrğa 
+sawungan edí. Túrúk kanlî marebelerí, Pugaçev isiyanî, Zaporoj Kazaklarnîñ 
+ayaklanmasî, Polak konfederasiyalarî tewúkede yazîlîp kalgandîr. Hem 
+ondokîzînğî yúzyîlî man Napoliyon marebelerí de keldí, hem onîñ Urusiye’ge 
+feğiy kíríşí men barabar, hem Awrupa’da bír uyanuw man barabar, ğañî bír 
+fikir hayatîñ başlamasî man barabar. 
+Miskiebiç Lituwaniye’níñ Nawurdek kasabasînda tuwgan. Bo kasaba "Pan 
+Severin Soplisanîñ katírlerí" başlîgînda Polak onsekízínğí yúzyîlnîñ kiyíplí 
+añîlarîn yazîp, hem óz Lituwan “şîlasik” (bay) korantasî man, hem tuwgan 
+kasabasî man maktanîp kopaygan Kont Henriy Rizewuski’níñ tuwum yerídír. 
+O Minísk'te bír dewlet mektebínde okîp soñra şo kúnlerníñ ğezwit inanşî, 
+kaswetlí manastîr man şîñlagan şan kasabasî Vilne’ge barîp Darúlfúnunga 
+koşîldî. Îslaf topraklarî úşún ğañî şaklar aşkan olaylarîñ zamanlarî edí. Onîn 
+hayatî úşún kayet ilham berúwğí zamanlar edí, o da ósíp şo şaknîñ íşínde 
+uygun bír yer alîp "Polak şiirníñ ólímsíz kahramanî" dep aytîlgan. Hem 
+Polagîstan şo wakît sade Awrupa ordîlarîñ marebe yerí tuwul edí, 
+diplomatlarîñ tabîşîp kúreşken yerí de edí. Dewlet kíşílerí şîganaklarîn 
+kazangan yerí edí. Petersburk’ka ya Warşaw’ga yollanîp başaruwlî şîksa, her 
+ağele íşke ázír sayîlîr edíler. Onîñ balalîgîn belagat, ilham man ğurtsúyúw 
+terbetken bolsa kerek.
+Miskiebiç’níñ tuwum zamanînda Urusiye yakînğa bútún insan faaliyetlerínde 
+sîradantîşî yúkselíşíñ ald bír kaytîmîna kírgen edí. Hakîykattan keleğekte 
+kuwetlí hareket etíp ğañî şaknîñ bír kîsîmîn húkúm eteğek bír ulusîñ tuwum beşígí edí. Hem Úyken Katerina’nîñ dúniya hakimiyetí wahşiy hayallarî man 
+ela, heşbírwakît tekrarlanmayğak maskara hayatî zamanî, hem sade 
+Awurupa’nîñ siyíríne ketmegen Yunanîstan’nîñ şanlî ğanlanma zamanî edí. 
+Bo şaknîñ aytuwlî ísímlerín sîralap şakîrmak ne kadar feğiy, ne kadar 
+tîlsîmlî: Orlowlar, Potemkin, Panin, Poniyatowskiy, Bestuşew-Rúmin, 
+Mîrzakîz Daşkof, Razumowskiy. 
+Şo aldkaytîm Fírengístan'da da ğîltîragan edí. Bo Volter’níñ, 
+ençiklopedistlerníñ, eñ saltanatlî we pîrîltîlî kîralnîñ Fírengístan’î edí. Şo 
+wakîtlarda Ingílístan eñ balaban nutukşîlarîn, başbakanlarîn tanîgan. Úyken 
+Fírederik’níñ Purusiye’sí zamanlarî edí; Ekínğí Yosup’nîñ Awustriye’sí 
+zamanlarî.
+Bíraz soñra, Miskiyebiç’níñ ğaşlîgînda - Goethe kúş yúkseklígíne ğetíp 
+Awrupa’nîñ múnevver diktatorî yetíşken edí. Onîn húkúmínde we ondan 
+ğesaret alîp Şark edebiyatîñ hazinelerí terğúme etílíp Garp’ka tanîtîla edí. Bo 
+sade eñ şanlî Polak şayirleríñ hayatîndan ewel saknaga ağeleden atîlgan bír 
+kózdír. Allah yaratkan mutlakîyet kîrallarîñ ádetlerí men mîrzalarîñ zamalarî 
+soñga keliyatîr edí, herşiyní deñíştíreğek, hem demokratik, hem modern bír 
+dúniya kolnîñ kolayîna kelyatîr edí, Kaderníñ yaratkan bo ğañî kírílgen 
+yúzyîlda kîsmetíñe kawuşağak bolsañ sade kolîñnî uzatmañ keregír edí. O, 
+hem soñgî kiyor kuwet men ayîruwğî kîraliyetlerní kóríp, hem eskí zamannî 
+sîpîrîp atağak modern ruhîñ şafaklarîn kóreğek edí. Búgúníñ men yarînîñ 
+bellí bolmagan, seviyesíz, ólşímsíz, zor bír kóşím kaytîmî edí.
+Onîñ yaşap gezgen kúnleríne karap, askerlígíne, muwallímlígíne, şo 
+kaynagan şaknî uzaktan kózden geşírgende Miskiyebiç’níñ ne kadar kóp 
+yazganîna şaşîp kalmamak múmkin tuwul. Hem kóp yaşagan bír kíşí 
+sayîlmaz; altmîşîna barmadî. Ama şalîşkan yîllarînda, tuyuw melekesín baya 
+túşúrúp akîlîn kaşîra yazdîrgan sîrlî moñlîgîndan ewel, onîñ zihiní pîrîldar, 
+kayet tertíplí, fewkaláde tóretúwğí edí. Ístekke teslím bolîr edí. Bir arada 
+Lozan'da Látinğe hoğasî bolîp ğúrdí; başka bír zamanda Paris'te Îslaf tílleríñ 
+hoğalarî başkanî boldî. Kovno'da Polakşa man Látinğe derslerí berdí. Polak 
+inkilabî faydasîna Italiye’ge barîp kele berdí. Hem kópkenarlî bír akîlî bar 
+edí, hem túrlí faaliyette ğeteneklí edí. Yúksek riyaziyet, felsefe aktarmasîna 
+kóp zaman ayîrgandîr. Lalewel’níñ ğónetímínde Vilne’de bílím aktarmalarî 
+yaptî. Awrupa’nîñ túrlí kalasînda yaşadî. Almaniye'de Goethe men tanîşîp 
+dost boldîlar. Helwetiye’de Kîrasinskiy men bír araga keldí. 1833 yîlînda 
+Çelina Şimanowska man úylendí, ğaşlîgînda Goethe’níñ kayet súygen Îslaf 
+gúzelí we namlî muzika sanatşîsîñ kîzî.
+Hayatî Miskiyebiç men zamandaş, Polagîstan man Urusiye yazuwğîlarnîñ 
+arasînda şonlar bardîr: 1797 tuwumlî romanğî Kórzenowskiy (Çarkof’ta Adem Miskiyebiç’níñ bír kardaşî Kórzenawskiy men úyretmenlík yaptîlar) ; 1829 
+tuwumlî romanğî Danilewskiy, o “El-Kîrîm sónetlerín” Urusşaga terğúme 
+etken; Kimin Malzweskiy, Polak ğurtsúyerí we şayirí, “El-Kîrîm sónetlerí” men 
+yakîn wakîtta ortaga şîkkan, belkí de kalkîñ eñ kóp súygen “Maria” tízmelí 
+hikáyesíñ yazuwğîsî; soñra 1802’de tuwgan Zaleskiy, 1809’nîñ balasî 
+Sîlowaskiy, 1812 tuwumlî Kîrasinskiy bar, Miskiyebiç’níñ tîşînda eñ aytuwlî 
+úş Polak şayirlerí; Polak tenkîyîtşî Bîrodzinskiy de bar. 
+Urusiye’dekí edebiyatîñ altîn şagî Miskiyebiç’níñ hayatî man yakînğa órtíleşíp 
+eñ ónemlí ísímlerden sade bazîlarîn sîralasak aralarînda Puşkin, Lermontof, 
+Şukowskiy, Gogol bardîr.
+Onsekízínğí asîrîñ otîzlarînda Miskiyebiç, şo zaman súrgúnge atîlgan bír 
+súrúw ziya îslafka tolî, Paris kalasînda tabîlmaktadîr. Mínda Şopen men 
+arkadaşlîk kurup, Şopen’níñ eñ ğeteneklí ğaş kîskaayaklî talebelerínden bírsí 
+“El-Kîrîm sónetlerín” fíransîzğaga kaytardî. Miskiyebiç hem múğízelí, hem 
+múkemmel bír Paris’ke kírgen edí. Mína, bo kala şo zamalar "Zekáa’nîñ ógíy 
+anasî" dep aytîlgan edí. Onda súrgúnde Heine de bar edí, Bórne de. Bo kala 
+Ferdinant la Sal’nîñ şahsiy ğazibesín, şikáyetşí yazuwlarîn tanîr edí. Yúğe 
+ísímleríñ kúnlerí edí, rast kele bírkaş tanesín saysak aralarînda Ewjen Súy, 
+Berliyoz, Ğorj Sant, de Músset, Dúmas, Gotiye, Gonkurt kardaşlarî, 
+Gavarniy, Sent Bóv, List, Feliks Mendelssohn, Ary Şeffer, Delakruwa, Horas 
+Vernet bar edí. Juliyus Sîlowaskiy, Kont Kîrasinskiy men Adem Miskiyebiç, 
+hepísí mínda ilham berúwğí Sen ğîlganîñ kalasînda tabîlîp ğîltîrawğî hayatîñ 
+ortasînda şiirlerín túzetíp tertípliy edíler. Paris'tekí bo kaytîm belkí 
+Miskiyebiç’níñ eseríñ eñ inğe íş işaretídír. Zaten Balatlar man Romanslar’nî, 
+Ğiady’nîñ úşúnğí bólímín, Pan Tadewuz’nî yazgan edí. 
+“El-Kîrîm” sîralamasî Miskiyebiç’nîñ ğaşlîgîdan, Vilne kaytîmîndandîr. Şo 
+arada húkúmet hoş kórmegen bír ğemiyetke koşîlîp ğúre edí. Soñînda, şo 
+ğemiyetten heş ayîrîlmaganî úşún, Urusiye’níñ kúneş betíne súrgún etíldí. 
+Ğolda, Yedísan’ga barayatîrganda sónetlerní yazmaga başladî. Olarîñ 
+başaruwî hem hîzlî, hem şaşîrtuwğî boldî. Awrupa’nîñ her tílíne terğúme 
+yapîlganlar. Şekílí hem nakliy, hem kîlasik sónet şekílí bolsa da, bíraz farklî 
+bír píşímde kullanîlîr, sade ruh man zihin tuygîlarîn kóstermek maksatî man 
+tuwul, bo olaylî seferníñ sîrasînda kórgenlerín de ifade etmege bír alat gibí 
+kullanîlîr. Sónetní Fílenderístannî kóstermege “Fílamantlar" şiirínde 
+Verhaeren gibí kullangandîr, Versailles bakşalarîn resímín boyalamaga "Altîn 
+araba" şiirinde Albert Samain gibí kullangandîr. Bonîñ dîkkatka alînmasî 
+keregír. Hem şonî da unutmamak kerek ke senesí 1826’dan ewel edí. 
+Miskiyebiç’níñ tízme şalîşmalarînda her wakît hem ğañî, hem denelmegen 
+imkáanlar karap ádetlerden bazgeşmek kiyífíñ ízlerí tabîlmaktadîr.
+Bo Urusiye'deki súrgúnínde Puşkinní tanîmaga başladî. Şo zaman ózí de ğaş, 
+Puşkin oga mínda şaşma hálínde tanîtîlgan mîsralî bír mektúp yazgan edí. "O 
+bír súre aramîzda yaşadî. Insan oga kuğurlî kórínír edí. Kene de, ne íşínde 
+bír yaramazlîk taşîdî, ne de bírwakît ağîsîn şîgarmaga túşúndí. Ğumart, 
+ğuuwaş kaálplí, neğip fikirlí, o bízím akşam eşlígímízge koşîlîp ğúrgen edí, 
+bíz de onî súydík. Hayallarîmîznî, tasawurlarîmîznî, şiirlerímízní paylaşîp 
+añlattîk. Allah oga zekáa man ilham bergen. O herzaman yúkseklíklerde otîrîp 
+hayatka başîn aşaga alîp karar edí. Tewúkeden, uluslardan sóz eter edík. O, 
+îrklar bútún yaman şiylerní unutup, bagîşlap – marebesín de, isiyanîn da - 
+keleğekte insanlarîñ, tîpkî geñíş bír korantaday, uzlaşîp huzur íşínde 
+yaşayğaklarîn ilan eter edí. Allahtan yakşî bír nutukşî bolganî úşún bíz onî 
+kayet heweslí sesler edík. Bír súreden soñ o şîgîp kettí, bíz de ketíşín 
+kayîrladîk. Soñra întîkamğî îrgî onî óşíktíríp o endí bízím dúşmanîmîz 
+bolganîn eşíttík. Yaramaz tuwganlarîn kuwantmaga bútún dúrkísín kîzgînlîk 
+man totîrdî. Sayîp súygen arkadaşîmîzdan bízge sade întîkam man ópkelí 
+túşúnğeler kaytîp keldí. Alla onîñ dargîn kaálbíne sabîr man akîl bersín!”
+Miskiyebiç’níñ aşîklagan Kîrîm resímlerín belagatlî, usta sózlí Puşkin de 
+kalemge aldî. Tatar kaanlarîñ eskí başkalasîndan o da esínlengendír. Onîñ 
+"Bakşesaray şeşmesí" akîlîmîzdan heş şîkmaz. “Kawkaz esírí” şiirínde 
+Urusiye’níñ kúneş betíndekí ónder daknî kózímízníñ aldîna o da bírtaa 
+ketírer.
+“El-Kîrîm sónetlerí” kazandîrgan motlîgîñ etkísínde húkúmet Miskiyebiç’ke 
+Galisin Mîrzanîñ katînda Moskaw’da bír wazife teklif ettí. “Gaşlîkka kunak” 
+şiirín yazîp soñînda akîlîn karîştîrgan sîr aktarmalarîn başlaganda Rum 
+kalasînda, ğañgîzlîkta edí. Şayirlík kuwetíñ ğariyetín kaybetken soñ, bír 
+súrege kadar obír ğeteneklerí bîzîlmadan saklandî. Başlaganî gibí, ómírí 
+başka feğiy bír kaytîmda soñ taptî. Kîrîm marebesín başlaganîn kóríp 1855’te 
+senesínde Ístanbul’da vefat ettí. Súrgún seferínde Kîrîmîñ tasarî resímín 
+sîzmaga sónet alatîn ayîrganî bír yaktan kuğurlî kórínír, obír yaktan da onîñ 
+ózí hakk etíp kazangan faziletídír, şúnkí ondan ewel sónet îslaf şayirleríñ sîkî 
+kullangan ifade usulî sayîlmagan edí, taa keş zamanlarda Vírçliskiy, 
+Píreseren ya da başkalarî sayîsîz sónet yazgan bolsalar da. Sónet taa bek 
+Látin uluslarî man Ingílíz şayirleríñ alatî edí. Gene de, “El-Kîrîm sónetlerí” eñ 
+şanlî sîralamalarîñ arasîndandîr.
 `;
