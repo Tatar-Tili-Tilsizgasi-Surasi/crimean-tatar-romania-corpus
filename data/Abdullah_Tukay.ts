3c75049a07965ecf5c687be6a55a3eb2ab2715c3
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:40:58 +0200

    Update Abdullah_Tukay.ts

--- data/Abdullah_Tukay.ts
+++ data/Abdullah_Tukay.ts
@@ -1,4 +1,26 @@
 export const abdullahTukayEntries: string = `
-# Tugan tilim - altın beşik. — My native language is a golden cradle.
-# Şiirlerim, halqımnıñ yureginden çıqa. — My poems come from the heart of my people.
+# Tuwgan tíl
+Ey, tuwgan tíl, ey gúzel tíl, atam-anamîñ tílí
+Dúniyada neler bílsem, sen tuwgan tíl arkalî.
+Eñ síptí bo tíl men anam beşíkte ayya aytkan,
+Ondan soñra keşe boyî tatam masal añlatkan.
+Ey, tuwgan tíl, herwakîtta yardîmîñ man hep seníñ,
+Kíşkeneden añlaşîlgan şenlígím, kaygîm mením.
+Ey, tuwgan tíl, sen men bolgan eñ síptí kîlgan duwam,
+Bagîşla dep, ózímní de, atam-anamnî, Allam!
+# Bayram búgún
+Bar góñíllílík bútún dúniyada, bar bír ğem búgún. 
+- Ne eken bo?  Men bíldím: Bayram búgún, bayram búgún! 
+Bír mukaddes his yaşap herbír kíşí hayran búgún,
+Oynay sazîmda mením bayram şaluwum, bayram búgún! 
+Arttîra, kórdím, kúneş ádetten ğîltîrawun. 
+ "O kíyíngendír" – dedím, "Bayram búgún, bayram búgún!"
+Hissetíp heryerde de bír túrlí hoş mús kokîsîn,
+"Múslí may ğakkan eken dúniya" – dedím, "Bayram búgún!"
+Bír tílenşíníñ kuşaklaşkanîn kóríp bír akay man, 
+"Góñílí ğîmşagan"- dedím, "Bayram búgún, bayram búgún!"
+Ğar başîndan tîñladîm men bír suwnuñ dulkunlarîn: 
+Aytîşalar arasînda: "Bayram búgún, bayram búgún!..." 
+Tîñladîm ekren gene eskende kîblalar ğelín,
+O da şîbîrday yawaş: "Bayram búgún, bayram búgún!"
 `;
