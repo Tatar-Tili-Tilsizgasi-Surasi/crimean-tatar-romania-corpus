daf7384d1d818fa3a93e2905184d0c1a61906cbd
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:57:04 +0200

    Update Paul_Verlaine.ts

--- data/Paul_Verlaine.ts
+++ data/Paul_Verlaine.ts
@@ -1,2 +1,37 @@
 export const paulVerlaineEntries: string = `
+# Ádet túşúm
+Kele bere ótken, kuğurday túşúme,
+Bír eğnebiy katun, şegermíz bíz sewda,
+Mení ğandan súye, mení añlay-ala,
+Tabiyatî sabit, deñíşmiy túrlene.
+
+Ke o mení añlar, mením íşím, saydam
+Onîñ úşún tek, ay! heş bolmay mesele
+Onîñ úşún sáde, mañlayîmnî bíle
+Ğîlap suwutmaga ateşlensem-kîzsam.
+
+Esmer, sarî, kîzîl? Şáşín rengín bílmem
+Lákin atî ela, tatlî, ğîmşak, mahrem,
+Eskí dostlar atî, kayîplarga sîrdaş.
+
+Ekí kózí algan hewkellerden nazar,
+Sesí mesafelí, taşîr hilmiy, ğuwaş,
+Ólílerden kalgan, aziz awaz-ğîrlar. — Mon rêve familier
+Je fais souvent ce rêve étrange et pénétrant
+D'une femme inconnue, et que j'aime, et qui m'aime
+Et qui n'est, chaque fois, ni tout à fait la même
+Ni tout à fait une autre, et m'aime et me comprend.
+
+Car elle me comprend, et mon coeur, transparent
+Pour elle seule, hélas ! cesse d'être un problème
+Pour elle seule, et les moiteurs de mon front blême,
+Elle seule les sait rafraîchir, en pleurant.
+
+Est-elle brune, blonde ou rousse ? - Je l'ignore.
+Son nom ? Je me souviens qu'il est doux et sonore
+Comme ceux des aimés que la Vie exila.
+
+Son regard est pareil au regard des statues,
+Et, pour sa voix, lointaine, et calme, et grave, elle a
+L'inflexion des voix chères qui se sont tues.
 `;
