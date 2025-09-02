6ff8773fc3766e9a36d8c181b5ac04429d1db10b
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 12:54:00 +0200

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -5,6 +5,7 @@ Henriy s. (antrop. m.) Henry.
 Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
 Minísk s. (topon.) Minsk.
+Moskaw s. (topon.) Moscow.
 Petersburk s. (topon.) Petersburg.
 Şînğa s. (topon.) Xinjiang, China.
 Vilne s. (topon.) Vilnius.
