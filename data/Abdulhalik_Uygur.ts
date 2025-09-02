d218a2bdaa932aae0279e92db58218dcb8dce2b1
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 20:38:42 +0200

    Update Abdulhalik_Uygur.ts

--- data/Abdulhalik_Uygur.ts
+++ data/Abdulhalik_Uygur.ts
@@ -1,4 +1,34 @@
 export const abdulhalikUygurEntries: string = `
-# Közleriñde bir ateş bar, sönmez. — There is a fire in your eyes that never goes out.
-# Vetan üçün can feda. — For the homeland, a life sacrificed.
+# Uyan!
+Hey, pakîr Uygur, uyan, yukuñ yeter,
+Sende mal yok, endí ketse ğan keter.
+Bo ólímden ózíñní kutarmasañ,
+Ah, seníñ hálíñ píter, hálíñ píter.
+
+Tur! dedím, başîñ kóter, yukuñnî aş!
+Rakibníñ başîn kesíp kanîn şaş!
+Kóz aşîp etrapka eger bakmasañ,
+Ólseñ armanda, bír kún yok ilaş.
+
+Zaten ğansîzga uşadî teníñ,
+Şoga yok mî heş te ólímden gamîñ?
+Şakîrsam kîbîrdamadan ğatasîñ,
+Uyanmay ólgen mí şo seníñ betíñ?
+ 
+Kózíñní geñíş aşîp etrapka bak,
+Óz istikbalîñ hakkînda oylan uzak.
+Ketse koldan bo ganimet, pîrsatî,
+Keleğek úşún íşíñ şîltak, íşíñ şîltak.
+
+Ağînîr góñílím saga, hey Uygurum,
+Arkadaşîm, kardaşîm, bír tuwganîm.
+Kúyúnúp hálíñe tursatsam sení,
+Añlamaysîñ zatiy neler bolganîn?!
+
+Keleğek bír-gún, sen peşman kelersíñ,
+Sóz manasîna şo wakît ğetersíñ.
+"Way" deseñ şo wakît sayîlmay kalîr,
+O zaman, Uygur, sózge hakk berersíñ.
+
+1921-Yîl, Turpan
 `;
