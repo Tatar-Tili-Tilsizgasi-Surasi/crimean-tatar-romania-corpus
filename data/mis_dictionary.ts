b2743df817bff9376a6e08753e5a52775f976852
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 12:51:40 +0200

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -1,6 +1,13 @@
 export const misDictionaryRawText: string = `
-Ğadîlar Bayramî s. Halloween
+Ğadîlar Bayramî s. Halloween.
+Ğorj s. (antrop. m.) George.
+Henriy s. (antrop. m.) Henry.
+Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
+Minísk s. (topon.) Minsk.
+Petersburk s. (topon.) Petersburg.
 Şînğa s. (topon.) Xinjiang, China.
-Virjiniye s. (topon.) Virginia
+Vilne s. (topon.) Vilnius.
+Virjiniye s. (topon.) Virginia.
+Warşaw s. (topon.) Warsaw.
 `;
