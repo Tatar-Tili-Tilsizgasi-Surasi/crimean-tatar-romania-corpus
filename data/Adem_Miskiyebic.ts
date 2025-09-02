71d6adbf7aeb009ed206a1005b79c038a34cdcbf
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 13:19:30 +0200

    Update Adem_Miskiyebic.ts

--- data/Adem_Miskiyebic.ts
+++ data/Adem_Miskiyebic.ts
@@ -6,7 +6,6 @@ Kîrîm Tatarğa terğúmesí Taner Murat — translated into English by Edna Wo
 translated into Crimean Tatar by Taner Murat
 # Adem Miskiyebiç
 Bír terğúmiyhál taslagî
-  
 Sade siyasiy kóríşínden tuwul, edebiyat man sanat kóríşínden de onsekízínğí 
 yuzîl Urusiye men Polagîstan úşún emiyetlí bír kaytîm boldî. Dewlet, 
 ğurtsúygen, şayir men yazuwğîlarnîñ tuwumîna paya boldî. Bo aytuwlî 
@@ -149,4 +148,155 @@ kullangan ifade usulî sayîlmagan edí, taa keş zamanlarda Vírçliskiy,
 Píreseren ya da başkalarî sayîsîz sónet yazgan bolsalar da. Sónet taa bek 
 Látin uluslarî man Ingílíz şayirleríñ alatî edí. Gene de, “El-Kîrîm sónetlerí” eñ 
 şanlî sîralamalarîñ arasîndandîr.
+Edna Wortliy Anderwut — Adam Mickiewicz
+A Biographical Sketch
+The last of the eighteenth century was an important period for Russia and 
+Poland, not only politically, but in letters and art. It marked the birth of 
+statesmen, patriots, poets and writers. It was into a Poland of great names and 
+greater activities that Adam Mickiewicz was born in 1798, as son of an 
+impoverished family of the old nobility. Three years before, the third and last 
+partition of his native land had taken place, and the signed documents had 
+been hastened to Petersburg to make more triumphant the birthday of the 
+Great Catherine.
+Just a few years before this (1792), Kosciusko had courageously led his forty-
+five thousand valiant Poles in their brave defiance of an overwhelming number 
+of Cossacks and Russians. History had recorded the bloody Turkish wars, the 
+Pugatshev rebellion, the uprising of the Zaporogian Cossacks and the Polish 
+confederations. And with the nineteenth century came the Napoleonic wars 
+with the dramatic entry of Napoleon into Russia, and a new and different 
+mental life began to dawn over Europe.
+Mickiewicz was born in Novogrodek in Lithuania. This was the birthplace of 
+Count Henry Rzewuski, who wrote the delightful memories of the Polish 
+eighteenth century, under the title of "The Memories of Pan Severin Soplica,"*
+Cupbearer of Parnau, by Count Henry Rzewuski. and who declared he 
+considered it an honor to be born a "schlazig" (noble) of Lithuania, and of 
+Novogrodek. He went to a government school in Minsk, and later attended the 
+University of Vilna, which city in his day was a place of Jesuit faith, gloomy 
+convents and echoing bells. All about him epoch-making events for Slav lands 
+were taking place. It was a resounding, inspired age for his race, and he grew 
+up to take a fitting place in that age and to be called "the immortal hero of 
+Polish poetry." Poland just then was the battle-ground not only for the armies 
+of Europe, but for the diplomats. It was a place for statesmen to win their 
+spurs. If accredited to Petersburg or Warsaw, and successful, they were 
+believed to be equal to any diplomatic emergency. Eloquence, inspiration, and 
+patriotic fervor must have cradled his childhood.
+At the time of the birth of Mickiewicz, Russia was bringing to a close a 
+prodigious period of development in almost every field of human activity. It was 
+really the birth-throe of a nation that was to move powerfully, and to dominate-
+-partially--the new age. And the splendid and never again to be equalled pageant of the life of Catherine the Great, with its wild dreams of world 
+dominance and of the glorious revival of perished Greece, had just been 
+unrolled for the amazement of Europe. What dramatic and enchanting 
+memories the names of her followers call up: the Orlows, Potemkin, Panin, 
+Poniatowski, Bestushew-Rjumin, Princess Daschkov, Razumowski.
+In France, too, the same preceding period had been brilliant. It had been the 
+France of Voltaire, the Encyclopedists, and a most resplendent and luxurious 
+monarch. England had known her greatest orators and prime ministers. It had 
+been the Prussia of Frederick the Great; the Dresden of August the Strong; the 
+Austria of Joseph the Second.
+A little later--during Mickiewicz's own youth--Goethe was at the height of his 
+power and the intellectual dictator of Europe. Under his direction and 
+encouragement the treasures of oriental literature were being translated and 
+made known to the West. This is merely a hasty glimpse of the "mise-en-scene" 
+that preceded the debut in life of the most renowned of Polish poets. The old 
+traditions of absolute and God-created monarchs and princely times were 
+coming to an end, and that democratic modern world, where everything was to 
+change, was close at hand, just over the crest, indeed, of this new century into 
+which Fate was ushering him. He was to see the last of blind power and royal 
+prerogative, and the first dawn of a modern spirit which in time would sweep 
+away forever, the old. It was an uncertain, difficult transition period, without 
+standards and without measurements.
+As we take a fleeting, bird's-eye view of the stirring times in which his days 
+were spent, his travels, his army life, his periods of professorship, we can not 
+help but wonder at the amount of writing Mickiewicz did. And his life was not a 
+long one; it did not reach to sixty years. But during the working years allotted 
+him, before a mystical melancholy--which was threatening to degenerate into 
+madness--had impaired his faculties, his mind was unusually brilliant, 
+creative and marvelously disciplined. It obeyed at will. At one time he was 
+professor of Latin in Lausanne; at another time he held the chair of Slavic 
+languages in Paris. He taught Polish and Latin in Kovno. He traveled 
+extensively in Italy in the interest of the Polish revolution. His mind was many-
+sided and capable of various activities. He devoted considerable time to 
+advanced mathematics and philosophy. He made scientific investigations in 
+Vilna under Lalewel. At one time and another he lived in various large cities of 
+Europe. In Germany he met and became friendly with Goethe. In Switzerland 
+he met Krasinski. In 1833 he married Celina Szymanovska. Her mother was 
+the famous Slav beauty and musician who had so delighted Goethe in her 
+youth.
+Among writers of Russia and Poland whose life period somewhat coincided with that of Mickiewicz's are: Korzenowski (born in 1797), the novelist (a brother of 
+Adam Mickiewicz was fellow-teacher with Korzenowski at Charkov); Danilewski 
+(1829), likewise a novelist--it was he who translated The Crimean Sonnets into 
+Russian; Malzweski, Polish patriot and poet, whose "Maria"--perhaps the most 
+popular poetic story in Poland--appeared at almost the same time as The 
+Crimean Sonnets; Zaleski (1802), Slowacki (1809), Krasinski (1812), the three 
+greatest poets of Poland excepting only Mickiewicz himself, the Polish critic, 
+Brodzinski.
+In Russia, the golden age of literature almost covered the same period as 
+Mickiewicz's own life--Puschkin, Lermontov, Schukowski, Gogol, to mention 
+only some of the most important names.
+In the eighteen-thirties we find Mickiewicz in Paris, which happened to be filled 
+just then with a crowd of brilliant Slavic exiles. Here he became the friend of 
+Chopin, and one of Chopin's most talented pupils--a young Polish girl--made 
+the first translation of the Sonnets into French. It was a wonderful and 
+brilliant Paris which Mickiewicz entered. This was the time when the city was 
+first called "the stepmother of Genius." Heine was here in exile, and Boerne. It 
+knew the personal fascination and the denunciative writings of Ferdinand la 
+Salle. It was the day, too, of Eugene Sue, Berlioz, George Sand, de Musset, 
+Dumas, Gautier, the Goncourt Brothers, Gavarni, Sainte Beuve, Liszt, Felix 
+Mendelssohn, Ary Scheffer, Delacroiz, Horace Vernet--to mention only a few 
+great names at random. Julius Slowacki, Count Krasinski and Adam 
+Mickiewicz were all here editing their poetry in the midst of this brilliant life in 
+the inspiring city by the Seine. This period in Paris signs perhaps the high-
+water mark of the creative genius of Mickiewicz. He had already written the 
+Ballads and Romances, the third part of Dziady, Pan Tadeuz.
+The Crimean Sequence belongs to the period of Mickiewicz's youth, the Vilna 
+period. He joined a society at this time which was looked upon with disfavor by 
+the Government. At length, because of his continued participation in it, he was 
+exiled to southern Russia. On that trip, while he was going toward Odessa, he 
+began the Crimean Sonnets. Their success was quick and astonishing. They 
+were translated into every language of Europe. Although the form is the 
+traditional and classic sonnet form, he makes use of it in a slightly different 
+manner, not altogether as an exposition of the sentiments of the soul, and the 
+convictions and emotions of the mind, but as an instrument with which to 
+sketch what he saw upon this eventful journey. He used the sonnet form at 
+that period just as Verhaeren used it in "Les Flamandes," to show us Flanders, 
+and as Albert Samain in "Le Chariot d'Or," to picture the gardens of Versailles. 
+This is worthy of note. And this we must remember was before 1826. In the 
+poetical works of Mickiewicz there was always traceable an inclination to break 
+tradition and to search for new and untried possibilities.
+On this exile in Russia he learned to know Puschkin, then a young man like 
+himself. Puschkin has written a verse letter to him which we transcribe in free 
+prose. "He lived among us for a while--a people strange to him. And yet his 
+mind cherished no hatred and no longing for revenge. Generous, kind of heart, 
+noble-minded, he joined our evening circles, and we loved him. We exchanged 
+our dreams, our plans--our poems. God gave him genius and inspiration. He 
+stood always on the heights and looked down on life. We talked of history and 
+of nations. He declared a time would come when races would forget all evil 
+things--like war, rebellion--and dwell together peaceably in one great family. 
+We listened to him eagerly for he had the gift of speech. After a while he went 
+away and we gave our blessing to him. Then we learned our guest--spurred on 
+by his revengeful race--had become our enemy. To please that bitter race of his 
+he filled his songs with hatred. Of our beloved friend there came to us only 
+revenge and angry thoughts. God grant that peace may come again to his 
+embittered heart!"
+Puschkin himself wrote eloquently of these same Crimean scenes that 
+Mickiewicz shows us. He, too, was inspired by the old capital city of the Tartar 
+rulers. We recall his "Fountain of Baktschi Serai." And he, too, brings before 
+our eyes again that gigantic mountain world of southern Russia in "The 
+Prisoner of the Caucasus."
+The fame of The Crimean Sonnets was so great that Mickiewicz was offered a 
+government position which attached him to the person of the powerful Prince 
+Galitzin, in Moscow. It was in Rome, and singularly enough it was when he 
+wrote the "Ode to Youth" that he began to devote himself to mystical studies 
+which had such an injurious effect upon his mind. For some time after he had 
+lost his fluent power as a poet, he retained his conversational gifts which were 
+remarkable and brought him almost as much fame as his poetry. His life ended 
+in a period as dramatic as that in which it began. He entered the Turkish wars 
+in 1855 and died in Stamboul in that same year. It is somewhat peculiar and 
+at the same time no little to his credit that he should have chosen the sonnet 
+as the instrument of his quick sketching of Crimea on the trip of exile, because 
+the sonnet has never been a frequently chosen means of expression of the Slav 
+races, despite the numerous sonnets written later by Vrchlicky, Preseren and 
+others. The sonnet has belonged more to the Latin races, and to the English 
+race. The Crimean Sonnets, however, rank among the famous sequences.
+Edna Worthley Underwood
 `;
