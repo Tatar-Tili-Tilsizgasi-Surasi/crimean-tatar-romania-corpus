7ed8c5dd492b39f519577d7f611f1ffad0224cc8
Tatar Tílí Tílsîzga Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-02 12:58:45 +0200

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -1,4 +1,6 @@
 export const misDictionaryRawText: string = `
+Awustin s. (topon.) Austin, Texas.
+Çikago s. (topon.) Chicago.
 Ğadîlar Bayramî s. Halloween.
 Ğorj s. (antrop. m.) George.
 Henriy s. (antrop. m.) Henry.
@@ -8,6 +10,7 @@ Minísk s. (topon.) Minsk.
 Moskaw s. (topon.) Moscow.
 Petersburk s. (topon.) Petersburg.
 Şînğa s. (topon.) Xinjiang, China.
+Teksas s. (topon.) Texas.
 Vilne s. (topon.) Vilnius.
 Virjiniye s. (topon.) Virginia.
 Warşaw s. (topon.) Warsaw.
