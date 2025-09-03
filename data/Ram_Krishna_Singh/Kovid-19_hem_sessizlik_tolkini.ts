362b722ddd3aa9bcf060875d0309c43727bc5b98
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-03 11:28:54 +0200

    Update Kovid-19_hem_sessizlik_tolkini.ts

--- data/Ram_Krishna_Singh/Kovid-19_hem_sessizlik_tolkini.ts
+++ data/Ram_Krishna_Singh/Kovid-19_hem_sessizlik_tolkini.ts
@@ -1,2 +1,380 @@
 export const kovid19Entries: string = `
+# Kovid-19 hem sessízlík tolkînî — COVID-19 and surge of silence
+# Awlanîp kuwîlgan 
+Kólekelí bír yúz 
+yarî karañgîda yuklay betín ğașîrîp 
+katlamanîñ renklerínde 
+onîñ bașîn, șáșín ya da ayaklarîn sîypalap 
+nefes alîp akîlîma kele 
+Mukaddes Ruh 
+yarî keșeníñ soñrasîn íșíne alîp 
+akîlîmda kalgan túnkúldemesín: 
+men yukîsîndakî konîșkanîn kórmiymen 
+apakayîm șegíșmeden kașar 
+torînlarîm da korkar 
+men sessízlíkní yukîlatmam 
+bírew eșítmese de 
+rahátsízlangan ruhnî 
+yaman darîlîp aytkanda 
+mením heșbírwakît aytmaganlarîmnî — Haunted 
+A shadowed figure 
+sleeps in half-dark, face hidden 
+in blanket’s colour 
+I grope its head, hair or feet 
+with bated breath remember 
+the Holy Ghost 
+to contain the post-midnight 
+murmur in mind: 
+I don’t see the sleep-talk 
+my wife hates to suffer 
+and grandchildren fear 
+I don’t let silence sleep 
+even if none hear 
+the disturbed spirit 
+growing wild to say 
+what I never say
+# Bo Mením zamanîmdîr 
+120 dolarlîk 
+bír tuwum egíp 
+aldîmda tabîlgan dep aytîlgan 
+eñ yakșî kúnlerímní ka-típ satîp alîrman? 
+Men de múrekkep hakîykatnî akîlîmda tuttîm 
+katírler yazgan kîyanetçílerden saklamaga 
+mísápírlerge katírler 
+Isanîñ kîrallîgînda tînîșlîk yardîmî 
+karañgî awda saw kaldîm 
+1976 senesínden berítlí 
+„Ğerler síznínkí, alîñîz!” 
+men șalîștîrîp tuttîm ğol kambîrlî, 
+aylanîșlî hem taydîraklî bolsa da 
+şúndí hîznîñ tîșlî dóngeleklerín awuștîrmak zamanî keldí 
+boran yónelíșín deñíștíríp 
+múğizení kabletmek zamanî 
+Kuday ğemíșín kabletíp, evliya Peter bolîp 
+suw ústínde ğúrmek zamanî keldí
+peygamber dostîm îșandîrdî 
+endí mením wakîtîmdîr: 
+moyîndîrîk oñay 
+ğúk te ğeñgíldír — It’s My Time 
+How can I buy my best days 
+said to be ahead of me 
+by sowing a seed of 
+120 dollars? 
+I’ve hydraheaded truth too 
+to save from traitors who write 
+souvenirs for the guests 
+in Christ’s kingdom silence aids 
+the dark web I survived 
+since 1976 
+“The land is yours for the taking!” 
+I’ve kept going despite the bumps 
+and rollercoaster ride 
+now is the time to shift the gears 
+change the direction of storm 
+receive the miracle 
+the divine fruit, and be Peter 
+to walk on the water
+assures the prophet friend 
+it’s my time now: 
+the yoke is easy 
+and the burden light
+# Enerğiya kesílmesí 
+Kawșak hem tîñîșsîz 
+kaygîlarga tutîlîp 
+alîșkanlîk, yukîsîzlîk 
+omîrga súyegíníñ zayîplamasî 
+moyîn hem arka awurmasî 
+katîp kalgan ayaklar 
+tíșsíz, hewessíz 
+kanamalar túpden, her yerden 
+dîkkatsîzlîk 
+yapkanlarîmnîñ 
+yapmak hewesíníñ kartaymadan 
+ólímí 
+herșiy kîllî, korkînîșlî boldî, 
+kewde ğetíșsízlígí men 
+awuruwlar sekíríp barlîkka kele 
+ruh kurîdî 
+akîl baglantîsî kesílíp kaldî
+ózímní toplay-almayman 
+yașîmnîñ kírín temízlemege 
+íșímdekí çakralarda 
+oylap unutîp 
+katírleríñ ğúgín 
+bașka hayat bolîp ólírmen — Energy Block 
+Frazzled and restless 
+bouts of anxiety 
+addiction, sleeplessness 
+spinal degeneration 
+pain in the neck and back 
+numbness in the legs 
+loss of teeth, libido 
+anal bleeding and what not 
+failure to stay focused 
+and dying desires to do 
+what I used to do 
+are not mere aging 
+things get hairy, scary 
+with body failure 
+ailments pop up 
+spirit dries up 
+mind disconnects
+I’m hardly centered 
+to clean my age’s turd 
+on inner chakras 
+meditate and forget 
+the memories’ load 
+and die a new being
+# Óz-ózín itibardan túșírmek 
+Oylanuw – 
+uzun ómír yașamak, amma keñ yașamakta 
+bașaruwsîz bolmak 
+Senekanîñ degenídiy bíz 
+óz-ózímízden kașkanlardanmîz 
+șúndí íșlí bolîp 
+boș bolmak 
+aylanîp turîp 
+bírí-bírísíñ yukîsîn 
+tertíplerler 
+hoș ya da yaman kóríp 
+bașkalarîñ buyurîgîn 
+aldanîp kalîrmîz 
+ğok bolgan zamanîmîzda, 
+íșímízní totîra-almadan — Self-neglect 
+Meditation— 
+living long but failing 
+to live wide 
+says Seneca we are 
+fugitives from ourselves 
+the busyness 
+and weariness of now 
+they toss about 
+regulating their sleep 
+by one another’s 
+love or hate 
+what others dictate 
+we get duped 
+our time lost, without 
+inner wholeness
+# Saylawdan soñ 
+Olar eșítmezler 
+miliyonlarnîñ 
+tawușsîz kîșkîruwlarîn 
+bahîtsîzlîktan yorîlîp 
+oñaylî oyînlar oynarlar 
+sábiy saylawğîlar 
+awur yașam – 
+ğennetten úmítlengen 
+milletníñ kaderí — Post-election 
+They don’t hear 
+the silent screams of 
+millions 
+tired of misfortune 
+play games of convenience 
+innocent voters 
+sordid life-- 
+nation’s destiny 
+heaven-fed
+# Șírík 
+Ay enerğiyasî 
+íșímízdekí keñlíkní totîra – 
+uyanuwga șakîruw 
+ya da óz yîldîzlarînda yañgîșlarnî 
+hemen túzete-almagan 
+ğaralarîñ kefíllígínde kalîrsîñ 
+yîkpalnî óríp turganda 
+tañrîlarnîñ ğayragan șópleríñ arasîndan 
+murînîma șírík kokîsî kele — Rot 
+Moon energy 
+fills up the inner space-- 
+call to wake up 
+or be hostage to wounds 
+that don’t autocorrect 
+astral faults 
+knitting the luck 
+amidst the waste gods spread 
+I smell the rot
+# Músleme 
+Hoș îtîrlî șópler 
+hawanî kokîlatîr – 
+kurbanlîk taktasînda 
+melekler bagîșçîlarnî kuwandîrîp 
+tertípsízlíkke karșî múbareklík sezdíreler 
+kún ğarîgîna karañîz 
+búgúnkí Kudaynîñ yakșîlîgî 
+șúndígí múslemesínde — Anointment 
+Incense sticks 
+perfume the air around-- 
+offering on altar 
+angels rejoice donors 
+feeling blessed despite havoc 
+see the light of day 
+God’s goodness in present 
+his anointment now
+# Soñîș hesaplamasî 
+Șeneșkelí hem tíkenlí yollarîñ arasînda 
+kólekeler șóger. O gázatalarnî aldatîp baș-bakannî 
+maktar 
+gonzo tartîșuwlarîndan pîșkîrgan hikmet 
+șalîmlar satar. Bașkalarî da onîñ osallîgîn sezíp sessíz 
+kalmasîn 
+ústín tutalar. Olar kafestekí dudukușnî bașka túrlí 
+karalgan yetmíș yîllîk demokrasiyanî ayîplamaga 
+azat ekenín bíleler. Olarnîñ kaberíler ne kadar kúșsíz 
+ekenlerínden 
+ğanîp turgan orman kúlín kîzarmasîn toktatmaga 
+șúndí akîlda bîzîklîklar uyîp 
+diniy kaytarîlmasîndan ewel Ramnîñ asîlmasîn beklep 
+kîșkîruwlarîn, kamșîlarîn hem saplamalarîn sayalar — Aftermath 
+Between the mossy and thorned pathways 
+shadows slant. He trumps the press and praises PM 
+wisdom splashed in gonzo arguments 
+cocks the walk. Others too feel his sting but prefer 
+silence. They know the caged parrot’s free 
+to shame seven decades of democracy groomed 
+differently. They know how weak they are 
+to stop the burning forest’s ash from reddening 
+now aberrations clot in the mind 
+await Ram’s hanging before the wounded converts 
+count the cries, lashes and piercings
+# Túș 
+Men ótmíșíñ ezílíp yalanğî evliyalarîñ kóterílgeníne 
+karap turaman, idarege șîkkanlarîna 
+kurnazlîknî yúkseltkenleríne 
+aldatîp kazanganlarîna 
+kalîknî bír koșkar túrtep dagîtîr 
+temír tíșlí hem konîșkan múyúzlí 
+Ganj suwîñ kenarîndan 
+kóșebeleríñ umutî man beklemesí 
+kúnbatarga, sîrtka, kúntuwarga 
+hem kúneș betke itep awnî koparağak bolîp 
+ğañî tírílíș 
+heșbír yerníñ obír yagînda 
+ólgenleríñ ólgenín eșítemen 
+tereklerde îñgîragan meleklerní
+sîzîksîz beden tením 
+hem pîșak, hem koruwğî kolnî tuyar 
+hayatîmnî bírtamam túkete. 
+ğañî korkînîș dalgalarî kóteríle. 
+kitap așîktîr 
+amma kóreğek kóz kayda? — Dream 
+I watch the past crushed 
+and fake saints raising to reign 
+exalt cunningness 
+envision deceit to prosper 
+a ram rigs the folk 
+with iron teeth and speaking horns 
+from the Ganga’s bank 
+immigrant hope and waiting 
+pushes west-, north-, east-
+and southward for net-breaking 
+new resurrection 
+beyond the brimming nowhere 
+I hear the dead dying 
+angels groaning in the trees
+my unscarred body 
+feels the hand that’s blade and guard 
+ends me like full-stop. 
+new waves of terror rise high. 
+the book is opened 
+but where is the seeing eye?
+# Amma eger… 
+Tóze-almadan 
+kadmiy ğúkní míyínímde 
+yukîmda ğîlap ğoklarman 
+șalt kelíp ketken bít mísapír kibí 
+mením kîșkîruwlarîm onî ráhátsíz ete eken 
+o koñșî bólmede de yuklay almay eken 
+mením Allahtan yardîm tílemem kerek 
+yakșî-yakșî yuklatmaga 
+șo yalanğî siyarrelerní analarîñ-babalarîñ 
+aș ruhlarî úșún 
+o mením akîlîmnî yaman kóre eken 
+korkînș filmler… ğoldan șîkkan 
+çakralar maga bútún keșe kórsete 
+men onîñ baskîsîna teslím bolîp 
+olarga puja teklif ettíremen 
+tîñîșsîz ruhlarîna ğem beríp 
+amma eger kalgan șópler 
+bírewge yaramadan ulup tursa? — But What If… 
+Unable to endure 
+ancient load on the psyche 
+I cry in sleep that visits 
+like a guest and leaves fast 
+she says my loudness disturbs 
+she can’t sleep even in next room 
+I must seek divine relief 
+to sleep well pacify 
+the rogue planets for hungry 
+spirits of the forefathers 
+she says she hates my mind 
+horror movies…deviant 
+chakras make me see the whole night 
+I yield to her pressure 
+and let them offer puja 
+to feed the restless spirits 
+but what if the left-overs 
+keep howling and help none?
+# Bírew kórmegen 
+Bek kóp súrgen ómír 
+ómír men de fazla ómírler 
+hem fazla ólímler 
+amma belgílenmegen 
+yuklagan tañlar 
+yañgîz karañgîlar 
+hem kara ruhlar 
+kaytîp ketmesín unutîp 
+mením hayat bólmemden 
+ke șúndí bírew kórmegen 
+túșlerge tolîp kalgan — No One Sees 
+So much lived life 
+more life with life 
+and so much of death 
+yet undefined 
+sleeping dawn 
+lonely dusk 
+and dark spirits 
+forgetting to leave 
+my living room 
+now cluttered with dreams 
+no one sees
+# Kuğurlî sînğîrlar 
+Olar zaman-zaman dîkkatîmîznî bașka yerge ğónetken 
+ğañî yalanlar oylap tabalar 
+dogrîlîk șúphege túșkenșík yalanlarnî tekrarlaylar 
+ewlatlarga yamanlîknîñ ğollarîn úyreteler 
+ayîruw pornografiyasîn úyreteler, bólíp, óltíríp 
+koñșîlarîn súygen sábiylerní 
+turlílíkke, tegízlíkke hem tîñîșlîkka kîymet bergenlerní 
+tamîrlarnî óstírgen 
+kúneșní kapatkan kuğurlî șînğîrlarnî kabletmegenlerní — Weird Chains 
+They invent new lies with periodical distractions 
+repeat falsehood till truth turns doubtful 
+teach generations the ways of hating 
+the porn of discriminating, dividing, killing 
+the innocents that love their neighbours 
+value diversity, equality and peace 
+shunning the weird chains that lock the sun for roots to 
+grow
+# Tolkînlarnîñ arasînda 
+Kalabalîk lift men katnîñ boșlîgî 
+maga ketkenlerní tíríltmege yardîm etmiy 
+keșeníñ keș máhálínde, alsuw káátke umut ğîyîp 
+yașayğak ğañî bír hayat oylap tapmaga tîrîșîp 
+kólekelí ğarîkta ğeteklengen awuruwga tutîlîrman 
+ekí kíșílík ğatak, boș ayna hem bozarîp kalgan tañ 
+yastîk awurta, suret mîskîllay, duwalar yardîm etmiy, 
+íșímde kîșkîruw, hayaliy tînîșlîk, 
+hem túkenmez karañgîlîk. Men Odisew tuwulman 
+amma tolkînlarnîñ arasînda adașîp turaman — Against the Waves 
+The crowded lift and emptiness of the flat 
+doesn’t help me resurrect what’s gone 
+late at night, I may pile hopes on a pale paper 
+trying to invent a new life to live with 
+end up seized by pain trolling in the shady light 
+double bed, blank mirror, and still greyer dawn 
+the pillow hurts, the image derides, the prayers fail 
+the roaring inside, the ghostly silence 
+and unfading darkness. I’m no Odysseus 
+but keep straying against the waves
 `;
