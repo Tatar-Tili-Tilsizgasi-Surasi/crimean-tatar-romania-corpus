ec3a8b7c52da3e12d31266eeab975fc607d7f641
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-17 16:43:15 +0200

    Update The_Sounds_of_Tatar_Spoken_in_Romania.ts

--- data/Taner_Murat/The_Sounds_of_Tatar_Spoken_in_Romania.ts
+++ data/Taner_Murat/The_Sounds_of_Tatar_Spoken_in_Romania.ts
@@ -5,4 +5,18 @@ export const theSoundsOfTatarSpokenInRomaniaEntries: string = `
 # Awaz tertíbí — The harmonic hierarchy principle
 # Karşî turgan deñge — The harmonic parallelism principle
 # Belgílí buwunlî irtibat — The articulatory determinism principle
+# Ses zewukî — The musicality principle
+# Eñ az zahmetlí telaffuz — The least effort principle
+# Ótíş tertíbí — The sonority sequencing principle
+# Eñ az zahmetlí eğíkleme — The least effort of sylabification principle
+# Eğík íşí ses uyîmî — The Khwarezmian syllable and the law of intrasyllabic harmony
+# Eğík tíşí ses uyîmî — The golden nature of the Khwarezmian syllable and the law of intersyllabic harmony
+# Şîñlamama — Dissonance
+# Kagîşma — Cacophony
+# Múrekkep talaw — Complex syllable onset
+# Múrekkep kuyruk — Complex coda clusters
+# Alákasîzlîk — Hiatus
+# Alîştîruw — Naturalization
+# Ses kîstîruwî — Final devoicing
+# Ses arttîruwî — Voicing
 `;
