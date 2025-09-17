291f3416f68c5cf57108c5f36a90c1d010218ad0
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-17 11:22:13 +0200

    Update The_Sounds_of_Tatar_Spoken_in_Romania.ts

--- data/Taner_Murat/The_Sounds_of_Tatar_Spoken_in_Romania.ts
+++ data/Taner_Murat/The_Sounds_of_Tatar_Spoken_in_Romania.ts
@@ -1,3 +1,8 @@
 export const theSoundsofTatarSpokeninRomaniaEntries: string = `
 # Edebiy Tatarğa — The literary Tatar spoken in Romania
+# Tírkeleşúw — The agglutinative nature of Tatar spoken in Romania
+# Başkayideler — Phonetic and phonotactic principles
+# Awaz tertíbí — The harmonic hierarchy principle
+# Karşî turgan deñge — The harmonic parallelism principle
+# Belgílí buwunlî irtibat — The articulatory determinism principle
 `;
