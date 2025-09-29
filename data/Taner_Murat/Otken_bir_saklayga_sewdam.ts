50b81bc2e8ae894b7cbffa482e772082da012835
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-09-29 21:56:35 +0200

    Update Otken_bir_saklayga_sewdam.ts

--- data/Taner_Murat/Otken_bir_saklayga_sewdam.ts
+++ data/Taner_Murat/Otken_bir_saklayga_sewdam.ts
@@ -1,2 +1,470 @@
 export const otkenBirSaklaygaSewdamEntries: string = `
+# Bír şaklay kabîp ğuttum
+Bírgún
+ğazîñ-gúní gálíba
+óz azbarîmda
+otîrîp úyúm aldînda
+okîp renklí meğmuwamnî
+ázír ertengí kaweme
+aşayatîrganda yemegímní
+beşínğí sayifaga
+kózlerímní
+soganday patlatkanîmda
+bír parşa
+pener alağak boldîm
+bír parşa
+sak bolmadîm
+şanagîñ kenarîna
+kelíp kongan
+bír şaklay kabîp ğuttum.
+
+Ay-way
+bír şaklay
+o da ótken terakay
+ne yaptîm men
+degenşík
+n-íşledím men
+degenşík
+sak bolgaydîm
+degenşík
+endí ka-termen
+degenşík
+tomalanîp şo şaklay
+bír ğutum ewel sarîmay
+man maylangan bogazîmnî
+kullanîp taydîraktay 
+tayîp kettí
+karînîma. 
+
+Kórdíñ mí kaday?
+Yaramaz şaklay
+Wahşiy kagay
+Moynîm baylay
+Ózí ótken kuşka uşay.
+
+N-íşliyím endí?
+Ka-ttí ettí
+íşliyğegín íşledí
+maksatîna ğettí
+kíríp íşíme yerleştí.
+
+Ka-ttí yaptî
+ózín attî
+íşíme battî
+yerín taptî
+raátím sattî.
+
+ 
+Endí men
+ne kadar
+beş para bermiyím
+íşíme karay-beriyím
+yakîşmaz mením yaşîmda
+bír erkekke
+desem de
+bolmay.
+
+Ağîmasîz şaklay
+sáde íşíme
+kírgení men
+nokta koymay.
+
+Merhametsíz kagay
+sáde karînîma
+konganî man
+toymay.
+ 
+Hergún, her sáát
+Tapkanda fîrsat
+Koşaksîz sebat
+Boşîna lugat
+Tabîlmaz neğat
+Túmşugî alat
+O kaza bír hat
+O aşa nurbat.
+
+Burgulap ğúregímní
+bír teşík aşa
+bír teşík aşa.
+Yaslanîp góñílíme
+raát yuwalana
+raát yuwalana.
+ 
+Yemegíme sak bolmay
+Kabîp ğuttum bír şaklay.
+Góñílíme kol salîp
+Şokîy-şokîy parşalay
+Ğúregím men egleníp
+Ístegeníndiy oynay
+Kózín ğumup yuklasa
+Kanat astînda saklay
+Ğawun ğawsa íşíne
+Yerleşe de yuwalay.
+# Şaklayîñ yîkpalsîzlîgî
+Bír yîkpalsîzdîr
+bo ótken şaklay.
+Kím men ómírínde
+sayragan bolsa da
+illa bír yerlerden
+şîgîp bírşiyler
+ya bazî sebepler 
+ílíşkísí yaman hálde
+soñîşlangandîr.
+
+Síptí sayraganda
+Jim Morrison'nîñ sesín súyúp
+onîñ artîndan sayrap ğúrgen.
+The Doors man Jim
+kaysî yerge barsa
+o da onda edí
+artîndan o da
+heş kalmaz edí.
+ 
+Hoşlanîp Jim de onîñ ótíşínden, oga
+úyretken kóp şiyler.
+"Ózíñní atmalîsîñ
+eñ balaban
+eñ zor
+eñ korkînş
+eñ şeşílmez
+korkîlarîñ arasîna.
+Alîşîp kalganda
+o balaban korkîlarga 
+başlaganda korkmamaga
+kutulağaksîñ
+azatlîk korkîsîndan da.
+Mína, bodîr azatlîk!"
+dep añlata edí
+Jim oga
+onîñ ótíşínden hoşlanîp.
+
+Bírgún
+Paris'te ekende
+ótíp pítírgen soñ
+egleníp bír kenarda
+sokaklarîñ barînda
+ğalbarîp Jim şaklayga
+"Maga da
+wakît-wakît
+zaman-zaman
+bírew men paylaşmayğak
+bírkaş dakka kerektír"
+dep aşîp kapîsîn
+abdestkanaga kírgen.
+
+Terí pantolanlî
+Kertenkele Tañrî
+bírtaa aşîp kapînî
+şîkmagan eken.
+ 
+Şaklay soñra
+Elvis Presley'níñ kîywaldawuna
+ğúrek baylap ğúrgen
+amma maalesef
+taa da ogîrsîz
+taa da yîkpalsîz şîkkan.
+Hakkîykatîn Elvis oga
+bazda-bír
+kiyípí yerínde bolsa
+"There will be peace
+in the valley"
+dúrkúsún kîywaldar edí.
+Amma torasî şo ke
+şondan başka
+Elvis oga
+fazla zaman
+ayîrmaz edí.
+
+Elvis Presley
+hewesín taa bek
+şelpeklerden-tatlîlardan
+almaga
+alîşîp kalgan edí.
+Bonîñ úşún
+zamanîñ kóbísín
+şekerler úşún
+tatlîlar úşún
+ayîrar edí
+zamanîñ kóbísín
+onlarîñ arasînda
+geşírer edí.
+Şonday etíp
+1977 senesíñ
+awustos ayînda
+bír şelpegíñ ústúne
+fazla şeker kaşîrganda
+Kral Elvis te
+taşlap ğañgîz şaklaynî
+ketíp bírtaa kaytmadî.
+ 
+John Lennon man
+şaklay tabîşkanda
+ekewúñ de
+bútún hayatî deñíştí.
+John oga
+aşîk kózí men
+kóríntíler kórmege
+úyretmege başladî.
+O da
+bonday dersler alganîna
+bek kopayîr edí.
+Bírí-bírísín kayet
+sayar edíler.
+Ayîrîlmadan sáde
+koltîklaşîp óter edíler.
+Biñ bereket
+bar eken dúniyada adalet.
+ 
+Amma kîzganşlîk
+tuwul mî, saga?
+Torasîn aytağak bolsak
+John Lennon man
+bolgan íşín-ílíşkísín
+kópten pítírgen
+Yoko Ono
+"Men súyúp o kadar onî
+men kolîñ tersí men
+iteldím kaldîm"
+dep ğîlanîp
+Mark David Chapman atînda
+bír şerefsízní ódep
+1980-de
+aralîk ayînda
+John'nî kurşunlap
+ótíşín kesken eken.
+ 
+Bondan soñ
+dargîn-dargîn
+kaswetlí-kaswetlí
+umutsuz-umutsuz
+sîrtîn tayatmaga
+yerí kalmagan şaklay
+bír kaplîmbakaday
+tartîlîp óz kabîgîna 
+uzun bír súre
+terk etkendiy dúniyanî
+onday etíp yaşadî.
+Kóríp sáde bazî úy íşlerín 
+kúnnúñ kóbísín
+úyúñ íşínde
+geşírer edí.
+Bo turum kíríp hayatîna
+Freddy Mercury
+hayatî aydînlanmaga
+başlaganşîk súrdí.
+ 
+Freddy men de
+herşiy baştan alîndî.
+Şaklay Freddy'ge
+Freddy demez edí
+Faruk der edí.
+Turmadan
+kalk Faruk'nî
+yersíz yamanlaganîn aytîp
+şaklay Faruk'ka
+toz kondîrmaz edí.
+Bírgún oga
+sayrap Faruk
+"I want to be free"
+şîgîp kastanege bargan.
+Bírtaa kastaneden kaytmagan.
+Şaklay taa da ğañgîz-ğañgîz
+koşaksîz bír kuş kalgan.
+Yîkpalsîz bír hayat
+yîkpalsîz bír şaklay.  
+ 
+Yîkpalsîzman dep şaklay
+Keşe-kúndúz bakîrîklay
+Tuzlî balîk uzattîm
+Heş kabletmiy, heş almay
+Tírí balîk ta berdím
+Tumşugun bírem salmay.
+
+- Ğok bolsîn bonday yîkpal
+Yemek tuwuldur dertím
+Mení bírew añlamaz
+Maga sayramak lázîm.
+# Şaklay blue-jeans kíygende
+Ótken şaklayîm man
+búgún akşam
+kíyíp blue-jeans-lerímízní
+şîgağakmîz kasaba íşíne.
+Aytkan edím mí?
+Şaklay blue-jeans kíygende
+blue-jeans oga
+bek yakîşa.
+Kazana ayîrî bír hawa
+bír azatlîk
+bír ózbaşlîk.
+Şîgağakmîz heş tanîlmagan
+bír ortamga.
+Kenardakî bír mallede
+tabîp kíşkene
+bír kîrşîma
+ğaşînmaga álemínde.
+ 
+Búgún akşam
+kíyíp blue-jeans-lerímízní
+delí kíşílerdiy
+ğanîmîz ne ístese
+onî yapağakmîz.
+
+Íşíp şarap
+kade artîndan kade
+dumanlatîrmîz sígara
+íşe-íşe
+aylandîrtîrmîz baş-kafa.
+
+Úş aylîk kozîlarday
+otlakka şîkkanda
+oynarmîz bíz de sekíre-sekíre
+şo kíşkene
+kîrşîmanîñ ortasînda.
+ 
+Soñra
+adam-akîllî terlegen soñ
+batkanda kafalarîmîz
+adam-akîllî dumanga
+bírleştírírmíz ğúrek
+takîldamalarîmîznî
+beríp kulak-kulakka
+basîp kókírek-kókírekke.
+
+Búgún akşam
+şaklayîmnî óbeğek bolaman
+şo yabanğîlîk
+dúniyanîñ ortasînda
+şo eğnebiyler arasînda.
+ 
+Ak-Murun'da bír şaklay
+Ğígerímní tîrnaklay
+Mením ğanîm ateşte
+Ağînîp bír suw atmay
+Men ğanîp kúl bolsam da
+Aylanîp ta karamay.
+
+- Kollarîm arasîna
+Kel şaklay, kel yaramaz
+Wak sabiy bír óbúwden
+Karlîgaş bírem kaşmaz.
+# Şaklay oyîn miydanînda
+"Ğanîm ótken şaklayîm
+búgún akşamga
+kîzîl entaríñní
+kíyseñ bolîr mî?
+Neşín deseñ
+sen kîzîl renkke
+baylîk beresíñ.
+Kíyersíñ ğañî rugan
+ayakkabîlarîñnî da.
+Neşín deseñ
+sen ruganga
+kózní algan
+ğîltîrîmlar beresíñ.
+Sen ústúñe ne kíyseñ
+beríp oga bír ústúnlúk
+ózíñ ústúnlúgúñ
+astîna parmak basasîñ."
+ 
+Aytmadan geşmiyím
+şaklayga kóre
+ókşelí ayakkabî
+hayatîñ eñ balaban
+heweslerínden sayîlîr. 
+Ótken şaklay
+"Ókşelí ayakkabî
+kîskaayaklînîñ bağagîn
+gúzelleştíríp ğúrúşúne
+letafet katar"
+dep aytar
+onîñ fíkírí bodîr.
+"Şaklay, saga da
+aytkan edím mí?
+Akşamga oyîn
+miydanî ázír
+şîgayîk şo miydanga."
+ 
+"Oyîn miydanîñ ortasîna şîkmaganşîk
+kalk bízge baş kaytarmaz
+kalk bízge kol şarpîldatmaz."
+
+"Eger oynagan oyînîña gúwenseñ
+mína, sáde añlap oynaganlarga
+awur hawa okîyğak
+buyur miydanga.
+Oynamak tírílík
+oynamamak kastalîk."
+
+"Ğañî ayakkabîlarîñ
+ayagîn sîga mî?
+Belletmemege şalîş
+arabamda alîp keldím saga pápíş."
+
+"Ótken şaklayîm
+kúlúmsúrep oynaganda
+bek ğazibelísíñ."
+ 
+Ótken yeşílkóz şaklay
+Moynîm sarîp kuşaklay
+Boş ber dep ğalbarsam da
+Ekí kolî boşamay
+Súymem sení desem de
+Sîkkanî heş ğîmşamay.
+
+- Ğabîştîñ túşúnğeme
+Uşup ket, ğîrtuwğî kuş
+Sende merhamet bolmaz
+Tuygî awğusî baykuş.
+# Ótken şaklayîñ asîl atî
+Ótken şaklayîñ asîl atî
+Şaklay tuwul.
+Men oga
+íşímní-bawurumnî parşalagan
+kuwatlî bír tumşugî bolganî úşún 
+Şaklay dep aytîp alîşîp kaldîm. 
+Yoksam
+menden atîn sorasañîz
+men onîñ atîn bírem bílmem.
+Çúnkí bíz
+heşbírzaman bír-bírímízden
+asîl atîmîznî soramadîk.
+Çúnkí bíz
+heşbírzaman bír-bírímízden
+asîl kím ekenímízní soramadîk.
+
+Men sáde onday alîşîp kaldîm.
+Men oga onday aytaman.
+O mením úşún  Şaklay
+O mením úşún Ótken Şaklay.
+ 
+Ótken Şaklay
+ortalîkka súygí bagîşlar.
+
+O onîştan tuwgan
+súygí bagîşlamaga
+súygúnúñ ózegí
+bolmak úşún tuwgan.
+
+O onîştan tuwgan
+bo dúniyanî
+ózín gúzellígí men
+ózín árúwlígí men
+ózín ğuwaşlîgî man
+ózín inğelígí men
+ózín letafetí men
+kuşaklap almak úşún tuwgan.
+
+O onîştan tuwgan.
+ 
+Men men dost bolgan şaklay
+Akîlîmdan heş taymay
+Inanîlmaz bír masal
+Mením atîm tanîmay
+Omîzîma baş salîp 
+Ózínkísín de saklay.
+
+- Insanulî kolîñnî
+Ber uşayîk taşlîkka
+Saray kurup merğandan
+Yaşatayîk adem-ğayka.
 `;
