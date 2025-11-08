95addb220a8e1ce4dd835a2472d3b10a3971a77f
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-08 18:58:10 +0100

    Add February 2011 entries for Crimean Tatars

--- data/Nazar_Look/Subat_2011.ts
+++ data/Nazar_Look/Subat_2011.ts
@@ -1,5 +1,557 @@
 
 export const subat2011Entries: string = `
+# TOMRÎĞA KÎRÎM TATARLARÎÑ TURUŞ-MAMURIYET MEĞMUWASÎ — ATTITUDE AND CULTURE MAGAZINE OF DOBRUDJA’S CRIMEAN TATARS
+# Şubat — February
+# Sene — Year
+# Sayî — Issue
+# ŞÎNĞA'NÎÑ túş kórmek SÎRASÎ TANÎLSÎN — LET XINJIANG dream
+# BAŞ KABÎMÎZDA: Hanna Turonek — ON THE COVER: Hanna Turonek
+# MEMBALAR: Aydîn Şemi-zade
+Borasan Ahmet (Borasan Ametov)
+Ioana Grecu, dreamstime.com
+Jon Helgason, dreamstime.com
+Tomasz Swiadczynski
+QHA
+Vasiliy Koval, dreamstime.com
+Zoï Environment Network — CONTRIBUTORS: Aydîn Şemi-zade
+Borasan Ahmet (Borasan Ametov)
+Ioana Grecu, dreamstime.com
+Jon Helgason, dreamstime.com
+Tomasz Swiadczynski
+QHA
+Vasiliy Koval, dreamstime.com
+Zoï Environment Network
+# ÍŞÍNDEKÍLER — CONTENTS
+# Tomrîğa Kîrîm Tatarlarîñ turuş-mamuriyet meğmuwasî — Attitude and culture magazine of Dobrudja’s Crimean Tatars
+# BAŞ-NAŞIR: Taner Murat — FOUNDER & EDITOR-IN-CHIEF: Taner Murat
+# SAYAR SÎZGAĞÎSÎ: Elif Abdul
+Hakaan Kalila (Hakan Calila) — COMPUTER GRAPHICS: Elif Abdul
+Hakaan Kalila (Hakan Calila)
+# ESER KEÑEŞÇÍSÍ: Gúner Akmolla — CREATIVE CONSULTANTS: Gúner Akmolla
+# Mamuriyet kaberlerí
+Ğenevre, 15.01.2011 - Ğenevre 
+Imaginaid Galerisinde 4 şubat 2011'de helwetiyelí 
+resímğí we gázeteğí Alban Kakulya'nîñ "Taking 
+Crimea" ("Kîrîmnî almak") atlî resím sergísí 
+aşîlağak. 
+Sergí Alban Kakulya'nîñ Kîrîmga yapkan 
+ğolşîlîgînda alîp toplagan resímlerínden ibarettír 
+we Zoï Environment Network yagîndan 
+tertíplengendír.
+Sergí 12 martkaşîk kalkka aşîk 
+kalağaktîr.
+ 
+Ankara, 07.01.2011 (QHA) - Kîrîm 
+Túrklerí we Yardîmlaşma Dernegí'ñ 
+tertíplemesínde aytuwlî Kîrîm kalk oyînlarî we 
+şarkîlarî takîmî Kîrîm Ansamblî koşaksîz 
+programî "Kîrîm Tatar Múzik we Dans Keşesí"ñ 
+síptí kósteríşínde Ankara'da, Milliy Egítím Şura 
+Salonînda kalknî ayakka tursatîp alkîşlattî. 
+Kîrîm Ansamblî Grubî Bayrak, Kîrîmlî 
+Ğígítler, Tîm Tîm, Bakçesaray Kaytarmasî we 
+Kîna Ğakma bír-bírínden gúzel oyînlarî man, 
+Rîza Yusuf ezberlegen manzúmelerí men, Server 
+Kakura ğîrlarî man, Dilaver Osman, Asiye Sale 
+bír de Emine Mustafa dúrkúlerí men, şarkîlarî 
+man tolî-tolî salonnî kúyğanlandîrdîlar. 
+Aşadakî programîna kóre oşak ayînda 
+Kîrîm Ansamblî Túrkiye'ní dolaşîp kósteríşleríne 
+dewam ettí: 7-8 oşak Ankara, 11 oşak Ğeyhan, 13 
+oşak Nevşehir, 15 oşak Konya, 16 oşak Polatlî, 19 
+oşak Kîrîkkale, 21 oşak Tekirdak, 22 oşak 
+Koğaeli, 23 oşak Gebze, 25 oşak Bandîrma, 28 
+oşak Izmir, 29 oşak Istanbul.
+ 
+Akmesğit (Simferopol) 12.01.2011 
+(QHA) - Nesíl dergísí, Akmesğit’íñ Ukrayna 
+Múzikal Tiyatrosînda tertíplegen tórení men 2010 
+yîlînda túrlí-túrlí alanlarda başaruwlî bolgan 
+Kîrîm Tatarlarîna ódíller dagîttî.
+Program sîrasînda SMS ğolî man 5109 
+numarasîna ğíberílgen oylarîna kóre bellí bolgan 
+Yîlîñ Kariyerí, Yîlîñ Tiğariy Markasî, "Nesíl" Bala 
+Fotografî Yarîşmasî, Yîlîñ Koşagî, Yîlîñ Kelín 
+Entárísí alanlarînda kazanganlarga ódíller 
+dagîtîldî.
+Yîlîñ Kariyerí alanînda Kîrîm Tatar Milliy 
+Meğlisí I-ínğí Başkan Yardîmğîsî Refat Çubar 
+(Refat Çubarov), Yîlîñ Markasî alanînda “Vega” 
+şirketí başkanî Erlan Ablaziz (Erlan Ablazizov), 
+“Nesíl” Fotograf Yarîşmasî alanînda bír yaşîndakî 
+Yakup ísmínde bala kazanîp şîktî.
+Kîrîm Ózerk Ğumhuriyetí Súrgúnden 
+Kaytkan Vatandaşlar we Etnik Ílíşkíler Komitesí 
+Başkanî Edem Dudak'îñ (Edem Dudakov) maddiy 
+destegí men yapîlgan program kapsamînda ateş 
+kósteríşí we konser yapîldî. Konserde, Ğemile 
+Ansamblî, Elzara Batal (Elzara Batalova), Hayat 
+Ansamblî, Dilara Mahmut (Dilara Mahmudova), Zera 
+Kağîkay (Zera Kağîkayeva), Ernes Pîrtîh (Ernes 
+Pîrtîhov), Sevil Memet (Sevil Memetova), Ğaşlîk 
+Ansamblî, Aliye, DJ Bebek, Nariman Baliç kibí 
+aytuwlî Kîrîm Tatar sanatşîlarî sahne aldî.
+Kîrîm Ózerk Ğumhuriyetí Kaberleşme 
+Komitesí Başkan Yardîmğîsî Şevket Memet (Şevket 
+Memetov) we Kîrîm Múhendislík we Pedagojiy 
+Úniversitesí Rektorî Prof. Dr. Fevziy Yakup (Fevzi 
+Yakubov) kaberleşme destegí sawladîlar, Kîrîm 
+milletvekillerí Enver Abdurrahim (Enver Abduraimov) 
+ve Remziy Iliyas (Remzi İlyasov), programîñ reklam 
+sponsorlarî boldîlar. Program sunuğulugun Kîrîm 
+Tatar Akademik, Múzikal we Drama Tiyatrosî 
+artistlerí Ellina Mustafa (Ellina Mustafayeva) we 
+Eskender Islam (Eskender Islamov) yaptîlar.
+Nara Nariman (Nara Narimanova) – Osman Bayramaliy 
+(Osman Bayramaliyev)
+ 
+Kóstenğe, 15.01.2011 - Kóstenğe 
+Yazarlar Bírlígí kazak şayirí Mihay Eminesku'nî 
+(Mihai Eminescu) añgan 
+bír şiir kúní 
+tertípledí. 
+Programga tatar 
+yazuwğîlarîmîz da dawetiye 
+alîp koşîldîlar. 
+Eminesku'nuñ eserlerínden 
+tatarğa kaytarmasîndan 
+Gúner Akmolla we túrúkşe 
+kaytarmasîndan Nermin 
+Hoğa-Susam okîp toplantîga ayîrî bír renk 
+berdíler. 
+Akmesğit, 19.01.2011 (QHA) - Akmesğit 
+Etnografiy Múzesínde Kîrîm Ózerk Ğumhuriyetíñ 
+kuruluşuñ 20-ínğí yîlkaytîmînda “Kóp îrklî 
+Kîrîm” atlî sergísí aşîldî.
+Kîrîm Ózerk Ğumhuriyetí, 20 oşak 1991 
+yapîlgan ilan we Kîrîm'îñ ózerlígín belletken 
+referendum soñîşlarî esasînda Ukrayna Parlamentosîn (O kaytîmda Ukrayna Soviyet 
+Sosyalist Ğumhuriyetí Parlamentosî) 12 şubat 
+1991 tewúkesínde algan “Kîrîm Ózerk Soviyet 
+Sosyalist Ğumhuriyetín Kaytarîp Akelme” kararî 
+man kurulgan edí.
+Kîrîm Ózerk Ğumhuriyetí Súrgúnden 
+Kaytkan Vatandaşlar we Etnik Ílíşkíler Komitesín 
+katkîları man tertíplengen sergíge sóz konîsî 
+komiteníñ başkan yardîmğîsî Emine Avamil (Emine 
+Avamileva), múze múdúrí Yuriy Laptev, Etnik Uyum 
+Teşkilatî Başkanî Vladimir Kazarin, Kîrîm 
+Almanlar Dernegí Başkanî Yuriy Gempel 
+katîldîlar.
+Sergíde, múzede saklangan we Kîrîm’da 
+yaşagan kalklarîñ kúndelík hayatîndan karelerin 
+yer algan fotograflar, Kîrîm Ózerk Ğumhuriyetí’ñ 
+simbollarî we başaruwlî Kîrîm sakinlerine 
+berílgen Kîrîm Ózerk Ğumhuriyetí’ñ rozetlerí yer 
+aldî.
+Kîrîm Ózerk Ğumhuriyetí’ñ kuruluşuñ 20-
+ínğí yîlkaytîmîna yapîlgan programlar, Kîrîm 
+Parlamentosîñ 27 aralîk 2010 tewúkesínde alînîp 
+588 numaralî kararîna kóre yapîla.
+Nara nariman (Nara Narimanova) – Osman Bayramaliy 
+(Osman Bayramaliyev)
+ 
+Istanbul, 12.01.2011 - Yrd. Doç. Dr. 
+Nedim Bakîrğî’nîñ Kómen Yayînlarîndan şîkkan 
+"Kîrîm Tatar Masallarî" atlî kitabî Túrúk 
+okuwğlarî man tabîşa.
+Eserde sayîlgan 50 Kîrîm Tatar masalîn 
+látin hárflerí (Túrkiye Túrkçesí elifbesí) we Túrkiye 
+Túrkçesíne aktarîmî tabîla. Bondan ayîrî kitabîñ 
+başînda masallarîñ inğeleme bólímí mevğut.
+2010 senesíñ soñînda ğayînlangan 554 
+sayifalîk karton kapaklî kitap internet 
+kitapşîlarînda satîlmaga başladî.
+ 
+Kezlew (Yevpatoriya), 11.01.2011 
+(QHA) – Kezlew’de ballarîñ şekken resímlerí 
+“Mením Ukraynam” we “Kîrîm-Mením Gúzel 
+Yerím” konîlî sergísínde yer aldî.
+Kezlew Belediyesíñ bergen bílgísíne kóre sergíde, 
+kasabada tabîlgan bútún mekteplerín we 
+Rovesnik (Yaşît) Bala Sanat Merkezíñ Pozitif 
+Stúdiyosî úyrenğíleríñ yapkan manzara we 
+serbest konîlî fotograflar yer aldî. Sergíden ewel 
+ğaş fotografşîlar juriyge yúzlerğe fotograf ğíberdí. 
+Juriy, bonlarîñ arasîndan eñ árúwlerín ayîrîp 
+sergíge saldî. 
+Juriyníñ kararî man sayîlağak 
+kazanganlarga túrlí ódíller beríleğek, kazanganlar 
+“Bízím Ukrayna” atlî Ukrayna resím sergísíne 
+katîlmaga hakk eteğekler. 27 aralîkta başlagan we 
+14 oşakka kadar dewam eteğek sergíníñ sponsorî 
+Blik Fotograf Merkezí boldî.
+Çingiz Súliyman (Çingiz Súleymanova) - Osman 
+Bayramaliy (Osman Bayramaliyev)
+Ankara, 20.01.2011 (QHA) - Kîrîm 
+Túrklerí we Yardîmlaşma Dernegí'ñ 
+tertíplemesínde 20 oşak 2011 perşembe kúní 
+“Altîndak Belediyesí Hamamóní sóyleşilerí – Oşak 
+2011” programı íşínde “Kîrîm Tatar Şiir ve 
+Edebiyat Kúní” yer aldî.
+Programda destanlarî, manilerí, ğîrlarî, 
+gazellerí, koşmalarî, hikayelerí, romanlarî man 
+Kîrîm Tatar Edebiyatî anlatîldî.
+Program Altîndak Belediyesí Tarihiy 
+Kabakşî Konagînda yapîldî.
+ 
+Akmesğit (Simferopol), 13.01.2011 
+(QHA) – Kîrîm Ressamlar Úyúnde 12 oşakta 
+ballarîñ yapkan resímlerí yer algan bír sergí 
+aşîldî. Sergí, Akmesğit Sanat Mektebíñ 
+kuruluşuñ 65-ínğí yîlkaytîmî múnasebetí men 
+tertíplendí. Mektebíñ sergísínde túrlí yîllarda bo 
+okîlda okîgan ballarîñ yapkan 50x70 çm ebatînda 
+114 adet we 50x40 çm ebatînda 12 resím yer aldî. 
+Sergíge, Ukrayna Ğumhurbaşkanî Dayimiy Kîrîm 
+Temsílğísí Vekílí Viktor Plakida katîldî. Sergí, 26 
+oşakka kadar dewam eteğek.
+1944 yîlînda kurulgan mektepke şo anda 
+350 bala okîy. Okîlda egítím kórgen úyrenğíleríñ 
+eserlerí Hindistan, Kúba, Fas, Sudan, Súriye, 
+Ğezayir, Afganîstan, Kamboğiya, Yugoslaviye, 
+Danimarka, Helwetiye, Hollanda, Breziliye, 
+Kanada gibi dúniyanîn bírkóp ulkasînda 
+sergílengen edí.
+Evelina Iliyas (Evelina Ilyasova) - Osman Bayramaliy 
+(Osman Bayramaliyev)
+# Eşmîrza Oysullî
+Eşmîrza kart 1803 senesí Kerç rayonîñ Oysul kóyínde, 
+pîkare korantasînda tuwa. Fakîr ómír geşírgen bo istidatlî 
+keday kóy-kóyden ğúrúp, ózí uydurgan ğîrlarnî ğîrlay. Onîñ 
+ğîrlarî çarlîkka, eziyetke karşî dogrîltîlgandîr.
+Eşmîrzanîñ iğadî yazuwlî tuwul da, awuzdan-awuzga 
+awuşuw netiğesínde kalk arasînda keñíş darkagandîr. Onîñ 
+yîrlarî Kîrîmnîñ şól taraflarînda we Kerç adasînda yaşagan 
+Kîrîm Tatarlar arasînda saklanîp kaldîlar.
+Eşmîrza kartnîñ kózí kórmiy edí. Onî elínden ğetep 
+ğúrgenler. Onday aliyğenap insanlardan bírsí Kîrîmnîñ 
+Argîmak-Elí kóyínden Kulmambet Bekmambet bolîp, soñîndan 
+onîñ ózí de bellí folklorğî, masal aytuwğî bolîp yetíştí. Eşmîrza 
+kartnîñ ekseriy şiirlerí onîñ awuzundan yazîlîp alîngandîr.
+Ğenkten ewel Eşmîrza kedaynîñ yaratîğîlîgî man A.S. 
+Puşkin atîna Kîrîm Tatar tílí we edebiyatî ilmiy-tedkikat 
+institutî ograştî. 1940 senesí institut folklorğî Bakî Gaziyevníñ 
+"Kalk şayirí Eşmîrza" atlî kitabîn neşir ettí. Bo kitapka Eşmîrza 
+kart hakkînda makaleler men barabar, kedaynîñ şiirlerínden de 
+kírsetíldí.
+Hiğrette neşir etílgen "Edebiyat krestomasiyasî" 
+(Taşkent, 1971) kitabîna Eşmîrza kartnîñ "Soldatlîk hakkînda" 
+serlevalî şiirí kírsetíldí.
+Eşmîrza kart 1883 senesí vefat etken.
+
+Oñma-pítme, Karaşay, 
+kóp yaşama sen, múftí!
+ 
+Kîrîmnîñ başî kóp tuwul
+Bolsa-bolsa beş mîrza.
+Bútún Kîrîm atîndan
+Olar şegerler imza.
+Kîrîmnîñ başî múftí 
+bolsa,
+Úş ğewap man kayîrdî.
+Oñmasîn da pítmesín,
+Tíríliy bízní ayîrdî.
+Eskadron úçún el koyîp,
+Bírer medal taktîñîz.
+Efendíler, azdîñîz,
+Kîrîmnîñ ğanîn ğaktîñîz.
+Ne aytmaga bílmiymen,
+Uluñ, kîzîñ alsînlar.
+Patşa bergen medalnî
+Kabíríñe salsînlar.
+Ğennetten dama isteme,
+Ğehennem úşún ázír bol.
+Kalknîñ ğanîn ğaktîñîz,
+Ğewabîna ázír bol.
+Ğamíge barsa yalan man,
+Şo yalan man kaytağak.
+Ólewuysa bo hînzîr,
+Kabrínde ne aytağak?
+Horladîñîz síz bízní,
+Oñmay-ósmiy kalîñîz.
+Şo medalnîñ ústúne
+Şápke de tílep alîñîz.
+Şápkení kíyseñ başîña
+Muratîña ğetersíñ.
+Kalknîñ hakkîn aşadîñ,
+Endí ka-típ ódersíñ?
+Şewre-çetí – gúl-bakşa,
+Limonîn alîp kokladîñ.
+Oñma-pítme, Karaşay,
+Ortalîknî bokladîñ!
+Oñma-bitme, Karaşay,
+Kóp yaşama sen, múftí!
+Yígírím bírde ğaşlarîmîz,
+Sízden sebep yer óptí.
+Ber pîştawnî kolîna,
+Ğesaret etíp at-almaz.
+Kayda barsa, o hînzîr
+Gúnasîndan ğat-almaz.
+Karaşaynî Alibey
+Arabadan túşúrttí.
+Traktirníñ íşínde
+Kótek bírem píşírttí.
+Alibeyníñ torînî
+Bastî-sóktí kózíne.
+İygí tokat wurdular
+Karaşaynîñ ózíne.
+Traktirníñ íşínde,
+Çay işkende tutuldî.
+Nasibí bar, Karaşay
+Ólmiy-ğetmiy kurtuldî.
+Haram emşek emízgen,
+Karaşaynîñ anasî.
+Alí beyim saw bolsîn
+O - Kîrîmnîñ babasî!
+Sabîr etsín Karaşay,
+Aççalmanday atlatîr.
+Ekí kózín Alibey,
+Ğumurduk man patlatîr.
+Delíğan man zewk súrúp,
+Sen dúnyada ne kórdíñ?
+Pîkarege, óksízge
+Kayîr etíp ne berdíñ?
+Gúbernator, Karaşay,
+Ekísí de bír dinde.
+Grafdayîn bír "baba"
+Kerek eken bo kúnde.
+Şo divannîñ kapîsî
+Keşe-kúndúz aşîlmay.
+Grafdayîn bír "baba"
+Bo Kîrîmga "tabîlmay".
+Graftan kalgan bír ul bar,
+O da ğaşlîk eteğek.
+O general bolganşîk,
+Basîmîzga ğeteğek.
+Eşmîrza dep añsañîz,
+Kardaşîm dep sayarman.
+Bo yîrlarnî tîñlañîz,
+Gúbernaga ğayarman.
+Bo Kîrîmnîñ babasî
+Men bellesem, beş mîrza.
+Bo yîrlarnî şîkargan,
+Oysullî sokîr Eşmîrza.
+ 
+Bo Kîrîmda kíşí yok, 
+soldatlîkka keteğek
+ 
+Bo Kîrîmda kíşí yok,
+Soldatlîkka keteğek.
+Alagaşnî kórdíñ mí?
+Başîmîzga ğeteğek.
+Sóz aytmaga bolmaysîñ,
+Kóz aldîñda daragaş.
+Boldî endí bolağak,
+Otka ğansîn alagaş.
+Ğolga şîksam, toñaman,
+Ayagîm-kolîm ğalañgaş.
+Harap ettí Kîrîmnî,
+Otka ğansîn alagaş.
+Ğanîm, anam, karasî,
+Onbaşî neşín bakîra?
+Golova ğúre úy-saytîn
+Pyankege şakîra.
+Takatîm kettí belímden,
+Úyúm nasîl hakklayîm?
+Ğanîm, balam, ketíñíz,
+Endí ka-típ saklayîm?
+Íşíme sîygan ballarîm,
+Dúnyaga sîymadî.
+Asker ketmiy ğîladî,
+Kózyaşlarîn tîymadî.
+Azbardan şîksa şîbîrşîp,
+Óríşke sîymay mallarî.
+Kîzîl bilet kolînda –
+Salte baynîñ ballarî.
+Ayakka etík tíktíler,
+Bogîrdagîn tar etíp.
+Soldat aldî padíşa,
+Anaylarnî zar etíp.
+Árúw Kîrîm ğaşlarî
+Sîktîrmaşnî kíy-almay.
+"Vstavay, soldat" degende,
+Kózyaşlarîn tîy-almay.
+Kazarmanîñ kapîsîn
+Aylana da ğúremíz.
+Kazanînda kaynagan,
+Domîz etín kóremíz.
+Kóp ğalbardîk bayarga,
+Slága bízní ğíbermiy.
+Ne rastkelse aşaymîz,
+O da bízge elvermiy.
+Ğañî ay tuwar ğaltîrap,
+Taleke kelír şaltîrap.
+Zabitlerníñ aldînda
+Bíz turamîz kaltîrap.
+ 
+Ğanîm, balam, ketíñíz, 
+soldat bolîp kalmañîz
+ 
+Ğoldan altîn tapsañ,
+Egílíp sakîn almañîz.
+Ğanîm, balam, ketíñíz,
+Soldat bolîp kalmañîz.
+Şamdan çîkar alağa,
+Tarak bilen tokuñîz.
+Çaresiní tapsañîz,
+Ğanîm, balam, okîñîz.
+Sízler ğawga dúşmansîz,
+Elge heş bermeñíz.
+Boş-boşîna síz, ballar,
+Ayak kagîp ğúrmeñíz.
+Suwî şerbet Kîrîmnîñ,
+Topragî onîñ altîndan.
+Mením ğanîm alînsîn
+Anayîmnîñ aldîndan.
+Kîrîm degen iygí yer,
+Kaytîp kene kórgeydík.
+Anam, babam úşúmúz
+Bíz bír yerde ólgeydík.
+Rumelí degen "iygí yer",
+Sîñîrdaşî Dayîrdîr.
+Kaytîñîz, balam, Kîrîmga,
+Inşalla, soñî kayîrdîr.
+At talimí bek kîyîn,
+Eğel kaza başîma.
+Ne yapayîm, yazîlgan
+Kara yazî başîma.
+Ólewuysak, bo yerde
+Mezarîmîz ğabîlmaz.
+Artîmdan ğîlay kalmaga,
+Kardaşlarîm tabîlmaz.
+Gúbernator, Karaşay –
+Ekisí de bír dinde.
+Olar da bízní kîynadîlar,
+Bonday awur bír kúnde.
+ 
+Hálímízní sorasañ, 
+kayîrga kaldî kúnúmúz
+ 
+Bo Rusiye kúnúnde
+Tek saklandî dínímíz.
+Hálímízní sorasañ,
+Kayîrga kaldî kúnúmúz.
+Ayagîm-kolîm ğalañgaç,
+Ğolga şîksam, toñaman.
+Eşím-dostîm kórgende,
+Bír laf etíp oñaman.
+Temíz kîlabdan bolmasa,
+Kapik bersem kaytanga.
+Hakkikiy tarih bîzîla,
+Uydurula şaytanga.
+Sadaka, kayîr heş yok,
+Zor fakîrnîñ hállerí.
+Kúflense de, anbarga
+Sîymay ğatkan mallarî.
+Yaltadan mínseñ poştaga,
+Peterburgga ğetersíñ.
+Murat etseñ, padíşa,
+Ne bolsa da etersíñ.
+Bo fîrînnîñ íşínde
+Bízím aşlar píşe mí?
+Kîrîmnîñ kalkîn aglatmak,
+Şanîñîzga túşe mí!?
+Dúnya-dúnya bolmadî,
+Yawm-beter kún-kúnden.
+Bo Kîrîm bek nasipsíz,
+Ílíşíp tura bír kîl man.
+Imperator babañîz,
+Kîrîmnî nasîl túzettí?
+Alganî man kolîna,
+Ğamílerní túzettí.
+Túrlí yakşî şiylerden,
+Bízler urba kíymedík.
+Bútún ómír boyînğa,
+Kún ğarîgî kórmedík.
+Eger ketsek Kîrîmdan,
+Bír taleke ğegermíz.
+Ólewuysak ka-termíz?
+Bíz hasretlík şegermíz.
+Suwî şerbet Kîrîmnîñ,
+Topragî da altîndan.
+Kímí ğurttan ayrîldî,
+Kímí ewlat, kadîndan.
+Ólewuysak bo yerde,
+Ne bolîr bízím hálímíz?
+Ústúmúzge ot píter,
+Ğoytîlîr bízím kabrímíz.
+Azî kettí Kîrîmnîñ,
+Azizler míndan awuşur.
+Ólmegen kul, inşalla,
+Hasretíne kawuşur.
+# Medeniyet dersí
+# Ata ğurtí úşún tatlî, gúzeldír ólím
+Tek ğanîm vatanga, bolsîn şeker eğelím — Dulce et decorum est pro patria mori
+Pro patria mori est dulce et decorum
+# EŞREF ŞEMI-ZADE
+Unutma!
+Hey, afatlî boranlarda
+Şatallangan ğaş ğúrek,
+Tamîrîñda kaynap turgan
+Kara kannî kurutma!
+Kan íşúwğí azgînlarnîñ
+Kalkka bergen azabîn,
+Ómírbilla bagîşlama,
+Ómírbilla unutma!
+Unutma sen o kargîşlî,
+Kanlî súrgún yîllarîn,
+Şeher, kóyler, úyler íşí
+Ayazlarga tolganîn.
+Úş yaşînda kúlgí nedír,
+Oyîn nedír unutup
+Biñlep-biñlep sabiylerníñ
+Ğolda ğetím kalganîn...
+Bízler kórgen horlîklarnî
+Túşúnde hem kórmegen,
+Bízler íşken çaymaçanîñ
+Lezzetíní bílmegen,
+Bír awuş kan íşúwğíler
+Búgún gene kan istiy,
+"Men yaşayîm, men oynayîm,
+Sen ómírlík şegíş" - diy.
+Hey, afatlî boranlarda
+Şatallangan ğaş ğúrek,
+Tamîrîñda kaynap turgan
+Kara kannî kurutma!
+Kan íşúwğí azgînlarnîñ
+Kalkka bergen azabîn,
+Ómírbilla bagîşlama,
+Ómírbilla unutma!
+1956 s.
+# ABDULHALIK UYGUR
+Uyan!
+Hey, pakîr Uygur, uyan, yukuñ yeter,
+Sende mal yok, endí ketse ğan keter.
+Bo ólímden ózíñní kutarmasañ,
+Ah, seníñ hálíñ píter, hálíñ píter.
+ 
+Tur! dedím, başîñ kóter, yukuñnî aş!
+Rakibníñ başîn kesíp kanîn şaş!
+Kóz aşîp etrapka eger bakmasañ,
+Ólseñ armanda, bír kún yok ilaş.
+ 
+Zaten ğansîzga uşadî teníñ,
+Şoga yok mî heş te ólímden gamîñ?
+Şakîrsam kîbîrdamadan ğatasîñ,
+Uyanmay ólgen mí şo seníñ betíñ?
+
+Kózíñní geñíş aşîp etrapka bak,
+Óz istikbalîñ hakkînda oylan uzak.
+Ketse koldan bo ganimet, pîrsatî,
+Keleğek úşún íşíñ şîltak, íşíñ şîltak.
+ 
+Ağînîr góñílím saga, hey Uygurum,
+Arkadaşîm, kardaşîm, bír tuwganîm.
+Kúyúnúp hálíñe tursatsam sení,
+Añlamaysîñ zatiy neler bolganîn?!
+ 
+Keleğek bír-gún, sen peşman kelersíñ,
+Sóz manasîna şo wakît ğetersíñ.
+"Way" deseñ şo wakît sayîlmay kalîr,
+O zaman, Uygur, sózge hakk berersíñ.
+ 
+1921-Yîl, Turpan
 # Odanîñ tóríndekí patta ğatkan loksa, arada-bír başîn kóteríp kundaklangan balasîna karay, kolîn uzatîp sîypay, "bebiyím, bebiyím" dep. Ğún tóşek astînda, ğún ğórkan ústúnde, sîğak ama hawalî oda, kúnbatarga karay, balaban kapîdan kelgen-ketkenlerní kóstertíp. Aydînlîk, kúneş bol-bol kíreğek, ama ğawunlî kúnlerde kórínmiy. Şo arada, azbarîñ íşíndekí sesler karîştî, kabarîştî, tanîgan sesler men yabanğî sesler bírleşíp, sankem Sarîgóllí Kavlamet akay keldí dep túşúndí Şeripe. Ekewí bír yaşta bolalar ama Fazîl ondan ekí yaş balaban bolganî úşún o da koğasîñ dogmîşî bolganî úşún, Şeripe ya akay dep ayta ya da Şelebí dep ayta, bolay kóstere o eskíden kalgan húrmetín "akay" yagîna. Bír dakka geşmeden hağíníñ sesí tuyuldî azbarda:
 - Koş keldíñ, Kavlamet, koş keldíñ, sen de Oriye de koş sepa keldíñíz! Ula, bílíp keldíñíz mí? Tuyup mî? Búgún bízde mújde bar, mújde!
 Arabadan kagînîp, sílkíníp túşken ğolşîlarnî hağí Fazîl man Ómer kuşaklap, óbíp, úyge kírsettíler, hayatîñ íşí toldî dawuşka, olarîñ sesleríne merak etken loksa, zor dayana bakîrmadan "men de barman" dep.
