fdaa92896f3da812306a39a852269511baeaae60
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-08 09:59:41 +0100

    Add files via upload

---
+++ data/Nazar_Look.ts
@@ -0,0 +1,5 @@
+
+export const nazarLookEntries: string = `
+# Nazar Look, Qırımtatar tilinde bir mecmua edi. — Nazar Look was a magazine in Crimean Tatar language.
+# Taner Murat tarafından idare etildi. — It was regulated by Taner Murat.
+`;
