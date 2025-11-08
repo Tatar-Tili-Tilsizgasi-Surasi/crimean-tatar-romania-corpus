ba53e7e08de6ae98b8ea5cc23ad70b725771bfb7
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-08 11:42:25 +0100

    Add February 2011 event entries for Crimean Tatar
    Added detailed entries for February 2011 events related to the Crimean Tatar community.

--- data/Nazar_Look/Subat_2011.ts
+++ data/Nazar_Look/Subat_2011.ts
@@ -1,3 +1,190 @@
 
 export const subat2011Entries: string = `
+# Mamuriyet kaberlerí
+Ğenevre, 15.01.2011 - Ğenevre 
+Imaginaid Galerisinde 4 şubat 2011'de helwetiyelí 
+resímğí we gázeteğí Alban Kakulya'nîñ "Taking 
+Crimea" ("Kîrîmnî almak") atlî resím sergísí 
+aşîlağak. 
+Sergí Alban Kakulya'nîñ Kîrîmga yapkan 
+ğolşîlîgînda alîp toplagan resímlerínden ibarettír 
+we Zoï Environment Network yagîndan 
+tertíplengendír.
+Sergí 12 martkaşîk kalkka aşîk 
+kalağaktîr.
+ 
+Ankara, 07.01.2011 (QHA) - Kîrîm 
+Túrklerí we Yardîmlaşma Dernegí'ñ 
+tertíplemesínde aytuwlî Kîrîm kalk oyînlarî we 
+şarkîlarî takîmî Kîrîm Ansamblî koşaksîz 
+programî "Kîrîm Tatar Múzik we Dans Keşesí"ñ 
+síptí kósteríşínde Ankara'da, Milliy Egítím Şura 
+Salonînda kalknî ayakka tursatîp alkîşlattî. 
+Kîrîm Ansamblî Grubî Bayrak, Kîrîmlî 
+Ğígítler, Tîm Tîm, Bakçesaray Kaytarmasî we 
+Kîna Ğakma bír-bírínden gúzel oyînlarî man, 
+Rîza Yusuf ezberlegen manzúmelerí men, Server 
+Kakura ğîrlarî man, Dilaver Osman, Asiye Sale 
+bír de Emine Mustafa dúrkúlerí men, şarkîlarî 
+man tolî-tolî salonnî kúyğanlandîrdîlar. 
+Aşadakî programîna kóre oşak ayînda 
+Kîrîm Ansamblî Túrkiye'ní dolaşîp kósteríşleríne 
+dewam ettí: 7-8 oşak Ankara, 11 oşak Ğeyhan, 13 
+oşak Nevşehir, 15 oşak Konya, 16 oşak Polatlî, 19 
+oşak Kîrîkkale, 21 oşak Tekirdak, 22 oşak 
+Koğaeli, 23 oşak Gebze, 25 oşak Bandîrma, 28 
+oşak Izmir, 29 oşak Istanbul.
+ 
+Akmesğit (Simferopol) 12.01.2011 
+(QHA) - Nesíl dergísí, Akmesğit’íñ Ukrayna 
+Múzikal Tiyatrosînda tertíplegen tórení men 2010 
+yîlînda túrlí-túrlí alanlarda başaruwlî bolgan 
+Kîrîm Tatarlarîna ódíller dagîttî.
+Program sîrasînda SMS ğolî man 5109 
+numarasîna ğíberílgen oylarîna kóre bellí bolgan 
+Yîlîñ Kariyerí, Yîlîñ Tiğariy Markasî, "Nesíl" Bala 
+Fotografî Yarîşmasî, Yîlîñ Koşagî, Yîlîñ Kelín 
+Entárísí alanlarînda kazanganlarga ódíller 
+dagîtîldî.
+Yîlîñ Kariyerí alanînda Kîrîm Tatar Milliy 
+Meğlisí I-ínğí Başkan Yardîmğîsî Refat Çubar 
+(Refat Çubarov), Yîlîñ Markasî alanînda “Vega” 
+şirketí başkanî Erlan Ablaziz (Erlan Ablazizov), 
+“Nesíl” Fotograf Yarîşmasî alanînda bír yaşîndakî 
+Yakup ísmínde bala kazanîp şîktî.
+Kîrîm Ózerk Ğumhuriyetí Súrgúnden 
+Kaytkan Vatandaşlar we Etnik Ílíşkíler Komitesí 
+Başkanî Edem Dudak'îñ (Edem Dudakov) maddiy 
+destegí men yapîlgan program kapsamînda ateş 
+kósteríşí we konser yapîldî. Konserde, Ğemile 
+Ansamblî, Elzara Batal (Elzara Batalova), Hayat 
+Ansamblî, Dilara Mahmut (Dilara Mahmudova), Zera 
+Kağîkay (Zera Kağîkayeva), Ernes Pîrtîh (Ernes 
+Pîrtîhov), Sevil Memet (Sevil Memetova), Ğaşlîk 
+Ansamblî, Aliye, DJ Bebek, Nariman Baliç kibí 
+aytuwlî Kîrîm Tatar sanatşîlarî sahne aldî.
+Kîrîm Ózerk Ğumhuriyetí Kaberleşme 
+Komitesí Başkan Yardîmğîsî Şevket Memet (Şevket 
+Memetov) we Kîrîm Múhendislík we Pedagojiy 
+Úniversitesí Rektorî Prof. Dr. Fevziy Yakup (Fevzi 
+Yakubov) kaberleşme destegí sawladîlar, Kîrîm 
+milletvekillerí Enver Abdurrahim (Enver Abduraimov) 
+ve Remziy Iliyas (Remzi İlyasov), programîñ reklam 
+sponsorlarî boldîlar. Program sunuğulugun Kîrîm 
+Tatar Akademik, Múzikal we Drama Tiyatrosî 
+artistlerí Ellina Mustafa (Ellina Mustafayeva) we 
+Eskender Islam (Eskender Islamov) yaptîlar.
+Nara Nariman (Nara Narimanova) – Osman Bayramaliy 
+(Osman Bayramaliyev)
+ 
+Kóstenğe, 15.01.2011 - Kóstenğe 
+Yazarlar Bírlígí kazak şayirí Mihay Eminesku'nî 
+(Mihai Eminescu) añgan 
+bír şiir kúní 
+tertípledí. 
+Programga tatar 
+yazuwğîlarîmîz da dawetiye 
+alîp koşîldîlar. 
+Eminesku'nuñ eserlerínden 
+tatarğa kaytarmasîndan 
+Gúner Akmolla we túrúkşe 
+kaytarmasîndan Nermin 
+Hoğa-Susam okîp toplantîga ayîrî bír renk 
+berdíler. 
+Akmesğit, 19.01.2011 (QHA) - Akmesğit 
+Etnografiy Múzesínde Kîrîm Ózerk Ğumhuriyetíñ 
+kuruluşuñ 20-ínğí yîlkaytîmînda “Kóp îrklî 
+Kîrîm” atlî sergísí aşîldî.
+Kîrîm Ózerk Ğumhuriyetí, 20 oşak 1991 
+yapîlgan ilan we Kîrîm'îñ ózerlígín belletken 
+referendum soñîşlarî esasînda Ukrayna Parlamentosîn (O kaytîmda Ukrayna Soviyet 
+Sosyalist Ğumhuriyetí Parlamentosî) 12 şubat 
+1991 tewúkesínde algan “Kîrîm Ózerk Soviyet 
+Sosyalist Ğumhuriyetín Kaytarîp Akelme” kararî 
+man kurulgan edí.
+Kîrîm Ózerk Ğumhuriyetí Súrgúnden 
+Kaytkan Vatandaşlar we Etnik Ílíşkíler Komitesín 
+katkîları man tertíplengen sergíge sóz konîsî 
+komiteníñ başkan yardîmğîsî Emine Avamil (Emine 
+Avamileva), múze múdúrí Yuriy Laptev, Etnik Uyum 
+Teşkilatî Başkanî Vladimir Kazarin, Kîrîm 
+Almanlar Dernegí Başkanî Yuriy Gempel 
+katîldîlar.
+Sergíde, múzede saklangan we Kîrîm’da 
+yaşagan kalklarîñ kúndelík hayatîndan karelerin 
+yer algan fotograflar, Kîrîm Ózerk Ğumhuriyetí’ñ 
+simbollarî we başaruwlî Kîrîm sakinlerine 
+berílgen Kîrîm Ózerk Ğumhuriyetí’ñ rozetlerí yer 
+aldî.
+Kîrîm Ózerk Ğumhuriyetí’ñ kuruluşuñ 20-
+ínğí yîlkaytîmîna yapîlgan programlar, Kîrîm 
+Parlamentosîñ 27 aralîk 2010 tewúkesínde alînîp 
+588 numaralî kararîna kóre yapîla.
+Nara nariman (Nara Narimanova) – Osman Bayramaliy 
+(Osman Bayramaliyev)
+ 
+Istanbul, 12.01.2011 - Yrd. Doç. Dr. 
+Nedim Bakîrğî’nîñ Kómen Yayînlarîndan şîkkan 
+"Kîrîm Tatar Masallarî" atlî kitabî Túrúk 
+okuwğlarî man tabîşa.
+Eserde sayîlgan 50 Kîrîm Tatar masalîn 
+látin hárflerí (Túrkiye Túrkçesí elifbesí) we Túrkiye 
+Túrkçesíne aktarîmî tabîla. Bondan ayîrî kitabîñ 
+başînda masallarîñ inğeleme bólímí mevğut.
+2010 senesíñ soñînda ğayînlangan 554 
+sayifalîk karton kapaklî kitap internet 
+kitapşîlarînda satîlmaga başladî.
+ 
+Kezlew (Yevpatoriya), 11.01.2011 
+(QHA) – Kezlew’de ballarîñ şekken resímlerí 
+“Mením Ukraynam” we “Kîrîm-Mením Gúzel 
+Yerím” konîlî sergísínde yer aldî.
+Kezlew Belediyesíñ bergen bílgísíne kóre sergíde, 
+kasabada tabîlgan bútún mekteplerín we 
+Rovesnik (Yaşît) Bala Sanat Merkezíñ Pozitif 
+Stúdiyosî úyrenğíleríñ yapkan manzara we 
+serbest konîlî fotograflar yer aldî. Sergíden ewel 
+ğaş fotografşîlar juriyge yúzlerğe fotograf ğíberdí. 
+Juriy, bonlarîñ arasîndan eñ árúwlerín ayîrîp 
+sergíge saldî. 
+Juriyníñ kararî man sayîlağak 
+kazanganlarga túrlí ódíller beríleğek, kazanganlar 
+“Bízím Ukrayna” atlî Ukrayna resím sergísíne 
+katîlmaga hakk eteğekler. 27 aralîkta başlagan we 
+14 oşakka kadar dewam eteğek sergíníñ sponsorî 
+Blik Fotograf Merkezí boldî.
+Çingiz Súliyman (Çingiz Súleymanova) - Osman 
+Bayramaliy (Osman Bayramaliyev)
+Ankara, 20.01.2011 (QHA) - Kîrîm 
+Túrklerí we Yardîmlaşma Dernegí'ñ 
+tertíplemesínde 20 oşak 2011 perşembe kúní 
+“Altîndak Belediyesí Hamamóní sóyleşilerí – Oşak 
+2011” programı íşínde “Kîrîm Tatar Şiir ve 
+Edebiyat Kúní” yer aldî.
+Programda destanlarî, manilerí, ğîrlarî, 
+gazellerí, koşmalarî, hikayelerí, romanlarî man 
+Kîrîm Tatar Edebiyatî anlatîldî.
+Program Altîndak Belediyesí Tarihiy 
+Kabakşî Konagînda yapîldî.
+ 
+Akmesğit (Simferopol), 13.01.2011 
+(QHA) – Kîrîm Ressamlar Úyúnde 12 oşakta 
+ballarîñ yapkan resímlerí yer algan bír sergí 
+aşîldî. Sergí, Akmesğit Sanat Mektebíñ 
+kuruluşuñ 65-ínğí yîlkaytîmî múnasebetí men 
+tertíplendí. Mektebíñ sergísínde túrlí yîllarda bo 
+okîlda okîgan ballarîñ yapkan 50x70 çm ebatînda 
+114 adet we 50x40 çm ebatînda 12 resím yer aldî. 
+Sergíge, Ukrayna Ğumhurbaşkanî Dayimiy Kîrîm 
+Temsílğísí Vekílí Viktor Plakida katîldî. Sergí, 26 
+oşakka kadar dewam eteğek.
+1944 yîlînda kurulgan mektepke şo anda 
+350 bala okîy. Okîlda egítím kórgen úyrenğíleríñ 
+eserlerí Hindistan, Kúba, Fas, Sudan, Súriye, 
+Ğezayir, Afganîstan, Kamboğiya, Yugoslaviye, 
+Danimarka, Helwetiye, Hollanda, Breziliye, 
+Kanada gibi dúniyanîn bírkóp ulkasînda 
+sergílengen edí.
+Evelina Iliyas (Evelina Ilyasova) - Osman Bayramaliy 
+(Osman Bayramaliyev)
 `;
