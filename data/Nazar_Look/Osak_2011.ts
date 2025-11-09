448d3f4d32e439fe4ed5143db4c1dbdab11d1eef
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-09 17:47:34 +0100

    Update Osak_2011.ts

--- data/Nazar_Look/Osak_2011.ts
+++ data/Nazar_Look/Osak_2011.ts
@@ -1,4 +1,70 @@
 
 export const osak2011Entries: string = `
 # Oşak — January
+# NAŞIRÍÑ MEKTÚBÍ
+Kîymetlí soydaşlarîm marhabalar,
+ 
+Búgún ğap-ğañî bír ğolîñ başîndamîz. Rumaniye’dekí Tatarlîgîñ 
+tîşawun kesíp ğolîn aşmak ístegí men bo aylîk meğmuwanîñ 
+temelín attîk.
+Maga kalgan bolsa men raát-raát íşíme karay berer 
+edím. Zaten bo meğmuwa şîkmagan bolsa da men ózímní ifade 
+etmege túrlí ğollar tabar edím, kitap yazîp bastîrmak gibí, 
+awlarda sayifalar aşmak gibí, we sayire. Lákin yígírím seneden 
+berítlí índemeden susup turdum, şúndí de oñîma-solîma 
+karaganda kózím aldîna neday bír manzara kele? Tomrîğa'da 
+tílímíz Tatarğa sakawlarîñ kolîna kírdí, ses uyumundan 
+kabersíz insanlar tílímíz men, elifbemíz men oynap, astîn kazîp, 
+biñlerğe senelerden kalgan mamuriyetímízní ğok etmege 
+şalîşalar. Rumaniye'de geşen yúzyîlîñ ortasînda kartbabalarîmîz 
+kurgan látin elifbemíz karma-karîşîk bolîp kaldî. Tatarğa ekí laf 
+koşa-almagan sesler bíz sozîk awazmîz dep seslí bolîp şîgalar. 
+Ediplerímíz yaratkan elifbege bo sakawlar óz ğartî akîlîna kóre 
+túrlí-túrlí deñíştírímler ketírmege ogîraşalar. Meğmuwamîzda bo 
+şatlak awazlarnî susturup, taa arka betke otîrtîp tartîklarîñ 
+sîrasîna salağakmîz, sessízleríñ arasîna, ózleríne láyîk bír yerge. 
+Hep şo zamanda tartîlîp otîrgan bazî hoş sesler meğmuwamîzda 
+taa aldga alînîp sozîklarîñ sîrasîna geşíríleğektír, seslíleríñ 
+arasîna.
+Bondan ayîrî wurgî meselesín de dîkkatîmîzga almaga 
+şabalanağakmîz. Ne yazîk ke soñ yígírím senesíñ 
+kayretsízlígínden Tatarğa tílímízde wurgular da yerlí-yersíz 
+kullanîldî. Bonîñ úşún bíz yañgîş wurgular kullanmadan sáde 
+dogrî basîp, wurmak kerekmegen eğíklerní raát bîrakîp 
+wurulağak eğíklerní wurgulamaga ğúkleneğekmíz. Bonday etíp, 
+tílímízge kongan tertípsízlíkní temízlemege karap Tatarğanîñ 
+tatlî ses uyumun kaytartmaga ğan bereğekmíz çúnkí hakkîmîz 
+anatílímízdedír, bo inanîş man terlep şalîşamîz.
+Maalesef bo faaliyetímízge ğol aşkan Tomrîğa'dakî 
+Tatarğanîñ bolîmsîz turumî. Rumaniye'de Tatar tílí pítiyatîr. 
+Yígírím sene bekliy-bekliy otîrdîk, Rumaniye'de Tatarğa mektebí 
+gene yok! Televiziyon yok. Tyatro yok. Yok, yok, yok. Bíz, 
+Nazar Look meğmuwasî, bútún bo zorlîklarnî karşîmîzga alîp 
+Tomrîğa'dakî Tatarğasîn dogrî kullanmasîna karawulluk 
+yapmaga ázírmíz. Bízím tek emelímíz tílímíz. Bízím tek 
+istikametímíz memleketímíz!
+Kîymetlí okîwğular, bo bízím túşlerímízní 
+kanatlandîra-algan tek sízsíñíz, dúniyanîñ dórt yagîna tarîday 
+atîlîp ğayîlgan Tatarlar, yuwasîn sagînîp yuwasîna kaytkîsî 
+kelgen sayrawğî kuş Tatarlar. Buyuruñuz, bo emelní bíz men 
+paylaşsañîz manzúmeleríñízní, şaşma şalîşmalarîñîznî, 
+makaleleríñízní yollañîz, sabîrsîzlîk man bekliymíz! Buyuruñuz, 
+tek silagîmîz, tek zewzegímíz tílímízní kullanîp bo 
+meğmuwanîñ satîrlarînda sayraşayîk, sayifalarînda ótíşiyík, 
+maksatîmîzga barayîk.
+2011'ge Kayîrlî Seneler
+Kaytarîp alînsîn eskí memleketler!
+Taner Murat
+# MILLIY IHANET
+13 aralîk 2010 Tatarlarîñ Kúní múnasebetí men Kóstenğe mezarlîgînda 
+yapîlgan tórende
+HEŞBÍR TATARĞA SÓZÍ KULLANÎLMADÎ!
+ 
+BOGADARÎ DA FAZLA,
+YETER ENDÍ!
+ 
+BO NAMUSSUZLUKKA
+BÍR SOÑ BERÍLMELÍ!
+TUWUL MEZATLARGA SATLÎK
+BO TATARLÎK
 `;
