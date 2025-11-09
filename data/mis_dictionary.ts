3aecbf6a73b5180c8aecad9cfdd839ec57e617fb
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-09 18:29:58 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -1,10 +1,10 @@
 export const misDictionaryRawText: string = `
-çm (abreviere pentru „çentimetre” = centimetru) cm
+çm — abreviere pentru „çentimetre” = centimetru (cm)
 dinozor s. dinozaur.
 Ğadîlar Bayramî s. Halloween.
 Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
-ok (abreviere pentru „okka” = kilogram) kg 
+ok — abreviere pentru „okka” = kilogram (kg) 
 Şînğa s. (topon.) Xinjiang, China.
 Virjiniye s. (topon.) Virginia.
 `;
