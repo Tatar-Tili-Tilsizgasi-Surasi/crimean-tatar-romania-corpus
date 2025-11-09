c2cbff6abcedb60ddd9c0629e8ba5f9a3330ff9a
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-09 17:54:15 +0100

    Update Osak_2011.ts

--- data/Nazar_Look/Osak_2011.ts
+++ data/Nazar_Look/Osak_2011.ts
@@ -67,4 +67,181 @@ BO NAMUSSUZLUKKA
 BÍR SOÑ BERÍLMELÍ!
 TUWUL MEZATLARGA SATLÎK
 BO TATARLÎK
+# Mamuriyet kaberlerí
+Akmesğit (Simferopol), 03.12.2010 
+(QHA) - ABD’níñ eñ prestijlí burs programî 
+bolgan Fulbright Programîñ Ukrayna’dakî kolî 
+bolgan Fulbright Ukraine, 2 Aralîkta Akmesğit 
+Ukrayna Úyretím Reformlarî Merkezínde 
+tanîtîldî.
+Tanîtîma, Fulbright Ukrayna Programî 
+Múdúrí Miron Stahiv, ABD Úniversitelerínde 
+úyretím programî ğónetúwğísí İnna Barîş, Kîrîm 
+Kúltúr ve Sanat Úniversitesí Rektorî Oleg 
+Gabrieliyan, Tavriya Milli Úniversitesí úyretím 
+úyesi Doçent Dr. Elmira Murat (Elmira Muratova), 
+program bursalîsî Terrel Starr, program 
+mensuplarî ve program mezunlarî katîldî.
+Tanîtîm sîrasînda program hakkînda bílgí 
+beríldí, programîñ sawlagan imkáanlarî, 
+perspektifler añlatîldî.
+Fulbright programî, 1946 yîlînda kuruldî. 
+1992 yîlîndan berítlí yakîn 700 Ukraynalî bílím 
+adamî, úniversite úyrenğísí men ğaş uzmanlar, 
+ABD húkúmetí tarafîndan finans etílgen program 
+vasîtasî man ABD’ní ziyaret etíp onda úyretím 
+kórdí, bílím aktarmalar yaptî. Bo súre íşínde 
+ABD’den yakîn 400 úniversite úyrenğísí men 
+bílím adamî da Ukrayna’nî ziyaret ettí.
+Nara Nariman (Nara Narimanova) - Osman Bayramaliy 
+(Osman Bayramaliyev)
+ 
+Kiyev, 06.12.2010 (QHA) - Ukrayna’nîñ 
+“Ukraina” Televizyonînda yapîlgan şarkî 
+yarîşmasîñ úşúnğí sezonîn Kîrîm Tatar şarkîğîsî 
+Asan Bilal (Asan Bilalov) kazandî.
+Kalk şarkîğîlarî arasînda yapîlgan yarîşmanîñ 
+finalî 4 aralîkta boldî. Asan Bilal finalda eñ kóp 
+siyírğí oyîn toplap (%53,94 oy) yarîşmanî 
+kazandî. Bilal, yarîşma man barabar 500 biñ 
+grivna (yakîn 62 biñ dolar) para da aldî.
+Asan Bilal 40 yaşînda. Ózbekistan’îñ 
+Uçkuduk kasabasînda tuwdî. Klarinet sahasînda 
+múzik okulîn pítírdí. Tewúke úyretmení Asan 
+Bilal balşîbînğîlîk man inşaattan ilgílene. Úylí, 
+ekí kîz balasî babasî. Bilal, projede kazangan 
+parasî man úy salîp eñ súygen eskí SSCB filmín 
+de bírtaa şegeğegín ayttî.
+Iliyas Aliy (Ilyas Ali)
+ 
+Akmesğit (Simferopol), 10.12.2010 
+(QHA) - Akmesğit’te 10 aralîkta Kîrîm Tatar 
+Sanat Múzesínde, “Kîrîm Tatar ressamlarîñ 
+eserlerínde múzik dúniyasî” sergísí aşîldî. 
+Sergíde ressamlarîñ we Kîrîm Tatar Múzesí 
+kolleksiyonînda yer algan resímler ziyaretşíleriñ 
+ilgísíne sunuldî.
+Erkinlígín konîsîna uygun, múzik 
+eşlígínde yapîlgan aşîlîş tórení, Kîrîm 
+Milletlerarasî Ílíşkíler ve Súrgúnden Kaytkan 
+Vatandaşlar Komitesí Başkan Yardîmğîsî 
+Vladimir Blinov, aytuwlî Kîrîm Tatar besteğísí, 
+şarkîğî Kîrîm Kalk Sanatşîsî Fevziy Aliy (Fevzi 
+Aliyev), Kîrîm Tatar sanat bílímğísí Elmira Çerkez 
+(Elmira Çerkezova), “Makam” Kîrîm Tatar Klasik 
+Múzikasî Toplîlîgî ğónetúwğúsí Ğemil Karîk (Ğemil 
+Karikov), Ukrayna Kalk Sanatşîsî Rústem Memet'íñ 
+(Rústem Memetov) katîlîmî man kerşekleştí.
+Sergíde yer algan aytuwlî Kîrîm Tatar ressamlarî 
+Zarema Trasin (Zarema Trasinova), Alim Húseyin (Alim 
+Useinov), Asan Baraş, Ramazan Húseyin (Ramazan 
+Useinov), İrfan Nafiy (Irfan Nafiyev), Ayder Aliy (Ayder 
+Aliyev), Rústem Emin (Rustem Eminov), Aliy Bekir (Ali 
+Bekirov), Yakup Ablay (Yakubovve Ablayev) hanedanî 
+ressamlarî, Ismet Veliyulla (Ismet Veliullayev),
+Rústem Vahap (Rústem Vaapov), Enver Izzet (Enver 
+Izzetov), Mustafa Murtaza (Mustafa Murtazayev), Kázîm 
+Ğeppar (Kazîm Cepparov) man Mayre Lúman'nîñ 
+(Mayre Lúmanova) eserlerínde múzik dúnyasî tasvir 
+etílgen.
+Resímler, eñ gúzel sanat dallarîndan bírsí 
+bolgan múzikasîn bírleştírgen ressamlarîñ 
+tuygularîn yansîta. 
+Nara Nariman (Nara Narimanova) - Yunus Kîrîmlî
+ 
+Mejdiye (Medgidia), Rumaniye, 
+04.12.2010 (QHA) – Mejdiye'de aytuwlî şayirímíz 
+Mehmet Niyaziy anîldî. Anîlmanîñ síftí bólímínde kasabanîñ merkezínde tabîlgan şayirníñ 
+hewkelíne şeşek tóşeldí. Ekínğí bólím mezarlîkta 
+tutulup, şayirníñ hewkelíne şeşek tóşelíp duwa 
+okîldî.
+Ondan soñra Mejdiye Mamuriyet Úyúnde 
+yapîlgan bír tórende Mehmet Niyaziy edebiyat 
+ódílí de beríldí. Bo sene ódíl Nevzat Yusuf 
+Sarîgól'níñ hakkî eken. Tórenní tertíplegenler 
+tekmílğí darúlfúnun muwallímí Nevzat Yusuf 
+Sarîgól'níñ kolîna tutturgan paranî saklî tutup 
+aytmadîlar. Eger bo dewletten kelgen paradan 
+berílgen ódílíñ degerí aytîlgan bolsa hakklî-
+hakksîz şúphelerge ğol aşîlmaz edí.
+Kóstenğe, 10.12.2010 (QHA) – Askeriye 
+Úyúnde tertíplengen kitap imzalamasînda kazak 
+yazuwğusî Vasilica Mitrea Tatarlîk konîsînda 
+yazgan "Vremuri, oameni şi… destine" kitabîn 
+tanîttî. Bo kitap yazuwğunuñ Kîrîm'ga yapkan 
+bír ziyaretíñ artînda kalemge alîndî. Sayifalar 
+bazî Tatarlarîñ bír de bútún Tatarlîgîñ kaderín 
+resímín sîzmaga şalîşa.
+"Men bo kitapka şalîşmaga başlaganda 
+bútún dost-tanîşlarîm Tatarlar man wakîtîmnî 
+kaybetmiyím dep maga akîl beríp turdî. Kîrîm'ga 
+yapkan ziyaretíme kadar heşbír Tatar tanîmagan 
+edím. Bo ğolda hem Rumaniye'dekí Tatarlarî 
+man, hem Kîrîm'dakî Tatarlarî man tanîşîp 
+konîşkan soñ onlarîñ insanlîgî bízge úyretílgen 
+masallar man heş uyumaganîn añlaganda, 
+utanuwumdan bo kitapnî yazmaga karar aldîm."
+"Vremuri, oameni şi… destine" Vasilica 
+Mitrea'níñ dórtínğí kitabîdîr. Bondan ewel "Ştiai 
+ca timpu-i colorat?", "Cândva,… prietene" bír de 
+"Confesiunile Angelei" kitaplarîna da imza atkan 
+edí. Vasilica'nîñ toplîm we mamuriyet şalîşmalarî 
+bírkóp meğmuwanîñ íşínde de yer taba. 
+Akmesğit (Simferopol), 13.12.2010 
+(QHA) - Kîrîm Tatar tílí, UNESCO’nîñ Aralîk 
+ayînda ğañîlaştîrgan Kaybolma Tehlikesí 
+Astîndakî Dúniya Tíllerí Listesíne kírdí. 
+UNESCO’nîñ ğañî şîgargan listede túrlí 
+dereğelerde kaybbolma tehlikesí astînda bolgan 
+toplam 2.473 tíl yer aldî.
+UNESCO, Kîrîm Tatar tílín Kîrîm’da 
+Kîrîm Tatar, Kîrîm Túrk ve Nogay (Kîrîm) úş 
+şiwesíne ayîrîp bo şiwelerní orta dereğede tehlike 
+astînda bolganlarîn bellí ettí. Obír yaktan 
+Tomrîğa bólgesínde de barlîgîn súrdúrgen Kîrîm 
+Tatarğasî da hep şo tehlike kategoriyesíne alîndî. 
+Bo dereğelendírmege kóre kartlar bo tílní konîşa, 
+orta yaş grubî tílní añlay amma óz aralarînda bír 
+de ballar man anatílde konîşmay. 
+Emine Muzafar (Emine Muzafarova) - Osman 
+Bayramaliy (Osman Bayramaliyev)
+ 
+Akmesğit (Simferopol), 16.12.2010 
+(QHA) – Ilham atlî Kîrîm Tatar kalk oyînlarî 
+toplîlîgî, Túrúk sanatşî Gúnay Naiboğlu man 
+barabar Kîrîm turnesíne şîktî. Turneníñ 
+tertíplewğílerínden Ayşe Yakup'núñ (Ayşe Yakubova) 
+QHA’ga bergen bílgílerge góre İlham man Gúnay, 
+Akyar (Sevastopol), Bakçesaray, Akmesğit men 
+Akmeçet’te (Çernomorskoye) ortak konserler berdí. 
+Yakup, siyírğíler men tîklîm-tîklîm salonlarda 
+yapîlgan konserlerde Ilham grubî dans ğónetmení 
+Aliye Memet'níñ (Aliye Memetova) ázírlegen oyînlarnî 
+oynaganda, Gúnay Kîrîm Tatar dúrkúlerí men 
+ğañî Túrúk şarkîlarîn iğra ettí.
+Yakup, “Bíz Akyar'da yaşagan Kîrîm 
+Tatarlarnî az dep konserge katîlîmîn az 
+bolağağîndan korktîk amma konserge kóp kíşí 
+keldí. Kîrîm Tatarlar bonday bír konserní kópten 
+bekliy edí.” dedí.
+Çingiz Súleyman (Çingiz Súleymanova) - Osman 
+Bayramaliy (Osman Bayramaliyev)
+ 
+Ankara, 17.12.2010 (QHA) - 16 ulkadan 
+29 edebiyat dergísín katîlîmî man Ankara’da 
+úşúnğí Túrúk Dúniyasî Edebiyat Dergílerí 
+Kongresí yapîldî.
+Avrasya Yazarlar Bírlígí'ñ bergen bílgísíne kóre, 
+14-16 aralîk 2010 tewúkelerí arasînda yapîlgan 
+Kongre, TİKA’nîñ katkîlarî man Avrasya Yazarlar 
+Bírlígí men Túrksoy yagîndan tertíplendí.
+Kongrede Azerbayğan, Başkurtustan, 
+Bulgariye, Hollant, Iran, Karaçay-Balkar, 
+Kazagîstan, Îrak, Kîrgîzîstan, Kîrîm, Kîbrîs, 
+Kosova, Ózbegístan, Rumaniye, Túrkmenístan 
+man Túrkiye’den mañlay edebiyat dergíleríñ genel 
+ğayîn ğónetmenlerí bír araga keldí. Kîrîm’nî 
+Kîrîm Tatar dergílerí “Úmút kervanî” man 
+“Nenkeğan” temsíl ettí.
+Yunus Kîrîmlî
 `;
