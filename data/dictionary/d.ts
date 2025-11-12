30df3ff3a38b296693331886b67a11aec2da94ef
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-12 21:39:29 +0100

    Update d.ts

--- data/dictionary/d.ts
+++ data/dictionary/d.ts
@@ -1,3 +1,60 @@
-export const rawDictionaryText = `dórtkatlî /
-
+export const rawDictionaryText = `
+da/de/ta/te I. adv. şi; de asemenea; în plus. II. conj. 1. şi. 2. dar; iar. 3. deşi; cu toate că; chiar dacă; în ciuda. // ●bír de — pe deasupra. ●bír de yoktan — din senin. ●bízím de barmamîz kerek — trebuie să mergem şi noi. ●bo da ne demek? — ce mai înseamnă şi asta?; ●bo da ne? — ce mai e şi ăsta?; ●boranga da kaber beríle — se anunŃă şi furtună. ●heş te — nicidecum; deloc; pentru nimic. ●hîz kolî da bîzîk şîktî — s-a dovedit defect şi schimbătorul de viteze. ●kaş bolsa da — oricât. ●kayday bolsa da — în orice caz; orice ar fi; oricare ar fi cauza; cum o fi; indiferent cum. ●kayet kolay bolsada — deşi este foarte uşor. ●may da salîñîz! — puneŃi şi ulei!; ●mutlak sen de! — exgerezi!; ●obír yaktan da — pe de altă parte. ●sízíñ de kórmeñíz lázîm — trebuie să vedeŃi şi dumneavoastră. 
+daam s. gust. // ●balalîktan óksízlíkníñ aşşî daamîn tattî — din copilărie a gustat amărăciunea de a fi orfan. ●etníñ daamî — gustul cărnii. 
+daametken adj. pofticios. 
+daametme s. poftă. 
+daametmek v.t. a pofti (d. mâncare). 
+daamlatîlgan adj. cu mirodenii; condimentat. 
+daamlatîlmak v.i. a se condimenta; a se pune mirodenii; a se da gust. 
+daamlatma s. mirodenie; condiment. 
+daamlatmak v.t. a condimenta; a pune mirodenii; a da gust. 
+daamlatmalî adj. cu mirodenii; condimentat. 
+daamlatmasî, ateş- s. amestec tradiŃional de condimente; chili mexican. 
+daamlatma-sumagî s. (bot.) oŃetar-comestibil; sumac-comestibil (lat., Rhus coriaria). 
+daamlatuw s. condimentare. 
+daamlatuwğî s. mirodenie; condiment. 
+daamlî adj. gustos. 
+daamsîz adj. fără gust; searbăd; fad; insipid. 
+daamsîzlîk s. lipsă de gust; insipiditate. 
+dabara s. ceremonie; solemnitate. 
+dabaralî adj. ceremonios; solemn. 
+daça s. vilă; reşedinŃă de vară. 
+dad s. 1. gust; savoare. 2. plăcere; desfătare. 3. gust dulce; dulceaŃă. //●dadîn almak — a da de gustul…; a începe să-i placă. ●dadîn kaşîrmak — a-i strica gustul; a exagera. ●dadîn şîgarmak — a savura; a se înfrupta; a-i tihni. ●dadîn yeríne akelmek — a-şi drege gustul. ●dadîna barmak — a da de gustul…; a începe să-i placă. ●dadîna karamak — a-l gusta; a-l degusta. // ●dad tuyuwî — (anat.) simŃul gustului. ●dadî tuzî yok — fără niciun gust. 
+dada s. soră mai mare. 
+dadaluw s. 1. acomodare; obişnuinŃă; deprindere; familiarizare. 2. degustare; savurare. 
+daday s. (dim., fam.) surioară mai mare; tanti. 
+dadberúwğí s. mirodenie; condiment. 
+dadî s. 1. doică. 2. bonă; dădacă. 
+dadîkaşkan adj. 1. (d. alimente, băuturi) trezit; răsuflat. 2. exagerat; excesiv; înflorit. 
+dadînabargan adj. acomodat; obişnuit; deprins; familiarizat. 
+dadînabaruw s. acomodare; obişnuinŃă; deprindere; familiarizare. 
+dadînakaraw s. degustare; savurare. 
+dadînalgan adj. acomodat; obişnuit; deprins; familiarizat. 
+dadlandîrmak v.t. a da gust; a îndulci. 
+dadlî adj. gustos; delicios; apetisant. 
+dadlî-dadlî adj. gustos; delicios; apetisant. 
+dadlîk s. mirodenie; condiment. 
+dadlî-nezzetlí adj. gustos; delicios; apetisant. 
+dadlî-tuzl î adj. gustos; delicios; apetisant. 
+dadsîz adj. insipid; searbăd; fad. 
+dadsîzlîk s. lipsă de gust; insipiditate; neplăcere. 
+dadsîzlîk-tuzsuzluk s. lipsă de gust; insipiditate; neplăcere. 
+dadsîz-tuzsuz adj. insipid; searbăd; fad; fără niciun gust. 
+dadşîgarmasî s. înfruptare. 
+dagaşasî adj. subalpin. 
+dagaşasî-ládiní s. (bot.) brad alpin (lat., Abies lasiocarpa). 
+dagaşasî-narusî s. (bot.) brad alpin (lat., Abies lasiocarpa). 
+dagaşasî-şîrşîsî s. (bot.) brad alpin (lat., Abies lasiocarpa). 
+dagetegí s. (geogr.) poalele munŃilor. 
+dagîlgan adj. 1. dispersat; împrăştiat. 2. dezordonat; răvăşit. 3. dizolvat; descompus. 4. confuz. 5. vâlvoi. 6. destrămat; deşirat. 
+dagîlîm s. dizolvare; dispersare; descompunere; disociere; dezagregare; disjungere. 
+dagîlîş s. repartiŃie; dispunere. 
+dagîlma s. 1. dispersare; împrăştiere. 2. dizolvare; descompunere. 3. împărŃire; distribuire. 4. dezordine; învălmăşeală. 5. destrămare; deşirare. 
+dagîlmak v.i. 1. a se dispersa; a se împrăştia. 2. a se dizolva; a se descompune. 3. a se împărŃi; a se distribui. 4. a se destrăma; a se deşira. 
+dagîtîmğî I. adj. distributiv. II. s. (d. persoane) difuzor; distribuitor. 
+dagîtma s. 1. dispersare; împrăştiere. 2. împărŃire; distribuire. 3. dizolvare; descompunere. 4. destrămare; deşirare. 
+dagîtmak v.t. 1. a dispersa; a împrăştia. 2. a împărŃi; a distribui. 3. a dizolva; a descompune. 4. a destrăma; a deşira. 
+dagîtuwğî I. adj. 1. distributiv. 2. subversiv. II. s. 1. (d. persoane) difuzor; distribuitor. 2. dizolvant. 
+dagma s. 1. bordei; bojdeucă; colibă. 2. cocioabă; coşmelie; magherniŃă. 
+dahil I. adv. 1. înăuntru; în interior; în cadrul. 2. inclusiv. II. s. interior; partea interioară. // ●dahil bolmak — a face parte din; a aparŃine. // ●úğretke dahil — inclus în preŃ.
 `;
