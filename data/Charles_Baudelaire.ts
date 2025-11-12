2ce476714d2747441cd3f228804cad210b4e5d27
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-12 20:26:03 +0100

    Update Charles_Baudelaire.ts

--- data/Charles_Baudelaire.ts
+++ data/Charles_Baudelaire.ts
@@ -3,4 +3,319 @@ export const charlesBaudelaireEntries: string = `
 # Saylangan sonetler — Sonnets choisis
 # Fransîzğa-kîrîm tatarğa ekítíllí baskî — Edition bilingue français-tatar criméen
 # Kîrîm Tatarğasî Taner Murat'îñ kaytarmasîndan — Traduction en tatar criméen par Taner Murat
+# Húkúmlí kitapka mezar kitabesí
+Ey, okîwğî! Ğuwaş, temíz,
+Awurbaşlî, dúrúst adam,
+Bo kitapnî at! Ke haram,
+Moñlî, bîzîk, telbegesíz.
+
+Şaytan man, bo kurnaz usta,
+Uzak kórsseñ ğenábíñní,
+At! Ke okîp boşka mení
+Şatlak tutarsîñ hesapta.
+
+Lákin akîl kaşîrmadan
+Kózíñ karay-alsa ğarga,
+Okîp mení, alîş maga.
+
+Şekken góñíl, meraklangan,
+Karap tursañ kókte ğennet,
+Maga insaf! Ya, bol nálet! — Épigraphe pour un livre condamné
+Lecteur paisible et bucolique,
+Sobre et naïf homme de bien,
+Jette ce livre saturnien,
+Orgiaque et mélancolique.
+
+Si tu n'as fait ta rhétorique
+Chez Satan, le rusé doyen,
+Jette! tu n'y comprendrais rien,
+Ou tu me croirais hysthérique.
+
+Mais si, sans se laisser charmer,
+Ton oeil sait plonger dans les gouffres,
+Lis-moi, pour apprendre à m'aimer;
+
+Âme curieuse qui souffres
+Et vas cherchant ton paradis,
+Plains-moi!... Sinon, je te maudis!
+# Gúzellík
+Túş hewkelí gibí gúzelmen, ey insan!
+Kókíregím, herkez sîra man ezílgen,
+Tîpkî madde gibí soñsîz, índemegen,
+Ilhamdîr şayirge, sewdadîr yaratkan.
+
+Zengerlíkte saklî ebúlkewúlmen men;
+Koşîp kardan ğúrek kuwnuñ ak rengíne,
+Haleketní şekmem kîbîrdatsa ğerge;
+Men heş te ğîlamam we bírwakît kúlmem.
+
+Uluw abideler istiyare algan
+Azam turuşumnuñ karşîsînda ozan
+Ómír boyî okîp, o túketír hayat.
+
+Ke bar, mením, ğuwaş aşîk bayîltmaga
+Nezih kúzgúlerím, hoş kaytîmlî ayna:
+Kózím - engin, aydîn, meñgí bír káyinat. — La Beauté
+Je suis belle, ô mortels! comme un rêve de pierre,
+Et mon sein, où chacun s'est meurtri tour à tour,
+Est fait pour inspirer au poète un amour
+Eternel et muet ainsi que la matière.
+
+Je trône dans l'azur comme un sphinx incompris;
+J'unis un coeur de neige à la blancheur des cygnes;
+Je hais le mouvement qui déplace les lignes,
+Et jamais je ne pleure et jamais je ne ris.
+
+Les poètes, devant mes grandes attitudes,
+Que j'ai l'air d'emprunter aux plus fiers monuments,
+Consumeront leurs jours en d'austères études;
+
+Car j'ai, pour fasciner ces dociles amants,
+De purs miroirs qui font toutes choses plus belles:
+Mes yeux, mes larges yeux aux clartés éternelles!
+# Ğaş dew kîz
+Tírí-ğanlî Tuwa, eskí bír zamanlar,
+Hergún şelbegenlí bala tuwurganda,
+Yaşar edím bír ğaş dew kîz man barabar,
+Mîşîktay heweslí katun ayagînda.
+
+Kórer edím abat ğanîn man vuğutun,
+Óser edím azat korkînş oyînînda.
+Ğaldap kózlerínde, denep yaşlî buwun,
+Tabar edím tuman góñíl alazînda.
+
+Azim píşímlerín kîdîrayîm kaársíz,
+Koğa tíz şewúne tîrmanayîm sessíz,
+Bazî-bazî, ğazda, kúneş píşírgende,
+
+Ğatîp uzanganda boylay bozkîrlarga,
+Tóşín kólgesínde ketermen yukuga
+Dertsíz bír kóy gibí daklar etegínde. — La Géante
+Du temps que la Nature en sa verve puissante
+Concevait chaque jour des enfants monstrueux,
+J'eusse aimé vivre auprès d'une jeune géante,
+Comme aux pieds d'une reine un chat voluptueux.
+
+J'eusse aimé voir son corps fleurir avec son âme
+Et grandir librement dans ses terribles jeux;
+Deviner si son coeur couve une sombre flamme
+Aux humides brouillards qui nagent dans ses yeux;
+
+Parcourir à loisir ses magnifiques formes;
+Ramper sur le versant de ses genoux énormes,
+Et parfois en été, quand les soleils malsains,
+
+Lasse, la font s'étendre à travers la campagne,
+Dormir nonchalamment à l'ombre de ses seins,
+Comme un hameau paisible au pied d'une montagne.
+# Ğañîdúniya'nîñ bír katununa
+Kúneş sîypalagan múslí memlekette,
+Tamîzdîrîp turgan kózge erínşeklík,
+Palmiyerler astî, ğîpkîn tereklíkte,
+Saklî almenílí katun man kóríştík.
+
+Tení biyaz, sîğak; ğazibelí esmer
+Neğip hawa taşîr aylandîrsa moynîn,
+Uzun boylî, inğe, awğî gibí ğúrer,
+Tebessúmí ğuwaş, ekí kózí emín.
+
+Eger barsañ, hanîm, motlîk ulkasîna,
+Yeşíl Lwar'nîñ boyî, Sen'níñ ğagasîna,
+Eskí saraylarga yaratîlgan dúlber,
+
+Elbet tuwar saga saywan kólgesínde
+Biñlerğe şiirler ğîraw ğúregínde,
+Kózleríñe ozan esaretke túşer. — À une dame créole
+Au pays parfumé que le soleil caresse,
+J'ai connu, sous un dais d'arbres tout empourprés
+Et de palmiers d'où pleut sur les yeux la paresse,
+Une dame créole aux charmes ignorés.
+
+Son teint est pâle et chaud; la brune enchanteresse
+A dans le cou des airs noblement maniérés;
+Grande et svelte en marchant comme une chasseresse,
+Son sourire est tranquille et ses yeux assurés.
+
+Si vous alliez, Madame, au vrai pays de gloire,
+Sur les bords de la Seine ou de la verte Loire,
+Belle digne d'orner les antiques manoirs,
+
+Vous feriez, à l'abri des ombreuses retraites
+Germer mille sonnets dans le coeur des poètes,
+Que vos grands yeux rendraient plus soumis que vos noirs.
+# Heş deñíşmez
+Sorar edíñ: "Kaydan bo kuğurlî kaswet,
+Deñíz gibí kara, ğalañ tomar míngen?"
+- Harman basîp şîksa góñíl aşmaz heş bet,
+Yamandîr yaşamak. Bellí, herkez bílgen,
+
+Adiy bír awuruw, sîr taşîmaz bír sel,
+Seníñ gibí aydîn we herkezge bebek. 
+Bazgeş aktaruwdan meraklangan gúzel,
+Sesíñ tatlî lákin endí susmañ kerek.
+
+Susup kal sen ğahil! Illa mesut ğúrek!
+Kúlgen bala yúzí! Tuwul ómír, taa bek,
+Ğuka baglantîsîn ğege ólím bízge.
+
+Taşla góñílímní yalan man mest bolsîn,
+Gúzel bír túş gibí  kózleríñe dalsîn,
+Yuklap uzun yukî kírpík kólgesínde. — Semper eadem
+"D'où vous vient, disiez-vous, cette tristesse étrange,
+Montant comme la mer sur le roc noir et nu?"
+- Quand notre coeur a fait une fois sa vendange
+Vivre est un mal. C'est un secret de tous connu,
+
+Une douleur très simple et non mystérieuse
+Et, comme votre joie, éclatante pour tous.
+Cessez donc de chercher, ô belle curieuse!
+Et, bien que votre voix soit douce, taisez-vous!
+
+Taisez-vous, ignorante! âme toujours ravie!
+Bouche au rire enfantin! Plus encor que la Vie,
+La Mort nous tient souvent par des liens subtils.
+
+Laissez, laissez mon coeur s'enivrer d'un mensonge,
+Plonger dans vos beaux yeux comme dans un beau songe
+Et sommeiller longtemps à l'ombre de vos cils!
+# Kayriyadiy kokî
+Kózím kapap kúzúñ sîğak akşamînda,
+Renksíz kalgan kúneş otîn órseñletken
+Dewlet ğagalarî geşer kózlerímden
+Ğîllî kókíregíñ músún koklaganda;
+
+Tuwa adasîndan erínşegíñ ózí;
+Hoş, nezzetlí ğemíş, kórílmegen terek;
+Delíkanlî, ğígít, inğe, ğanlî erkek,
+Kîskaayaklînîñ temízkaálplí kózí.
+
+Kokîñ alîp kírdím tîlsîm dúniyaga,
+Tírek-ğelkeñ kórdím kîsîp limanlarda,
+Meğal-takat kettí dalga-deñízlerde,
+
+Yeşíl hint-kurmasî kokîy murunumda,
+Gezíp hawalarda tolgan tanawuma,
+Deñízğíler ğîrî íşím men bírleşe. — Parfum exotique
+Quand, les deux yeux fermés, en un soir chaud d'automne,
+Je respire l'odeur de ton sein chaleureux,
+Je vois se dérouler des rivages heureux
+Qu'éblouissent les feux d'un soleil monotone;
+
+Une île paresseuse où la nature donne
+Des arbres singuliers et des fruits savoureux;
+Des hommes dont le corps est mince et vigoureux,
+Et des femmes dont l'oeil par sa franchise étonne.
+
+Guidé par ton odeur vers de charmants climats,
+Je vois un port rempli de voiles et de mâts
+Encor tout fatigués par la vague marine,
+
+Pendant que le parfum des verts tamariniers,
+Qui circule dans l'air et m'enfle la narine,
+Se mêle dans mon âme au chant des mariniers.
+# Geşken bír apakayga
+Etrafîmda sokak sagîrday bakîra.
+Gamlî, kara kíygen, inğe, uzun boylî
+Bír apakay geştí, aliyğenap kolî
+Eklím-kayuw tutup sallay alay yakka.
+
+Zarif, letafetlí, hewkeldiy bağagî.
+Ğutum-ğutum íştím, delíğe kózínden,
+Burum saklap turgan kurşunday kóklerden,
+Óttírúwğí hewes, bayîltuwğî tatlî.
+
+Ğalkîn… soñra keşe! Ey, geşúwğí gúzel,
+Men ğañîdan tuwdum bírden nazarîñdan,
+Tek mí imkáanîmîz kóríşmege, ezel?
+
+Başka yerde, uzak! Bek keş! Heşbírzaman!
+Kaydan keldíñ bílmem, ğolîm bílmezsíñ sen,
+Ey, imkáanlî sewda, bonî bílíp geşken. — À une passante
+La rue assourdissante autour de moi hurlait.
+Longue, mince, en grand deuil, douleur majestueuse,
+Une femme passa, d'une main fastueuse
+Soulevant, balançant le feston et l'ourlet;
+
+Agile et noble, avec sa jambe de statue.
+Moi, je buvais, crispé comme un extravagant,
+Dans son oeil, ciel livide où germe l'ouragan,
+La douceur qui fascine et le plaisir qui tue.
+
+Un éclair... puis la nuit! — Fugitive beauté 
+Dont le regard m'a fait soudainement renaître,
+Ne te verrai-je plus que dans l'éternité?
+
+Ailleurs, bien loin d'ici! trop tard! jamais peut-être!
+Car j'ignore où tu fuis, tu ne sais où je vais,
+Ô toi que j'eusse aimée, ô toi qui le savais!
+# Kan şeşmesí
+Bazî-bazî kanîm dalgalanîp agar.
+Íşín tartîp turgan bír şeşmege uşar.
+Şîrîltîsî bellí, kulagîmda şîñlay,
+Ğara karap kolîm boşka kewde ğoklay.
+
+Geşse kaldîrîmlar ast-úst, ada-ada,
+Kaytar túm kasaba kúreş miydanîna,
+Her suwsagan ğanîñ suwsuzlugun atar,
+Tekmíl tuwa yúzún kîzîl man boyalar.
+
+Aldatuwğî şarap denep íştím sîkî,
+Íşímdekí korkîm yuklar dep bír kúnge;
+Şarap bír aydînlîk kulak man kózlerge!
+
+Súyúp te karadîm unutturgan yukî;
+Iyne-iyne tóşek lákin súyúw maga
+Íşírmege yarar maw oraspîlarga! — La Fontaine de sang
+Il me semble parfois que mon sang coule à flots,
+Ainsi qu'une fontaine aux rythmiques sanglots.
+Je l'entends bien qui coule avec un long murmure,
+Mais je me tâte en vain pour trouver la blessure.
+
+À travers la cité, comme dans un champ clos,
+Il s'en va, transformant les pavés en îlots,
+Désaltérant la soif de chaque créature,
+Et partout colorant en rouge la nature.
+
+J'ai demandé souvent à des vins captieux
+D'endormir pour un jour la terreur qui me mine;
+Le vin rend l'oeil plus clair et l'oreille plus fine!
+
+J'ai cherché dans l'amour un sommeil oublieux;
+Mais l'amour n'est pour moi qu'un matelas d'aiguilles
+Fait pour donner à boire à ces cruelles filles!
+# Kúz sonetí
+Billúr gibí aydîn seníñ kózíñ ayta:
+"Demek, nemní súydúñ? Karíp oynaş, mením"
+- El-kol aşîp sussañ! Kuyludur ğúregím,
+Elk haywanîñ aşîk góñílínden başka,
+
+Óz ğennemiy sîrîn aştîrmayğak bola,
+Ne "ayya"-nîñ kolîn, yukî man aldatkan,
+Ne de kambîr falîn, ateş men yazîlgan.
+Góñíl, men, kaptîrmam we ruh ğanîm ğaga!
+
+Súyúş men men yawaş. Kutugunda Sewda,
+Bela ğayîn kergen, saklî, karañgîda.
+Bílemen bar onîñ eskí zewzeklerí:
+
+Helak, korkî, wahşet! - Ey, ak kasîm-patî!
+Tuwulsuñ mî kúneş, kúzde uşap maga?
+Ey, ak-şagan patî, salkîn Kasîm-patî! — Sonnet d'automne
+Ils me disent, tes yeux, clairs comme le cristal:
+"Pour toi, bizarre amant, quel est donc mon mérite?"
+- Sois charmante et tais-toi! Mon coeur, que tout irrite,
+Excepté la candeur de l'antique animal,
+
+Ne veut pas te montrer son secret infernal,
+Berceuse dont la main aux longs sommeils m'invite,
+Ni sa noire légende avec la flamme écrite.
+Je hais la passion et l'esprit me fait mal!
+
+Aimons-nous doucement. L'Amour dans sa guérite,
+Ténébreux, embusqué, bande son arc fatal.
+Je connais les engins de son vieil arsenal:
+
+Crime, horreur et folie! - Ô pâle marguerite!
+Comme moi n'es-tu pas un soleil automnal,
+Ô ma si blanche, ô ma si froide Marguerite?
 `;
