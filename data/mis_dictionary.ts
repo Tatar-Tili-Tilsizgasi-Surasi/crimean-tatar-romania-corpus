e2131bbfa4dbeef358f93d90b1be87e4172baa9a
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-14 22:19:37 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -5,6 +5,7 @@ Beyğe s. Beijing, China.
 Çikago s. Chicago.
 çm s. (abreviere pentru „çentimetre” = centimetru) cm
 dinozor s. dinozaur.
+Fukuşima s. (topon.) Fukushima, Japan.
 Ğadîlar Bayramî s. Halloween.
 Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
