d5acc8b70069c70c302796fb464edb4c3e3299d2
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-15 16:24:59 +0100

    Update foreign_words_adoption.ts

--- data/foreign_words_adoption.ts
+++ data/foreign_words_adoption.ts
@@ -3,6 +3,16 @@ export const foreignWordsAdoptionInfo: string = `
 Introduction: The loan words from Arabic and Persian are mostly adopted in authentic language, while the other words from other languages (like European languages) are written in academic language.
 Example: Çekiye (not Şekiye!), kalçiyum (not kalşiyum!), soçiyalism (not soçiyalísm, soşîyalísm or different!).
 
+In Crimean Tatar (Standard) most of the loans are from Russian or Ukrainian, in Crimean Tatar (Romania) the loans are from Romanian.
+
+Why to use loans?
+To have more modern terms.
+
+Which kind or which one of loans words (Romanian loans) to use?
+Terms wich do not exist in the language, example ençiklopediya (enciclopedie)
+Try to use loans if you don't know the Dobrujan Tatar translation, don't create made up terms. Like if you dont have translation for dinosaur don't create word like "korkînş kertenkele", get the loan from Romanian, in this case dinozaur → dinozawîr
+
+
 The Adoption of foreign words:
 Note: The letters B, D, G, Ğ, I, Ó, U, Ú, V typically do not occur as the final letter of a word (with some exceptions: "ald" and "dad"). Word-initial Ñ ñ is dissonant (no words begin with it).
 
