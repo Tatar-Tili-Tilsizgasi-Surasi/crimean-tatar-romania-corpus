77616236b9542ab3460013746a906af8d91daa36
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-15 13:03:36 +0100

    Add files via upload

---
+++ data/foreign_words_adoption.ts
@@ -0,0 +1,42 @@
+
+export const foreignWordsAdoptionInfo: string = `
+The Adoption of foreign words:
+Note: The letters B, D, G, Ğ, I, Ó, U, Ú, V typically do not occur as the final letter of a word (with some exceptions: "ald" and "dad"). Word-initial Ñ ñ is dissonant (no words begin with it).
+
+Word end -b becomes -p: Bob → Bop
+Word end -d becomes -t: Sand → Sant
+Word end -g becomes -k: ping-pong → pink-ponk
+Word end -ğ (English: -ge) becomes -j: George → Ğorj
+Word end -i becomes -iy: Gavarni → Gavarniy, Bali → Baliy
+Word end -u becomes -uw or -úw (depends on vowel harmony): Eminescu → Emineskúw
+Word end -v becomes -f or -w.
+
+Examples:
+
+California → Kaliforniye
+Virginia → Virjiniye
+Chicago → Çikago
+Texas → Teksas
+Austin → Awustin
+Peru → Peruw
+John → Jan
+Mihai → Mikayil
+David → Dawut
+Eugene Sue → Ewjen Súy
+Berlioz → Berliyoz
+George Sand → Ğorj Sant
+de Musset → de Músset
+Dumas → Dúmas
+Gautier → Gotiye
+the Goncourt Brothers → Gonkurt kardaşlarî
+Gavarni → Gavarniy
+Sainte Beuve → Sent Bóv
+Liszt → List
+Felix → Feliks
+Scheffer → Şeffer
+Delacroiz → Delakruwa
+Horace Vernet → Horas Vernet
+Julius Slowacki → Juliyus Sîlowaskiy
+Count Krasinski → Kont Kîrasinskiy
+Adam Mickiewicz → Adem Miskiyebiç
+`;
