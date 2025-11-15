2925774d26ab12e9e6a5f80ef36b2cb484b70f1c
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-15 13:58:16 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -1,7 +1,5 @@
 export const misDictionaryRawText: string = `
 Awustin s. (topon.) Austin.
-Baliy s. (topon.) Bali, Indonesia.
-Beyğe s. Beijing, China. 
 Çikago s. Chicago.
 çm s. (abreviere pentru „çentimetre” = centimetru) cm
 dinozor s. dinozaur.
@@ -10,8 +8,6 @@ Fukuşima s. (topon.) Fukushima, Japan.
 Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
 ok s. (abreviere pentru „okka” = kilogram) kg 
-Peniye s. (topon.) Phenian (Pyongyang), Coreea de Nord.
-Sewúl s. (topon) Seul (Seoul), Coreea de Sud.
 Şînğa s. (topon.) Xinjiang, China.
 Teksas s. (topon.) Texas.
 Virjiniye s. (topon.) Virginia.
