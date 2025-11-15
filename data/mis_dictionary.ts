7471954deaa8dd63da4ed73b4a53d6a1c4f1fd0b
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-15 08:12:37 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -11,7 +11,7 @@ Kaliforniye s. (topon.) California.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
 ok s. (abreviere pentru „okka” = kilogram) kg 
 Peniye s. (topon.) Phenian (Pyongyang), Coreea de Nord.
-Sewúl s. (topon) Seul, Coreea de Sud.
+Sewúl s. (topon) Seul (Seoul), Coreea de Sud.
 Şînğa s. (topon.) Xinjiang, China.
 Teksas s. (topon.) Texas.
 Virjiniye s. (topon.) Virginia.
