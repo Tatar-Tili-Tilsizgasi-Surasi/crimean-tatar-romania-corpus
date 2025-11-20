016b4f9be0150b503920b5b6347c40fc7d96993b
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-11-20 22:26:40 +0100

    Update 44_2015.ts

--- data/Nazar_Look/44_2015.ts
+++ data/Nazar_Look/44_2015.ts
@@ -1,3 +1,160 @@
 
 export const nazarLook44Entries: string = `
+# Tomrîğa Kîrîm Tatarlarîñ turuş-mamuriyet meğmuwasî — Attitude and culture magazine of Crimean Tatars in Dobruja
+# Sene — Year
+# Yarîyîl — Semester
+# Sayî — Issue
+# Kóstenğe — Constanţa
+# Ğangan ğerge baskanda (II) 
+Başsóz - II-ínğí bólím 
+1944 senesíñ kúzínde 
+Almaniyeníñ sîrt kókyúzín boz bulutlar kararta edí.
+Bulutlarnî vîjîldagan bír ğel alay yakka dagîtîp tura 
+edí. Kuwuşlî binada Naziy doktorlar kúntuwar man 
+kúnbatarga karşî soñ sogîşka ázírlengen Waffen 
+SS motorlî túmenge alînağak adaylarnî kózden 
+geşíríp inğeliy edíler. 
+- Ístepan Osmanop! 
+Babamnî faraziy atî man uzun boylî SS bír asker 
+doktor şakîrdî. 
+-Jawohl mein Herr!
+1
+ dep kurnazğa ğewap berdí o, 
+şîr-şîpalak hálínde tebeşír men sîzîlgan dayireníñ 
+ortasîna bír adîm atîp. 
+Etrafındakî ğarîklar neşter gibí ğîltîray edí. Şúndí 
+SS doktor aktaruwğî kózlerín babamîñ arkasîna 
+tíkken, o da oga aylandî. 
+- Aylan arkaña! kestíríp buyurdî o almanğa. 
+Babam kóz oynatmadan-kîrpmadan buyuruknî 
+yeríne akeldí. O kaderín karşîsînda ázírleníp tura 
+edí, tîpkî kewdesíñ mimarlîgî ólím káátín yerín 
+tutkanday. 
+Duwarlarîñ boyînda SS subaylarî - Hitler’níñ 
+súretlendírmesíne kóre “Kurupp bolatî kadar sert, 
+etík terísí kadar kattî, tazî kadar hîzlî”, yúksek 
+tertíplí erkekler sayîlgan bír takîm - kúneşíñ kîygaş 
+ğarîgînda otîra edí. Seviyelík urbalarîna tígílgen 
+run tamgalarî, eskí hem korkîtuwğî tamgalarga 
+uşap kete edíler. Kímlígínde fazla yalan 
+uydurulganîn babamîñ kayet árúw kaberí bar edí, kóp ketmiyğegín, yakînlarda bonîñ hesap zamanî 
+keleğegín de bíle edí. 
+Tuwum yerí Polak kasabasî Nawgurdektekí 
+rawinlerníñ okîganlarîn sáde şo wakît akîlîna 
+akeldí: “Hazretiy Ibrahimge Allah şay dedí… Sen 
+súnnet terísí etíñní súnnet etersíñ, bo bízím 
+antlaşmamîznîñ işaretí sayîlîr. We nesíller boyî 
+bútún erkekleríñíz sekíz kúnní toltîrganda súnnet 
+bolağaktîr”. Allaga bagîşlangan súnnet adetí şúndí 
+Mólek’ke2
+ bagîşlangan bír kurban bolağak edí. 
+“Aylan arkañ man!” dep bakîrdî sabîrsîzlangan SS 
+subayî. Yúksek boylî penğírelerden ğarîk 
+mîzraklarî kíre edí. 
+Obír odada kuwetlí kewdelí askerler urbasîn 
+şîgarîp taşlana edí. Odanîñ hawasî korkînş man 
+tolî. Babamnîñ kulagîna bír kalîn it mîrîltîsî keldí. 
+Oñ yagînda, aşîk penğírege tozday bír kîraw 
+kongan, sokaklarnî inğe buzlar kaplagan. 
+Babamîñ akîlîndan geşken kewde şektírmelerí onî 
+kuturta yazayatîr edí. Başînda miyí o kadar şíşken 
+ke kafatasîn patlatağak. Az kaldî herşiy miydanga 
+şîgağak. SS-şíler kómílgen geşmíşín ortaga şîgarîp 
+onî íşkenğe taktasîna salîp tartağaklar. Ket-kete 
+solîşîn almaga taa bek zorlana, miydesí de 
+turmadan talpînîp atîla. SS-şí subay ekí kózín oga 
+tíkken, başîna kalîn, tokîlgan bír tayak man 
+wurganday. Ğer ğarîlsa, babam ğerge kíríp 
+tóşemeleríñ astînda ğaşînağak. Kewde azalarî 
+sáde Naziy Almaniyede kader yazîsîna sebep bola 
+edí, şonday bír kírtleşme başka bír yerde taba-
+almaz edíñ. 
+“Solga aylan!” dep ayttî SS-şí aktaruwğî kózí men. 
+-Jawoh! dep bírtaa ayttî babam da, nezaketlí 
+ğewabî man raátsízlígín kóstermemege ogîraşîp. 
+Şúndí de korkînîñ taa zewuklî yúzí tabiyat kayidesí
+edí. 
+Şúndí bírden akîlînda uyumsuz sünnetíñ kayîrî 
+şîñlap başladî: “Ay, sen añlayîşlî, soñsîz sabîrlî,
+súygílí Allaga şúkúr et. Bo balaşîk saw bolsîn, ósíp 
+balaban bolsîn. Allanîñ karşîsînda o kalay başîn 
+aşaga algan bolsa, hep şonday etíp Kanunnîñ korşalamasînda tabîlsîn, akîtî kîyîlsîn, yakşî şiyler 
+yapsîn.” 
+Onî başîndan aşagaşîk aktargan soñ, Naziy doktor 
+masasîna otîrîp kolîna kúmúş uşlî tolmakalemín 
+aldî we sîradan yazîsî man boş bír káát parşasîna 
+bírşiyler yazîp aldî. Bo arada babam da heş 
+kîymîldamadan tora aldîna karay edí. 
+“Şemberín tîşîna şîk” dep emír berdí doktor. 
+Íşínden şonday kelíp, babam şîpalaklîgîn ústín 
+órttí. 
+“Ístepan Osmanop!” dep keldí obír doktornîñ 
+buyurugî. 
+Bo dúniyada ğañgîz kalgan suwuk bír buz 
+parşasîday aldga şîktî. Ómír boyî onî hayatta 
+tutkan tek yîkpaldîr, kaderíñ şîmarîk kumarî. Hem 
+tîpkî Demokles gibí, onîñ da eñ usta ayak oyînî 
+şírígen kînap man asîlgan kîlîşlarîñ astînda edí. 
+* * * 
+Nasîl kóríne, bír zamanlar heş yalan ayta-almagan 
+bír rawin talebesí bo yeşiwa bókerní soñ yîllarnîñ 
+herbír hareketí suwburumîñ ortasîna tarttî, onîñ 
+her adîmî ya Waffen SS-ke koşîlmasîna, ya 
+şúphesíz ólímge taa yakîn akele edí. 
+Waffen SS-şíler tabiy ke yúz sene ewel, 1834 
+yîlînda, Heinrich Heine taş atîp suratlagan 
+kîyamette sogîşağak şo “gîladiyatorlar” edí. 
+Madam de Stael’níñ Alman romantiklígíñ bolîmlî 
+resímíne tepkí etíp yazgan “Almaniyeníñ diní men 
+felsefesí torasînda” eserínde nefes kesken keleğek 
+kóríşí men Heine şolarnî ilan ettí: “Alman kók 
+gúdúrtísí tabiy ke hakkîykattan Almandîr; kayet 
+şalt tuwuldîr, yawaştan dúmbúrder geşer. Lákin 
+kúnníñ bírínde dúniya tewúkesínde sîraústí bír 
+ğalkînnîñ sesí kelse, o zaman kaberíñ bolsîn ke 
+Alman gúdúrtísí barağak yeríne yetíşíp soñînda 
+tamgasîn taşlap wurgandîr. Bo gúdúrtíníñ 
+sesínden kókten ğerge ólí búrkút túşer, Afrika’nîñ 
+eñ uzak şóllerínde de arslan kuyrugîn ayaklarîñ 
+arasîna tîgîp kîral yuwasîna kaşîp saklanîr. Alman 
+saknesínde oynalağak temaşaga kóre Fírenk 
+Inkilabî sáde bír temíz súygí masalîday kalîr.” 
+Heine, kayriytabiy sezúwí men, kagîşmasînda 
+egím ádetleríníñ ne kadar kolay şatlaganîn añladî. 
+Almaniye şonday bír uşurum bolağagîn başka kím 
+añlasîn? 
+Şúndí de babam wahşiyğe yeríne kelgen şo 
+aldkórúwníñ ortasînda tabîla edí. Alamaniye atîn 
+taşîgan bo ahláak şólínde o ğañgîz konagîn 
+kurmaga şalîşa edí. Bo mekáanda ózín korîmak 
+meğburiyetínde edí. Tîpkî gúwenlík awî 
+kullanmadan ğípte oynagan bír ğambazday, 
+kîymetlí şiyílerí tek ğaşlîgî man şáresízlígí, yañgîş 
+atkan síptí adîmînda ğípten túşíp uşurumîñ 
+túbínde param-parşa bolağagîndan babamîñ kayet 
+árúw kaberí bar edí. 
+Babam yîllarğa kuwalanîp talangandîr, tek 
+ğañgîzlîkka we aksiyetke tolî bír soñîşka barmak 
+úşún, SS-şílerge koşîlgan bír rawin talebesí. Onîñ 
+bo hayaliy íşín ne aşîklar eken, hem bo togîşîp 
+turgan ayîrî dúniyalarnî ka-típ baş etken eken? 
+Men balalîgîmnî babam ka-típ óz nesíl sílsílesín 
+koparîp atîp başka yabanğî bír sílsílenamege ózín 
+zor man yakîştîrganîn úyreníp geşírdím. Hem 
+kurnazlîk sanatîn, hem ğaşînmasîn úyreníp 
+sînîrsîz-oransîz şaytan kóşesíne ka-típ barganî 
+miydanga şîktî. Soñînda Paul Valery’níñ 
+aytkanlarînîñ karañgî añlamîna barîp şo sózler 
+men ne demek ístegenín añladîm: “Men 
+samimiyet men inanîrman ke başka insanlarnîñ 
+hayatîn yaşay-almagan bír kíşí ózíníñ hayatîn da 
+yaşay-almaz.” 
+(Dewamî keleğekke)
+
+ 1
+ Almanğa: Ebet, efendím! (terğumeğí ilavesídír) 
+ 
+2
+ Mólek - hem bír tañrî atîdîr, hem bír ateş kurbanîñ 
+atîdîr.(terğumeğíníñ ilavesí)
 `;
