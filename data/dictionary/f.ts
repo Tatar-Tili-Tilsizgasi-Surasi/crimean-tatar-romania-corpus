47e4c892b40a738c4327de125573b8363bb43263
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-03 10:27:54 +0100

    Add Turkish dictionary entries to rawDictionaryText

--- data/dictionary/f.ts
+++ data/dictionary/f.ts
@@ -1,1 +1,43 @@
-export const rawDictionaryText = ``;
+export const rawDictionaryText = `
+fa s. (muz.) fa. 
+faal adj. activ; energic. 
+faaliyet s. activitate; intensitate. //●faaliyet alanî — domeniu de activitate. ●faaliyet tarzî — mod de a acŃiona. ●faaliyet ústí — asupra faptului; în flagrant. ●faaliyetten soñra — după faptă; post factum. ●gúwenğîguw faaliyetí — acŃiune teroristă. ●mekteptîşî faaliyet — activitate extraşcolară. ●partitníñ faaliyetí — (pol.) activitatea partidului. ●siyasiy faaliyet — demers diplomatic. 
+faaliyetsíz adj. inactiv. 
+faaliyetsízlík s. inactivitate. 
+faaliyetşí s. făptaş. 
+faallaşkan adj. activat; intensificat. 
+faallaşma s. intensificare; înteŃire. 
+faallaşmak v.i. a se activa; a se intensifica. 
+faallaşmalî adj. intensiv. 
+faallaştîrmak v.t. a activa; a intensifica. 
+faallaştîruw s. activare; activizare; intensificare. 
+faallaştîruwğî adj. intensificator. 
+faallaşuw s. intensificare; înteŃire. 
+faallîk s. activitate; intensitate. 
+faallîksîz adj. inactiv. 
+faallîksîzlîk s. inactivitate. 
+faallîkşî s. făptaş. 
+fabrika s. fabrică. // ●káát fabrikasî — fabrică de hârtie. ●ótmek fabrikasî — fabrică de pâine. ●şeker fabrikasî — fabrică de zahăr. ●şelík/polat fabrikasî — oŃelărie. 
+fabrikağî s. fabricant. 
+fabrikant s. fabricant. 
+fabrikasiya s. fabricare. 
+fabrikat s., adj. fabricat. 
+fabula s. (lit.) fabulă. 
+fadîl I. adj. virtuos; merituos. II. s. favoare; privilegiu; binefacere. 
+fagoçitoza s. (fiziol.) fagocitoză. 
+fagot s. (muz.) fagot. 
+fagotşalgan s. (muz.) fagotist. 
+fagotşî s. (muz.) fagotist. 
+fağiya s. dramă; tragedie; dezastru; catastrofă; calamitate; nenorocire. //●kúldúrmelí fağiya — tragicomedie. ●ne fağiya! — ce tragedie! ; 
+fağiyağî s. tragedian. 
+fağiyalî adj. dramatic; tragic; dezastruos; catastrofal. 
+fahiş adj. imoral; obscen. 
+fahişe s. prostituată. 
+fahişekana s. casă de toleranŃă. 
+fahişelík s. prostituŃie. 
+fahişlík s. imoralitate; obscenitate. 
+fahiz s. (anat.) gambă; pulpă. 
+fahriy adj. onorific; de onoare. 
+fakat conj. dar; însă. // ●onî bekledím fakat kelmedí — l-am aşteptat dar nu a venit. 
+fakir s. fachir; magician; iluzionist; scamator.
+`;
