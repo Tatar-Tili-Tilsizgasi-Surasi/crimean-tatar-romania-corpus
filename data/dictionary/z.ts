6ced746693473ed99029efdd7085e22e5d6087b1
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-05 21:22:27 +0100

    Update z.ts

--- data/dictionary/z.ts
+++ data/dictionary/z.ts
@@ -1,1 +1,42 @@
-export const rawDictionaryText = ``;
+export const rawDictionaryText = `
+zabit s. ofiŃer. 
+zabît s. proces-verbal. 
+zabîta s. poliŃie. 
+zabîtname s. proces-verbal. 
+zadar s. străinătate. 
+zade I. adj. de origine. II. s. fiu; descendent. 
+zadegen s. nobil; aristocrat. 
+zadegenlík s. nobilime; aristocraŃie. 
+zafer s. victorie; triumf. // ●zafer tağî — arc de triumf. 
+zaferğí adj. victorios; învingător; biruitor. 
+zaferşalgan adj. victorios; biruitor; triumfător. 
+zafiyet s. sfârşeală; epuizare (fizică). 
+zagalmay s. (relig.) cruce; crucifix; semnul crucii. // ●zagalmay seferí — cruciadă. 
+zagalmayanasî s. naşă. 
+zagalmayatasî s. naş. 
+zagalmaybalasî s. fin; fină. 
+zagalmayğî s. (ist.) cruciat. 
+zagalmaykîzî s. fină. 
+zagalmaylama s. răstignire; crucificare. 
+zagalmaylamak v.t. a răstigni; a crucifica. 
+zagalmaylangan adj. răstignit; crucificat. 
+zagalmayulî s. fin. 
+zagar s. burtă; pântece. 
+zagara s. guler de blană. 
+zagarkarîn adj. burtos; pântecos. 
+zagarlî adj. burtos; pântecos. 
+zaharina s. zaharină. 
+Zahide s. (antrop. f., arab.) “Cea austeră”. 
+zahir I. adj. 1. clar; evident. 2. extern; exterior; din afară. 3. luminos; strălucitor. II. adv. la vedere; la prima vedere; după aparenŃă. III. s. exterior; înfăŃişare; aparenŃă. // ●zahir bolmak — a se manifesta; a se arăta. 
+Zahir s. (antrop. m., arab.) "Cel luminos" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+Zahir, El- s. (relig., arab.) "Cel neîndoielnic"; “Cel a cărui existenŃă este vădită şi limpede” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah). 
+zahire s. provizii; alimente. 
+Zahire s. (antrop. f., arab.) “Cea luminoasă”. 
+zahirğí s., adj. formalist. 
+zahirğílík s. formalism. 
+zahiriy adj. 1. aparent; formal. 2. (fig.) fals; prefăcut. 
+zahiriyleşken adj. 1. exteriorizat. 2. formalizat. 
+zahiriyleştírme s. 1. exteriorizare. 2. formalizare. 
+zahiriyleştírmek v.t. 1. a formaliza. 2. a exterioriza. 
+zahirlík I. adj. claritate; evidenŃă. II. s. luminozitate; strălucire.
+`;
