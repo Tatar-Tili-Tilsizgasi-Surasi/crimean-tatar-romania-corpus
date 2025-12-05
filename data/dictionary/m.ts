c3faca0ead936bc49d7d47429fa09f9c89c803a8
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-05 21:29:55 +0100

    Update rawDictionaryText with new definitions
    Added new entries to the raw dictionary text.

--- data/dictionary/m.ts
+++ data/dictionary/m.ts
@@ -1,2 +1,33 @@
 export const rawDictionaryText = `Menim adım... /
+ma interj. 1. Ńine!; na!; ia!; 2. (către câini) cuŃu!; 
+maada postp. în afară de…; cu excepŃia…; // ●bondan maada — în afară de aceasta. 
+Maah s. (antrop. m., arab.) "Abolitorul necredinŃei" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+maalesef adv. cu părere de rău; cu regret; din păcate. // ●maalesef bo bolmaz — din păcate asta nu se poate. 
+maarif I. adj. instructiv; educativ. II. s. 1. instrucŃie; educaŃie. 2. învăŃământ. 
+maariflendírmek v.t. a instrui; a educa. 
+maariflendírúw s. instruire; educare. 
+maariflengen adj. instruit; educat. 
+maariflenmek v.i. a se instrui; a se educa. 
+maarifsíz adj. neinstruit; needucat. 
+maarifsízlík s. lipsă de instruire; lipsă de educaŃie. 
+maarifşí s. instructor; educator. 
+maarifşílík s. instrucŃie; educare. 
+maaş s. salariu; leafă; retribuŃie. 
+maaşlî s., adj. salariat; lefegiu. 
+maatteessúf adv. cu părere de rău; cu regret; din păcate. 
+mabet s. 1. dragoste; afecŃiune; ataşament. 2. prietenie. 
+mabetílíşkíğísí s. aventurier; persoană care caută aventuri amoroase. 
+mabetílíşkísí s. aventură; legătură amoroasă întâmplătoare. 
+mabetlengen adj. îndrăgostit; amorezat. 
+mabetlenmek v.i. a se îndrăgosti; a se amoreza. 
+mabetlenúw s. îndrăgostire; amorezare. 
+mabetlí s. îndrăgostit; amorezat. //●mabetlílerníñ kawgasînday — ca supărările îndrăgostiŃilor; trecător. 
+mabetlík s. dragoste; afecŃiune; ataşament; prietenie. 
+mabut s. 1. divinitate; Dumnezeu. 2. idol. 
+MacQueen'níñ-toykuşî s. (orn.) dropia lui MacQueen (lat., Chlamy dotis macquee nii). 
+Madagaskar s. (topon.) Madagascar. 
+madagaskarlî s., adj. malgaş. 
+madam s. doamnă (formulă de adresare). 
+madde s. 1. (fiz.) materie; substanŃă. 2. element component; material. 3. (jur.) articol; paragraf. 4. subiect; chestiune. // ●asîl madde — substrat. ●ham madde — materie primă. ●kimiya maddesí — produs chimic. ●patlagan madde — material explozibil. ●uyutuwğî/şaşîrtuwğî madde — stupefiant. 
+maddeğí s., adj. (fil.) materialist.
 `;
