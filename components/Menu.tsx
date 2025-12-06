5ce546f00bb783fc27aaa2a70449fb8de23baae5
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-06 12:22:38 +0100

    Add files via upload

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -81,7 +81,7 @@ const Menu: React.FC<MenuProps> = ({ onNavigate }) => {
             className="block px-4 py-2 text-slate-700 hover:bg-slate-100 font-medium text-indigo-600"
             role="menuitem"
           >
-            Keyboard App
+            Keyboard
           </a>
           <a
             href="#"
