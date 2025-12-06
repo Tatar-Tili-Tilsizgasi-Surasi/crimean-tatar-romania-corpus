3a6fd32c21e2c193c923ae916920e2f6cdbe3b48
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-06 19:14:53 +0100

    Add raw dictionary text entries

--- data/dictionary/u.ts
+++ data/dictionary/u.ts
@@ -1,1 +1,94 @@
-export const rawDictionaryText = ``;
+export const rawDictionaryText = `
+ubudiy s. sclav; rob. 
+ubudiyet s. sclavie; robie. 
+ubudiyetşí adj. sclavagist. 
+ubur-subur adv. fremătând; palpitând; agitându-se. 
+uf interj. of!; uf!; 
+ufak adj. mic; mărunt. 
+ufaklîk s. 1. mărunŃiş; bani mărunŃi. 2. (iht.) baboi; fâŃă; plevuşcă. 
+ufak-siyenek s. (iht.) sardeluŃă; şprot (lat., Sprattus sprattus phalericus). 
+ufak-tefek s. mărunŃişuri; multe şi mărunte; chiŃibuşuri. 
+ufkiy adj. (mat.) orizontal. // ●ufkiy ğerge — (mat.) orizontală. ●ufkiy sîzîk — (mat.) linie orizontală. 
+ufuk s. orizont; zare. 
+ufuldagan adj. care oftează. 
+ufuldamak v.i. a ofta. 
+ufuldatmak v.t. a determina să ofteze. 
+ufuldaw s. oftat. 
+ufunet s. 1. duhoare; putoare; miros urât. 2. (med.) inflamaŃie. 
+ufunetlengen adj. (med.) inflamat. 
+ufunetlenme s. inflamare. 
+ufunetlenmek v.i. (med.) a se inflama. 
+ufunetlí adj. (med.) inflamat. 
+Uganda s. (topon.) Uganda. 
+ugandalî s., adj. ugandez. 
+uğa s. (anat.) coccis; noadă. 
+uğube s. 1. curiozitate; raritate; ciudăŃenie. 2. pocitanie; monstru. 
+uğubeleşken adj. pocit; sluŃit. 
+uğubeleşmek v.i. a fi pocit; a fi sluŃit. 
+uğubeleştírme s. pocire; sluŃire. 
+uğubeleştírmek v.t. a poci; a sluŃi. 
+uğubelí adj. pocit; sluŃit. 
+uğuz adj. ieftin. // ●uğuz atlatmak/kutulmak — a scăpa ieftin. ●uğuzga şîkmak — a ieşi la socoteală cu preŃul; a face banii daŃi; a ieşi ieftin. // ●taa uğuzî yok mî? — nu aveŃi ceva mai ieftin?; ●uğuz bílezík — brăŃară ieftină. 
+uğuzlaşkan adj. ieftinit. //●uğuzlaşkan mallar — (com.) solduri. 
+uğuzlaşma s. ieftinire. 
+uğuzlaşmak v.i. a se ieftini. 
+uğuzlatmak v.t. a ieftini; a reduce preŃurile. 
+uğuzluk s. ieftinire. ujin s. femeie. 
+ukaláa s., adj. pedant. 
+ukaláalîk s. pedanterie. 
+ukrayin s., adj. ucrainean. 
+ukrayinğe I. adv. în limba ucraineană. II. s. (limba) ucraineană. 
+Ukrayna s. (topon.) Ucraina. 
+ukraynalî s., adj. ucrainean. 
+ul s. fiu; fecior; băiat. // ●torînnîñ ulî — strănepot. ●yolğînîñ ulî — fiul drumeŃului. 
+ulagan adj. roşu; roşcat; roşcovan. 
+ulah s., adj. (ist.) valah; vlah. 
+ulak I. s. 1. ied. 2. (astr.) zodia capricornului. 3. mesager; curier. II. s., adj. valah; vlah. 
+Ulak-Kóy s. (topon.) ("Satul Iezilor") Vlahii (jud. ConstanŃa). 
+ulam s. 1. grup; grupare. 2. (fil.) categorie; tagmă. // ●tílsîzgasî ulamlarî — (gram.) categorii gramaticale. 
+ulama s. 1. adăugare. 2. alăturare; unire. 3. (gram.) alipire; ataşare. 
+ulamak v.t. 1. a adăuga. 2. a alătura; a uni. 3. (gram.) a alipi; a ataşa. 
+ulangan adj. 1. (gram.) alipit; ataşat. 2. a alăturat; unit. 3. adăugat. 
+ulanmak v.i. 1. (gram.) a se alipi; a se ataşa. 2. a se alătura; a se uni. 3. a se adăuga. 
+ulantî s. adaos; supliment. 
+ulaslîk s. cărpeniş; pădure de carpeni. 
+ulaş s. (gram.) gerunziu. 
+ulaşîm s. legătură; contact. 
+ulaşmak v.i. a sosi; a veni; a ajunge. 
+ulaştîrma s. 1. transport; comunicaŃie. 2. legătură; contact. 
+ulaştîrmak v.t. a duce; a transporta; a determina să ajungă (la…). 
+ulçer s. (med.) ulcer. 
+ulçerlí adj. ulceros. 
+ulema s. 1. savanŃi; oameni de ştiinŃă. 2. (la musulmani) cler; preoŃime. 
+ulga s. (mil.) avangardă. 
+ulgam s. striu; striaŃie. 
+ulgamlî adj. striat. 
+ulka s. Ńară; patrie. // ●biyak ulka — (pol.) stat neutru. ●ğelendeş ulkalar — Ńările limitrofe. ●kaysî ulkadan sîñîz? — din ce Ńară sunteŃi?; ●ulka bútúnlúgí — (jur.) integritate teritorială. ●ulkanîñ başkasabasî — capitala Ńării. ●ulkanîñ ósúwí — dezvoltarea Ńării. ●yabanğî ulkalar — Ńări străine. ●yakîn-uzak ulkalar — Ńările apropiate şi Ńările îndepărtate. 
+ulkardaş î s. frate. 
+ulkasîz s. apatrid. 
+Ulkayer s. (astr.) Pleiadele. 
+ulkum s. (gastron.) scoveargă. 
+ulşuk s. 1. băieŃel; băieŃaş; puşti. 2. (biol.) embrion. 
+ulşukbílímí s. embriologie. 
+ulşuklî adj. (biol.) embrionic. 
+ultan s. (la încălŃăminte) talpă; pingea. 
+ultimatum s. (jur.) ultimatum. 
+ultrakîska adj. (fiz.) ultrascurt. 
+ultramodern adj. foarte modern; ultramodern. 
+ultraşemeke adj. (fiz.) ultraviolet. 
+ultraviyolet adj. (fiz.) ultraviolet. 
+uluma s. urlet (de câine, de lup). 
+ulumak v.i. (d. câini, lupi) a urla. 
+ulus s. naŃiune; seminŃie. // ●ulus bayramî — sărbătoare naŃională. ●ulus egemenlígí — suveranitate naŃională. 
+uluslararasî adj. internaŃional. //●uluslararasî bílím kurultayî — congres ştiinŃific internaŃional. ●uluslararasî uzakses/telefon konîşmasî — convorbire telefonică internaŃională. 
+uluslararasîlîk s. internaŃionalism. 
+uluslaşkan adj. naŃionalizat. 
+uluslaştîrîlmak v.i. a se naŃionaliza. 
+uluslaştîrma s. naŃionalizare. 
+uluslaştîrmak v.t. a naŃionaliza. 
+ulusşî adj. 1. naŃional. 2. naŃionalist. 
+ulusşuluk s. naŃionalism. 
+uluw adj. măreŃ; grandios; impunător. 
+uluwanay s. străbunică. 
+uluwbabay s. străbunic.
+`;
