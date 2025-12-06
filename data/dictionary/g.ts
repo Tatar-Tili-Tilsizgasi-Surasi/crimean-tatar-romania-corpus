491601b8d6434e655248eb5649cd5a0374009971
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-06 19:23:54 +0100

    Update g.ts

--- data/dictionary/g.ts
+++ data/dictionary/g.ts
@@ -1,2 +1,79 @@
 export const rawDictionaryText = `
+gabarit s. (la vehicule) gabarit. 
+gabiya s. (nav.) gabie. 
+Gabon s. (topon.) Gabon. 
+gabonlî s., adj. gabonez. 
+gaddar adj. crud; nemilos; neomenos. 
+gaddarane adv. fără milă; cu cruzime; necruŃător. 
+gaddarlîk s. cruzime; sălbăticie; ferocitate. 
+gadir s. 1. persecuŃie; oprimare. 2. nedreptate; injusteŃe. 
+gadirlí adj. 1. persecutat; oprimat. 2. nedrept; injust. 
+gafa s. gafă. // ●gafa yapmak — a face o gafă. 
+Gaffar, El- s. (relig., arab.) "Cel cruŃător"; “Cel preamilos şi de păcate izbăvitor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+gafil adj. 1. neatent; distrat. 2. nepăsător; indolent; neglijent. //●gafil awlanmak — a fi luat pe nepusă masă; a fi surprins nepregătit. 
+gafilawlanma s. luare pe nepregătite; surprindere. 
+gaflet s. 1. neatenŃie; distracŃie; neluare-aminte. 2. nepăsare; indolenŃă; neglijenŃă. 
+gafletlí adj. 1. nepăsător; indolent; neglijent. 2. neatent; distrat; inadvertent. 
+Gafur, El - s. (relig., arab.) "Cel îndurător"; “Cel preamilos şi de păcate izbăvitor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+gagara, kalîn-tumşuklî- s. (orn.) corcodel cu cioc gros (lat., Podilymbus podiceps). 
+gagara, kara-moyînlî- s. (orn.) corcodel cu gât negru (lat., Podiceps nigricollis). 
+gagara, kekeşlí- s. (orn.) corcodel mare; cufundar mare; bodârlău (lat., Podiceps cristatus). 
+gagara, kíşkene- s. (orn.) corcodel mic (lat., Tachybaptus ruficollis). 
+gagara, kîzîl-moyînlî- s. (orn.) corcodel cu gât roşu (lat., Podiceps grise gena). 
+gagara, kulaklî- s. (orn.) corcodel de iarnă (lat., Podiceps au ritus). 
+gala s. gală (şi sport).
+galaksiya s. galaxie. 
+galaksiyalî adj. galactic. 
+galaktik adj. galactic. 
+galanteriya s. galanterie. 
+galebe s. victorie; biruinŃă. //●galebe şalmak/etmek — a învinge; a birui; a ieşi victorios. 
+galena s. (geol.) galenă. 
+galeriya s. (minr., arte plastice) galerie. 
+galeta s. 1. covrig. 2. pesmet; pâine uscată. 
+galibiyet s. victorie; biruinŃă; triumf. 
+galibiyetlí adj. învingător; victorios; biruitor; triumfător. 
+galibiyetşalgan adj. victorios; biruitor; triumfător. 
+galip adj. învingător; victorios; biruitor; triumfător. // ●galip kelmek — a învinge; a birui. 
+Galip s. (antrop. m., arab.) "Izbânditorul" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+galiyan s. 1. fierbere; clocotire. 2. (fig.) agitaŃie; efervescenŃă; vâlvă. 
+galiyota s. (nav.) galiotă. 
+galon s. galon. 
+galop s. galop. 
+galoş s. galoş. 
+galvanizasiya s. galvanizare. 
+galvanizasiyalî adj. galvanizat. 
+galvanometre s. (tehn.) galvanometru. 
+galvanoplastiya s. (fiz.) galvanoplastie. 
+galvanoskop s. galvanoscop. 
+gam s. tristeŃe; amărăciune; mâhnire. // ●gam aşamak — a regreta; a se întrista. ●gam tartmak — a suferi. 
+gama s. 1. (muz.) gamă. 2. gama (a treia litere din alfabetul grecesc).
+Gambiye s. (topon.) Gambia. 
+gambiyelí s., adj. gambian. 
+gamet s. (anat.) gamet. 
+gam-keder s. tristeŃe; amărăciune; supărare. 
+gamketírgen adj. întristător. 
+gamlandîrmak v.t. a mâhni; a întrista. 
+gamlangan adj. posomorât; mâhnit. 
+gamlanma s. întristare. 
+gamlanmak v.i. a se întrista; a se amărî; a se mâhni. 
+gamlanuw s. întristare. 
+gamlî adj. trist; mâhnit; abătut. 
+gamlî-kederlí adj. jalnic; mâhnit; trist; afectat. 
+gammaz adj. 1. denunŃător; delator. 2. defăimător; bârfitor; calomniator; maliŃios. 
+gammazlamak v.t. 1. a denunŃa. 2. a defăima; a bârfi; a calomnia. 
+gammazlangan adj. calomniat; defăimat; denigrat; ponegrit. 
+gammazlîk s. 1. denunŃ. 2. defăimare; calomnie; maliŃiozitate. 
+gamsele s. impermeabil; manta de ploaie. 
+gamsîz adj. 1. liniştit; netulburat; fără griji. 2. indiferent; nepăsător. 
+gamsîzlîk s. 1. linişte; tihnă; calm. 2. indiferenŃă; nepăsare. 
+Gamze s. (antrop. f., prsn.) “Cochetărie”. 
+gangster s. gangster. 
+ganim I. adj. distrugător; nimicitor. II. s. duşman; adversar; rival. 
+ganimet s. 1. trofeu (de război). 2. pradă de război; captură. 
+ganimetşí adj. profitor; acaparator. 
+ganimetşílík s. acaparare; şterpelire. 
+ganimlík s. duşmănie; adversitate; rivalitate. 
+ganiy adj. bogat; abundent; îmbelşugat; opulent. // ●ganiy góñíllí — generos; darnic. ●góñílí ganiy — mărinimos; darnic. 
+Ganiy s. (antrop. m., arab.) "Cel bogat" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Ganiy, El- s. (relig., arab.) "Cel îmbelşugat"; “Cel plin de belşug şi lipsit de nevoi” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
 `;
