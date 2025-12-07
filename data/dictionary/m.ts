ba4785819218ff50623178b4cc612368e9574182
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-07 20:01:25 +0100

    Update m.ts

--- data/dictionary/m.ts
+++ data/dictionary/m.ts
@@ -30,4 +30,214 @@ madagaskarlî s., adj. malgaş.
 madam s. doamnă (formulă de adresare). 
 madde s. 1. (fiz.) materie; substanŃă. 2. element component; material. 3. (jur.) articol; paragraf. 4. subiect; chestiune. // ●asîl madde — substrat. ●ham madde — materie primă. ●kimiya maddesí — produs chimic. ●patlagan madde — material explozibil. ●uyutuwğî/şaşîrtuwğî madde — stupefiant. 
 maddeğí s., adj. (fil.) materialist.
+maddeğílík s. (fil.) materialism. // ●dúniya tanîmlî maddeğílík — materialism dialectic.
+maddeleşken adj. materializat.
+maddeleşme s. materializare.
+maddeleşmek v.i. a se materializa.
+maddeleştírmek v.t. a materializa.
+madde-madde adv. punct cu punct; articol cu articol; paragraf cu paragraf.
+maddiy adj. material. // ●maddiy álem — lumea materială.
+maddiy-maneviy adj. material şi spiritual.
+madem conj. deoarece; fiindcă; întrucât; de vreme ce; dacă înt-adevăr.
+maden I. adj. 1. mineral. 2. metalic; de metal. II. s. 1. metal. 2. minereu. 3. mină. // ●altîn madení — mină de aur. ●maden ğatagî/tamîrî/tazîlî — zăcământ. ●maden suwî — apă minerală. ●şifalî maden suwlarî — ape minerale curative. ●tuz madení — salină.
+madenbílímğísí s. mineralog.
+madenbílímí s. mineralogie.
+madenğí s. miner.
+madenğílík I. adj. minier. II. s. minerit.
+madeniy adj. mineral; anorganic; neorganic. // ●madeniy kimiya — chimie anorganică. ●madeniy may — ulei mineral.
+madeniyleşken adj. mineralizat.
+madeniyleşmek v.i. a se mineraliza.
+madeniyleştírme s. mineralizare.
+madeniyleştírmek v.t. a mineraliza.
+madenírítken s. topitor; metalurg.
+madenírítme I. adj. metalurgic. II. s. topitorie; metalurgie.
+madenkómírí s. huilă.
+Madiyre s. (topon.) Madeira.
+Madiyre-boraganğîsî s. (orn.) rândunica furtunii de Madeira (lat., Oceanodroma castro).
+madrigal s. (muz.) madrigal.
+madun adj. subordonat; supus; subjugat.
+mafsal s. (anat.) articulație; încheietură.
+maga pron. spre mine; mie. // ●bo yerníñ hawasî maga yaradî — aerul de aici mi-a priit. ●kerekmiy maga — nu-mi trebuie. ●maga bír hamal kerek — am nevoie de un hamal. ●maga bír kawe akelírsíñíz mí? — vreți să-mi aduceți o cafea?; ●maga itimat etíñíz! — aveți încredere în mine!; ●maga kalsa — dacă ar fi după mine. ●maga karşî — față de mine. ●maga karşî konîşa — vorbeşte împotriva mea. ●maga kóre — după părerea mea. ●maga kóre hakkîñîz bar — după părerea mea aveți dreptate. ●maga yaman bír tesir bîraktî — mi-a făcut o impresie proastă. ●tiyme maga! — nu mă atinge!;
+magara s. peşteră; grotă; cavernă.
+magaralarbílímğísí s. speolog.
+magaralarbílímí s. speologie.
+magaralî adj. cavernos.
+magaza s. 1. magazin. 2. magazie. // ●ózíne kîzmet magazasî — magazin cu autoservire.
+magazağî s. magazioner.
+magazin s. (d. publicații periodice) magazin.
+magdur adj. 1. neîndreptățit; nejustificat. 2. injust; nedrept.
+magdurluk s. 1. neîndreptățire. 2. nedreptate; injustețe.
+magfiret s. 1. iertare; absolvire. 2. indulgență; îndurare; îngăduință; milostivire; clemență.
+magfiretlí adj. 1. iertător; indulgent; îndurător; îngăduitor; milostivit; clement. 2. iertat; scuzat.
+maglúbiyet s. înfrângere. // ●maglúbiyetke ogîramak — a suferi o înfrângere.
+maglúp adj. înfrânt; învins.
+magma s. (geol.) magmă.
+magnat s. magnat.
+magnetik adj. (fiz.) magnetic.
+magnetism s. (fiz.) magnetism.
+magnetow s. (fiz.) magnetou.
+magneziyum s. (chim.) magneziu.
+magnoliya s. (bot.) magnolie (lat., Magnolia).
+magrur adj. mândru; orgolios.
+mağar I. adj. maghiar; ungar; unguresc. II. s. maghiar; ungur.
+mağarğa I. adv. ungureşte. II. s. (limba) maghiară.
+Mağarîstan s. (topon.) Ungaria.
+Mağdiy/Majdiy s. (antrop. m., arab.) “Cel nobil”.
+Mağeddin/Mağettin s. (antrop. m., arab.) “Noblețea credinței”.
+mağera s. aventură; acțiune îndrăzneață.
+mağerağî s., adj. aventurier; persoană îndrăzneață.
+mağeralî adj. aventuros.
+Mağide/Majide s. (antrop. f., arab.) “Cea nobilă”.
+mağit adj. nobil; mărinimos; generos.
+Mağit/Majit, El- s. (relig., arab.) "Cel nobil"; “Cel vrednic de cinste; mărinimos şi îngăduitor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+mağîr s. țesală.
+mağîrlama s. țesălare.
+mağîrlamak v.t. a țesăla.
+mağîrlangan adj. țesălat.
+mağîrlanmak v.i. a se țesăla.
+mağîrlaw s. țesălare.
+mağîrlî adj. țesălat.
+mağun s. pastă (şi culin.). // ●ğamğî magunî — chit. ●kayise magunî — gem de caise. ●tíş magunî — pastă de dinți. ●tíş magunî tulumî — tub de pastă de dinți.
+mağunlama s. chituire.
+mağunlamak v.t. a chitui.
+mağunlangan adj. chituit.
+mağunlaw s. chituire.
+mah s. (cron., prsn.) lună.
+mahal s. loc; teritoriu; district; circă.
+mahalle s. cartier; zonă.
+mahalliy adj. local. // ●mahalliy ádetler — obiceiuri locale. ●mahalliy idare — administrație locală.
+maharet s. pricepere; iscusință; îndemânare.
+maharetlí adj. priceput; iscusit; îndemânatic.
+maharetsíz adj. nepriceput; neîndemânatic.
+maharetsízlík s. nepricepere.
+Mahdiy s. (antrop. m., arab.) "Cel călăuzit pe calea întemeiată".
+Mahdiye s. (antrop. f., arab.) "Cea călăuzită pe calea întemeiată".
+mahdut adj. 1. delimitat; limitat. 2. restrâns; redus; limitat.
+mahdutlaşkan adj. limitat; restrâns; redus.
+mahdutlaşmak v.i. a se limita; a se restrânge; a se reduce.
+mahdutlaştîrmak v.t. a limita; a restrânge; a reduce.
+mahdutlaştîruw s. limitare; restrângere; reducere.
+mahdutlaştîruwğî adj. limitator; limitativ.
+mahdutluk s. limitare; restrângere; reducere.
+mahfaza s. toc; teacă; husă.
+mahfiy adj. secret; ascuns.
+mahfuz adj. 1. ascuns; dosit. 2. păstrat (bine păzit).
+mahfuzen adv. sub escortă.
+mahğubiyet s. timiditate; sfială.
+mahğup adj. timid; sfios; ruşinos. // ●mahğup bolmak — a se sfii; a se ruşina; a se intimida. ●mahğup etmek — a intimida.
+mahğupluk s. timiditate; sfială; ruşine.
+mahğuz adj. (jur.) sechestrat.
+Mahide s. (antrop. f., arab.) "Cea călăuzită pe calea întemeiată".
+Mahin s. (antrop. m., arab.) "Abolitorul necredinței" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+mahir adj. priceput; iscusit; îndemânatic.
+Mahit s. (antrop. m., arab.) "Cel călăuzit pe calea întemeiată" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+mahiyet s. trăsătură; însuşire; caracteristică.
+mahkeme s. judecătorie; tribunal. // ●mahkemege şakmak — (jur.) a da în judecată. // ●aliy mahkeme — curte supremă. ●mahkeme yazuwğusî — grefier.
+mahkemeğí s. judecător.
+mahkúm I. adj. (fig.) obligat; constrâns. II. s. (jur.) condamnat.
+mahkúmiyet s. (jur.) condamnare.
+mahlás s. pseudonim.
+mahlúk s. ființă; vietate; viețuitoare; creatură; făptură.
+mahlúkat s. ființe; vietăți; viețuitoare; creaturi; făpturi.
+mahlút I. adj. amestecat. II. s. (chim.) amestec.
+Mahmudiye s. (topon.) Mahmudia (jud. Tulcea).
+mahmur adj. mahmur; posac.
+mahmurluk s. mahmureală; buimăceală.
+Mahmut/Mamut s. (antrop. m., arab.) "Preamăritul"; "Cel demn de glorie" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Mahmut/Mamut-Kuyusî s. (topon.) ("Fântâna lui Mahmut") Izvoru Mare (jud. Constanța).
+mahmuz s. (la cizmele călăreților, la unele animale şi păsări) pinten.
+mahmuzlamak v.t. a da pinteni.
+mahmuzlî adj. pintenat.
+mahrama s. (text.) maramă; năframă; voal.
+mahrek s. (astr.) orbită; traiectorie.
+mahrem I. adj. tainic; secret. II. s. confident. // ●mahrem zinasî — incest.
+mahremane adv. în mod confidențial.
+mahremiyet s. 1. secret; taină. 2. confidență.
+mahreş s. 1. ieşire; loc de ieşire. 2. (fiz.) evacuare. 3. (mat.) numitor. 4. (econ.) debuşeu.
+mahrukat s. combustibil.
+mahrum adj. privat; lipsit de …; // ●mahrum bîrakmak — a priva. ●mahrum bolmak — a fi privat.
+mahrumğí adj. privativ.
+mahrumiyet s. privațiune; privare de …; // ●mahrumiyet baylam figelí — (gram.) gerunziu privativ.
+mahrut s. (mat.) con.
+mahrutiy adj. conic.
+mahsul s. recoltă; producție (agricolă). // ●mahsul ğîymak — a recolta.
+mahsuldar adj. productiv.
+mahsulğîyuw s. recoltare.
+mahsullî adj. productiv; fructuos; fertil.
+mahsulsuz adj. neproductiv; steril.
+mahsulsuzluk s. neproductivitate; sterilitate.
+mahsup adj. calculat; socotit. // ●mahsup bolmak — a se calcula; a se socoti. ●mahsup etmek — a calcula; a socoti.
+mahsupluk s. calculare; socotire.
+mahsur adj. încercuit; înconjurat; împresurat.
+mahsurluk adj. încercuire; înconjurare; împresurare.
+mahsús adj. sensibil. // ●mahsús wenamah sús — abia perceptibil.
+mahşer s. (rel.) Armagedon.
+mahşerden adv. pentru totdeauna.
+mahsus I. adj. special; propriu. II. adv. intenționat; dinadins; în cunoştință de cauză.
+mahsusiye s. intenție; dedicație; omagiu.
+mahsustan adv. intenționat; dinadins; în cunoştință de cauză. // mahsustan ayta — o spune dinadins.
+mahun s. (bot.) mahon; acaju (lat., Swietenia mahagoni).
+mahunrengí adj. maro-roşcat; de culoarea mahonului; acaju.
+mahzar s. petiție; memoriu.
+mahzarğî s. petent; reclamant.
+mahzen s. pivniță; beci. // ●mahzen mezar — cavou. ●şarap mahzení — cramă.
+mahzenğí s. pivnicer.
+mahzenkat s. (constr.) subsol.
+mahzun adj. trist; melancolic.
+mahzunluk s. tristețe; melancolie.
+mahzur s. 1. inconvenient; dezavantaj. 2. obstacol; piedică. 3. impediment.
+mahzurlî adj. dezavantajos.
+Majeddin/Majettin s. (antrop. m., arab.) “Noblețea credinței”.
+Majide/Mağide s. (antrop. f., arab.) "Cea nobilă”.
+majit adj. nobil; mărinimos; generos.
+major adj. (muz.) major.
+majuskula s. (literă) majusculă.
+makabil adj. precedent.
+makale s. articol (de ziar). // ●baş makale — (lit.) articol de fond; editorial. ●okîl hakkînda bír makale — un articol despre şcoală.
+makam s. 1. loc; teritoriu. 2. rang; titlu; grad; poziție; post. 3. (muz.) ton; tonalitate; intonație. // ●askeriy makamlar — (mil.) autorități militare. ●ğetkílí makamlar — foruri competente.
+makara s. 1. bobină; mosor; mulinetă. 2. (tehn.) macara.
+makarağî s. macaragiu.
+makas s. 1. foarfece. 2. (la rac) cleşte. 3. macaz. // ●kíşkene makas — forfecuță. ●temír ğolî makasî — (ferov.) macaz. ●tîrnak makasî — forfecuță de unghii.
+makasşî s. acar.
+makat s. 1. şezut; dos; fund. 2. pătură; cuvertură; pled; macat; cergă.
+makaw-dudukuşî, al- s. (orn.) papagal ara macao (lat., Ara macao).
+makber s. mormânt; groapă.
+makbul adj. 1. acceptat; aprobat; admis; agreat. 2. (d. mărfuri) căutat. // ●makbulga geşmek — a fi admis; a fi acceptat.
+Makbule s. (antrop. f., arab.) “Cea agreabilă”.
+makbulluk s. 1. (econ.) căutare; cerere. 2. acceptare; aprobare; admitere.
+makbuz s. chitanță. // ●teslím makbuzî — dispoziție de livrare; aviz de expediție.
+makedún s., adj. macedonean.
+makedúnğe I. adv. în limba macedoneană. II. s. (limba) macedoneană.
+Makedúniye s. (topon.) Macedonia.
+makedúniyelí s., adj. macedonean.
+maketa s. machetă.
+makkaş s. pensetă.
+makkaşlama s. pensare.
+makkaşlamak v.t. a pensa.
+makkaşlangan adj. pensat.
+makkaşlanmak v.i. a se pensa.
+makkaşlaw s. pensare.
+makoron s. (gastron.) macaroane. // ●penerlí makoron — macaroane cu brânză.
+makoron-biydayî s. (bot.) grâu-tare (lat., Triticum durum).
+makoron-kîzîlşasî s. (bot.) grâu-tare (lat., Triticum durum).
+maksat s. 1. scop; țel. 2. dorință; ideal. // ●maksatîna barmak — a-şi atinge scopul. // ●maksatîñ gezmektír — scopul tău este plimbarea.
+maksatîndaturgan adj. înverşunat.
+maksatîndaturuw s. înverşunare.
+maksatkauygun adj. armonios; potrivit; adaptat; compatibil; convenabil; concordant; corespunzător; indicat; oportun; recomandabil.
+maksatkauymama s. incompatibilitate; nepotrivire; contradicție; neconcordanță; discrepanță; discordanță; paradox.
+maksatkauymaykalgan adj. paradoxal.
+maksatkauymaykaluw s. paradox.
+maksatkauymaz adj. incompatibil; nepotrivit; contradictoriu; neconcordant; discrepant; discordant; paradoxal.
+maksatkauyuw s. armonie; potrivire; adaptare; compatibilitet; concordanță; corespondență; oportunitate.
+maksatlî adj. 1. doritor. 2. intenționat; voit.
+maksatsíz adj. 1. nedoritor. 2. fără intenție; vără voie.
+maksatşî s. (fil.) idealist.
+maksatşîlîk s. (fil.) idealism.
+maksimum s., adj. maxim; maximum.
+maksiy adj. maxi.
+maksus I. adj. special; propriu. II. adv. intenționat; dinadins; în cunoştință de cauză.
+maksustan adv. intenționat; dinadins; în cunoştință de cauză. // ●maksustan bakîra — țipă dinadins.
+maksusiye s. intenție; dedicație; omagiu.
+maktagan adj. lăudator; elogiator.
+maktaganday adj. lăudabil.
 `;
