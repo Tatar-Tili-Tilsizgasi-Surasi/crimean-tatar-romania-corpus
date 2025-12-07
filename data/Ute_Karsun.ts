4e3114d8a814dce9133fcd3b1d279341ba2be96b
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-07 11:47:57 +0100

    Enhance Ute Karsun entries with translation details
    Added additional information about Ute Karsun's work and translation.

--- data/Ute_Karsun.ts
+++ data/Ute_Karsun.ts
@@ -1,4 +1,8 @@
 export const uteKarsunEntries: string = `
 # Hayatka bakşîşîm — My Gift to Life
 # Úte Karsun — Ute Carson
+# Úte Karsun'nuñ Hayatka bakşîşîm 
+ğîyîp Kîrîm Tatarğasîna Taner Murat'nîñ terğúmesínde — Му Gift to Life
+bу Ute Carson
+edited and translated into Crimean Tatar bу Taner Murat
 `;
