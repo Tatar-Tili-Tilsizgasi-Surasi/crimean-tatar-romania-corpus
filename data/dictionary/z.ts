e0bca3fd8d002a3332a40ccda8364cda2a48f7a9
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-07 12:25:05 +0100

    Update z.ts

--- data/dictionary/z.ts
+++ data/dictionary/z.ts
@@ -39,4 +39,32 @@ zahiriyleşken adj. 1. exteriorizat. 2. formalizat.
 zahiriyleştírme s. 1. exteriorizare. 2. formalizare. 
 zahiriyleştírmek v.t. 1. a formaliza. 2. a exterioriza. 
 zahirlík I. adj. claritate; evidenŃă. II. s. luminozitate; strălucire.
+zahit I. adj. auster; cumpătat. II. s. ascet; sihastru; pustnic. // ●zahit merkezí — schit. 
+Zahit s. (antrop. m., arab.) "Cel auster" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam). 
+zahitlík s. 1. ascetism; sihăstrie. 2. austeritate; cumpătare. 
+zahmet s. greutate; dificultate; osteneală; trudă. // ●zahmet bermek — a da cuiva de lucru; a da cuiva de furcă. ●zahmet tartmak — a avea de furcă; a trudi. ●zahmetín ódemek — a-i plăti osteneala; a-l răsplăti; a-l retribui. // ●zahmet bolmasa! — fiŃi bun!; fiŃi amabil!; ●zahmetke tiymez — nu merită osteneala. 
+zahmetetken adj. binevoitor. 
+zahmetínódegen s., adj. plătitor; angajator. 
+zahmetíódelgen adj. plătit; răsplătit; remunerat; retribuit. 
+zahmetkeş I. adj. muncitoresc. II. s. muncitor; lucrător. 
+zahmetkeşler s., pl. muncitorime. 
+zahmetkeşlík adj. muncitoresc. 
+zahmetlí adj. greu; dificil; anevoios; ostenitor. // ●arîza zahmetlí mí? — avaria e mare?; 
+zahmetódemesí s. plată; răsplată; remuneraŃie; retribuŃie. 
+zahmetsíz adj. uşor; facil; lejer. 
+zahmetsízğe adv. cu uşurinŃă; fără dificultăŃi; cu lejeritate. 
+zahmetsízlík s. uşurinŃă; facilitate; lejeritate. 
+zahmettartkan adj. silitor. 
+zak s. 1. tăiş; lamă; ascuŃiş. 2. lustru; strălucire. 
+zaka s. 1. vizuină; bârlog; culcuş. 2. peşteră; grotă; hrubă. 
+zaklama s. 1. ascuŃire; dare la tocilă. 2. lustruire. 
+zaklamak v.t. 1. a ascuŃi; a da la tocilă. 2. a lustrui. 
+zaklangan adj. 1. ascuŃit; dat la tocilă. 2. lustruit. 
+zaklanmak v.i. 1. a se ascuŃi; a se da la tocilă. 2. a se lustrui. 
+zaklî adj. 1. ascuŃit; dat la tocilă. 2. lustruit; strălucitor. 
+zal s. salon; sală. //●mínderidmanî zalî — (sport) sală de gimnastică. ●temaşa zalî — sală de spectacol; auditoriu. ●toplantî zalî — aulă. ●yakîştîrma salonî —  sală de fitness. 
+zalim s., adj. despot; tiran (şi fig.). 
+zalimlík s. despotism; tiranie (şi fig.). 
+zam s. 1. adaos; supliment. 2. majorare; mărire. 
+zaman s. 1. timp; vreme. 2. timp; perioadă. 3. sezon; anotimp. 4. epocă; eră; ev. // ●uygun bír zaman kollamak — a pândi momentul potrivit. ●zaman aldatmak — a-şi trece din vreme; a-şi petrece timpul mai uşor. ●zaman kaybetmek — a pierde timp; a-şi trece vremea; a-şi omorî vremea. ●zaman kazanmak — a câştiga timp. ●zaman taşlamak — a da răgaz. ●zamanga ayak uydurmak — a Ńine pas cu vremea. ●zamanî geşmek — a-i trece vremea; a se învechi; a îmbătrâni. ●zamanî kelmek — a fi timpul cuiva; a-i sosi momentul. ●zamanîn geşírmek — a-şi petrece vremea. // ●az zaman ewel — cu puŃin timp înainte. ●az/kîska zamanda — în scurt timp. ●bírleşken zaman — (gram.) timp compus. ●boş zaman — vacanŃă. ●eñ kîska zamanda — în cel mai scurt timp. ●eskí zamanlarda — pe vremuri. ●geñíş zaman — (gram.) aorist. ●geşken zaman — (gram.) timpul trecut. ●her zaman — totdeauna; mereu; tot timpul. ●her zamankî — uzual. ●kanama ne zaman başladî? — când a început hemoragia?; ●kaybeteğek zamanîm yok — n-am timp de pierdut. ●keleğek zaman — (gram.) timpul viitor. ●mínawday zamanda — pe asemenea vreme. ●ne kadar zaman? — cât timp?; ●ne zaman? — când?; ●o zamandan ekí sene boldî — de atunci au trecut doi ani. ●o zamanlar — da în acea vreme. ●oynamak zamanî — timp de joacă. ●şaktaş zamanlar tewúkesí — istoria modernă. ●şo zaman nenesín kórdí — atunci şi-a văzut mama. ●şúndúgí zaman — (gram.) timpul prezent. ●tartuw ne zaman? — când va avea loc cântarul?; ●tatil zamanî — vacanŃă. ●uygun zamanda — la momentul oportun. ●yakîn zamanda — într-un viitor apropiat; în scurt timp. ●yakîn zamanga! — pe curând!; ●yuklamak zamanî — oră de culcare; vremea somnului. ●zaman bírímí — unitate de timp. ●zaman man — cu timpul. ●zaman tarlîgî — criză de timp. ●zaman tílímí  — o scurtă perioadă. ●zamanîndan burun — prematur.
 `;
