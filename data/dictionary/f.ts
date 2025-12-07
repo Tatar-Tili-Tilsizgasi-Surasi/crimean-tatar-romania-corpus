341aa1bc9c48df44646957e20297ac22d9ee0bb0
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-07 13:59:27 +0100

    Update f.ts

--- data/dictionary/f.ts
+++ data/dictionary/f.ts
@@ -40,4 +40,153 @@ fahiz s. (anat.) gambă; pulpă.
 fahriy adj. onorific; de onoare. 
 fakat conj. dar; însă. // ●onî bekledím fakat kelmedí — l-am aşteptat dar nu a venit. 
 fakir s. fachir; magician; iluzionist; scamator.
+fakirism s. fachirism; magie; iluzionism; scamatorie. 
+fakirlík s. fachirism; magie; iluzionism; scamatorie. 
+fakîr adj. 1. sărac; pauper. 2. biet; sărman. 
+fakîr-fîkara s. oameni sărmani; oameni săraci. 
+fakîrlîk s. sărăcie; lipsuri. 
+Fakreddin/Fakrettin s. (antrop. m., arab.) “Onoarea credinŃei”. 
+Fakriy s. (antrop. m., arab.) “Cel demn de onoare”. 
+Fakriye s. 1. (antrop. f., arab.) “Cea demnă de onoare”. 2. (topon.) Făclia (jud. ConstanŃa).
+faksimil s. facsimil. 
+faktura s. factură. 
+fakturakesmesí s. facturare. 
+fakturasîkesílgen adj. facturat. 
+fakúlte s. (univ.) facultate. 
+fal s. 1. bun augur; noroc. 2. soartă; destin. 3. ghicit; ghicire; prezicere; prevestire; augur; auspiciu. 
+falan adj., pron. cutare. // ●falan kíşí — cutare ins. 
+falanda pron. la cutare. 
+falandan pron. de la/dinspre cutare. 
+falanday pron. ca şi cutare. 
+falanga pron. spre cutare; lui cutare. 
+falannî pron. pe cutare. 
+falannîñ adj. lui cutare. 
+falaşkan s. ghicitor; prezicător. 
+falğî s. ghicitor; prezicător. 
+falğîlîk s. ghicit; ghicire; prezicere. 
+falkaragan s. ghicitor; prezicător. 
+faltaşî s. ghioc. 
+fanatik adj. fanatic. 
+fanatiklík s. fanatism. 
+fanatism s. fanatism. 
+fanfara s. fanfară. 
+fanila s. flanelă; pulover. // ●atlet fanilasî — maiou; tricou. 
+faniy adj. muritor; trecător; efemer. 
+fantasma s. fantomă. 
+fantastik adj. fantastic. 
+fanteziya s. fantezie; imaginaŃie. 
+far s. (tehn.) far. // ●farlar kózímní aldî — farurile m-au orbit. ●kîska ğarîklî farlar — farurile fază scurtă. 
+faragat adj. liniştit sufleteşte; uşurat; bucuros. 
+faragatlangan adj. liniştit; uşurat; bucuros. 
+faragatlanma s. liniştire; uşurare. 
+faragatlanmak v.i. a se linişti; a se uşura; a se bucura. 
+faragatlanuw s. liniştire; uşurare. 
+faragatlaşkan adj. liniştit; uşurat; bucuros. 
+faragatlaşmak v.i. a se linişti; a se uşura; a se bucura. 
+faragatlaştîrmak v.t. a linişti; a uşura; a bucura. // ●ğúregín faragatlaştîrmak — a-şi uşura inima; a-şi despovăra inima; a-şi răcori inima. 
+faragatlaşuw s. liniştire; uşurare. 
+faragatlî adj. liniştit sufleteşte; uşurat; bucuros. 
+faragatlîk s. linişte sufletească; uşurare; bucurie. 
+faragon s. (ist.) faraon. 
+farat s. (fiz.) farad. 
+faraza adv. presupunând că…; admiŃând că… ; 
+faraziy adj. presupus; ipotetic. 
+faraziye s. supoziŃie; presupunere; ipoteză. 
+farinğita s. (med.) inflamarea faringelui; faringită. 
+fark s. deosebire; diferenŃă; distincŃie. // ●fark bolmak — a se diferenŃia; a se observa; a se remarca; a se deosebi. ●farkîna barmak — a-şi da seama; a se sesiza. ●farkînda bolmak — a-şi da seama; a se sesiza. ●farknî aşmak — a mări diferenŃa. // ●ortak fark — (mat.) progresie aritmetică. 
+farkbolgan adj. observat; remarcat. 
+farketílgen adj. remarcat. 
+farketílmek v.i. a se observa. 
+farketme s. observare; observaŃie. 
+farketmek v.t. a observa; a diferenŃia; a băga de seamă; remarca. 
+farketmez adv. indiferent. 
+farketúw s. observare; observaŃie. 
+farkîbolmagan adj. indistinct. 
+farkînabarîlgan adj. desluşit; distins; sesizat. 
+farkînabaruw s. desluşire; distingere; sesizare. 
+farkîndaboluw s. conştientizare; observare; sesizare. 
+farklî adj. diferit; deosebit; distinct. //●farklî tutmak — a discrimina. 
+farklîtutulgan adj. discriminat. 
+farklîtutuw s. discriminare. 
+fars s., adj. persan; iranian. 
+farslî adj. persan; iranian. 
+farsşa adv. în limba persană. 
+Faruk s. (antrop. m., arab.) "Cel care are putinŃa de a deosebi adevărul de minciună" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+farz s. supoziŃie; presupunere; ipoteză. 
+farzetmek v.t. a presupune; a face supoziŃii. // ●farzetiyík ke — presupunând că… ; 
+farziy adj. presupus; ipotetic. 
+farziye s. supoziŃie; presupunere; ipoteză. 
+Fas s. (topon.) Maroc. 
+fasçikula s. fasciculă. 
+fasçism s. fascism. 
+fasçist I. s. fascism. II. s., adj. fascist. 
+fasçistlík s. fascism. 
+fasçistlíkkekarşî s., adj. antifascist. 
+fasçistyagîndan adj. profascist. 
+fasih adj. (d. vorbire, exprimare) elocvent; cu elocinŃă. 
+Fasih s. (antrop. m., arab.) "Cel care se exprimă cu elocinŃă"; "Cel elocvent" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+fasile s. (biol.) familie. 
+fasit adj. stricat; alterat (şi fig.).
+fasitlík adj. putrezire; alterare. 
+fasîl s. 1. capitol (de carte etc.). 2. (muz.) pauză. 3. (fig.) bârfă; clevetire. 
+fasîla s. 1. pauză; întrerupere. 2. interval; distanŃă. 
+fasîlalî s. 1. întrerupt; suspendat. 2. distanŃat. 
+fasîlasîz adj. neîntrerupt; continuu; fără pauză. 
+fasîlasîzlîk s. continuitate. 
+faslî s., adj. marocan. 
+faş adj. public; divulgat. // ●faş etmek — a face public; a divulga. 
+faşşetme s. divulgare. 
+faşşetmek v.i. a divulga. 
+faşşetúw s. divulgare. 
+fatalism s. fatalism. 
+fatih adj. 1. învingător; atotbiruitor; cuceritor; victorios; triumfător. 2. deschizător al porŃilor; deschizător de drum. 
+Fatih s. (antrop. m., arab.) "Desferecătorul porŃilor"; "Atotbiruitorul” (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Fatma s. (antrop. f., arab.) “ObişnuinŃă”. 
+favorit s. 1. favoriŃi; perciune. 2. (la cursele de cai) favorit. 
+fawuna s. faună. 
+fayansa s. faianŃă. 
+fayda I. adj. utilitar. II. s. folos; avantaj; profit; câştig; utilitate; întrebuinŃare. 
+faydağî s., adj. utilitarist. 
+faydağîlîk s. utilitarism. 
+faydalanmak v.i. a se folosi de; a profita; a utiliza. 
+faydalî adj. folositor; avantajos. 
+faydalîk I. adj. utilitar. II. s. avantaj; folos. 
+faydasîz adj. neprielnic; inutil; nefolositor; neconvenabil. 
+faydasîzlîk s. inutilitate. 
+fayik adj. superior. 
+fayikiyet s. superioritate. 
+fayil s. 1. făptaş; autor. 2. (gram.) subiect. // ●bólme fayilí — coautor. 
+fayiz s. 1. (fin.) dobândă. 2. procent; procentaj; rată. // ●ğinayet fayizí — criminalitate. ●ólím fayizí — mortalitate. 
+fayizğí s. cămătar. 
+fayizğílík s. cămătărie; camătă. 
+fayizlí adj. (fin.) cu dobândă. 
+fayizsíz adj. (fin.) fără dobândă. 
+fayton s. faeton; caleaşcă; trăsură. 
+faza s. (fiz.) fază. // ●úş fazalî — (fiz.) trifazic. 
+fazilet s. 1. virtute; merit. 2. favoare; privilegiu; binefacere. 
+faziletlí adj. meritat; privilegiat. 
+fazîl I. adj. virtuos; merituos. II. s. favoare; privilegiu; binefacere. 
+fazla I. adj. mult; mai mult; în plus; excedentar. II. s. surplus; excedent; rest. 
+fazlalîk s. exces; surplus; excedent; rest. 
+fazlasî adj. mai mult; în plus; de peste. 
+Fazlulla(h) s. (antrop. m., arab.) "Binefacerea lui Allah" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Fea'nîñ-boraganğîsî s. (orn.) rândunica furtunii lui Fea (lat., Pterodroma feae). 
+feda s. sacrificiu; jertfă. // ●feda bolmak — a se sacrifica; a se jertfi. ●feda etmek — a sacrifica; a jertfi. ●ózín feda etmek — a se sacrifica. 
+fedakáar adj. devotat; credincios. 
+fedakáarlîk s. devotament; credinŃă. 
+fedalîk s. jertfire. 
+federal adj. federal. 
+federalğî adj. federativ. 
+federalism s. federalism. 
+federallîk adj. federalism; federaŃie. 
+federasiya s. federaŃie. 
+federatif adj. federativ. 
+feğir s. 1. zori; revărsatul zorilor. 2. auroră; aureolă; halo. // ●şimaliy feğir — auroră boreală. 
+feğiy adj. dramatic; tragic; dezastruos; catastrofal. 
+Fehmiy s. (antrop. m., arab.) “Cel înŃelegător”. 
+Fehmiye s. (antrop. f., arab.) “Cea înŃelegătoare”. 
+fekere I. adj. (anat.) vertebral. II. s. (anat.) vertebră. 
+fekerelí adj. vertebrat. 
+fekerelíler s., pl. vertebrate. 
+fekeresíz adj. nevertebrat.
 `;
