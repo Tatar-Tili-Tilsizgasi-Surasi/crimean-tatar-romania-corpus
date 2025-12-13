ce0e1db34e5db67e89ea3da85657463f497a4bf2
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-13 18:43:18 +0100

    Update o.ts

--- data/dictionary/o.ts
+++ data/dictionary/o.ts
@@ -1,1 +1,33 @@
-export const rawDictionaryText = ``;
+export const rawDictionaryText = `
+o I. adj. acel; acea; acei; acele. II. pron. 1. el; ea. 2. acela; aceea. // ●bar mî sende o ğúrek? — ai curajul acesta?; ●ketúw o ketúw — plecarea de pe urmă. ●kím o? — cine e?; ●ne o? — ce e?; ●o esnada — în acel moment. ●o gene keldí — el a venit din nou. ●o íş bolmay taa — lucrul acela nu este încă gata. ●o kadar árúw — atât de bun. ●o kalem men yaza — el scrie cu creionul. ●o kelmedí taa — el nu a venit încă. ●o kitap meñkí — acea carte este a mea. ●o man arañîz kayday? — ce relație aveți cu el?; ●o okîy — ea citeşte. ●o sáátte — la vremea aceea. ●o sîralarda kelgen edí — cam atunci venise. ●o toradan-tora ayta — ea le spune direct. ●o yerníñ hawasî sert — acolo clima este aspră.
+oanda adv. atunci.
+oba s. 1. grămadă; morman; troian; stivă; movilă; deal. 2. cătun; sătuc. // ●oba toplamak — a stivui.
+obalamak v.t. a îngrămădi; a grupa.
+obalangan adj. îngrămădit; grupat.
+obalanma s. îngrămădire; grupare.
+obalanmak v.i. a se îngrămădi; a se aduna grămadă; a se grupa.
+obalanuw s. îngrămădire; grupare.
+obalaşkan adj. îngrămădit; grupat.
+obalaşma s. îngrămădire; grupare.
+obalaşmak v.i. a se îngrămădi; a se aduna grămadă; a se grupa.
+obalaştîrmak v.t. a îngrămădi; a grupa.
+obalaşuw s. îngrămădire; grupare.
+obalî adj. deluros.
+oba-oba s. grămezi-grămezi.
+obiyektif s. (foto.) obiectiv.
+obír adj. celălalt; cealaltă; ceilalți; celelalte. // ●obír hapta — săptămâna cealaltă. ●obír túrlí — altfel; altcum. ●obír yakta — în cealaltă parte. ●obír yaktan da — pe de altă parte. ●sokaknîñ obír başînda — în celălalt capăt al străzii.
+obírgún adv. poimâine; alaltăieri. // ●obírgún pazartesí — poimâine e luni.
+obírgúnden adv. de poimâine; de alaltăieri.
+obírgúnge adv. pentru poimâine; pentru alaltăieri.
+obírgúngeşík adv. până poimâine; până alaltăieri.
+obírí pron. celălalt; cealaltă.
+obírín pron. pe celălalt; pe cealaltă.
+obírínde pron. la celălalt; la cealaltă.
+obírínden pron. de la/dinspre celălalt; de la/dinspre cealaltă.
+obíríndiy pron. ca celălalt; ca cealaltă.
+obíríne pron. spre celălalt; celuilalt; spre cealaltă; celeilalte.
+obíríñ adj. celuilalt; celeilalte.
+obírler(í) pron. ceilalți; celălalte.
+obírlerde pron. la ceilalți; la celălalte.
+obírlerden pron. de la/dinspre ceilalți; de la/dinspre celălalte.
+`;
