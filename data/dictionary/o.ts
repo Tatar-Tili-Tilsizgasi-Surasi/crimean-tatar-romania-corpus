db712f08968e1e3ebe50cde72d431e5d1ca387ed
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-13 18:52:23 +0100

    Update o.ts

--- data/dictionary/o.ts
+++ data/dictionary/o.ts
@@ -30,4 +30,111 @@ obíríñ adj. celuilalt; celeilalte.
 obírler(í) pron. ceilalți; celălalte.
 obírlerde pron. la ceilalți; la celălalte.
 obírlerden pron. de la/dinspre ceilalți; de la/dinspre celălalte.
+obírlerdiy pron. ca ceilalți; ca celălalte.
+obírlerge pron. spre ceilalți; celorlalți; spre celălalte; celorlalte.
+obírlerín pron. pe ceilalți; pe celălalte.
+obírlerínde pron. la ceilalți; la celălalte.
+obírlerínden pron. de la/dinspre ceilalți; de la/dinspre celălalte.
+obírleríndiy pron. ca ceilalți; ca celălalte.
+obírleríne pron. spre ceilalți; celorlalți; spre celălalte; celorlalte.
+obírleríñ adj. celorlalți; celorlalte.
+obírlerní pron. pe ceilalți; pe celălalte.
+obírlerníñ adj. celorlalți; celorlalte.
+obírsí pron. celălalt; cealaltă.
+obírsín pron. pe celălalt; pe cealaltă.
+obírsínde pron. la celălalt; la cealaltă.
+obírsínden pron. de la/dinspre celălalt; de la/dinspre cealaltă.
+obírsíndiy pron. ca celălalt; ca cealaltă.
+obírsíne pron. spre celălalt; celuilalt; spre cealaltă; celeilalte.
+obírsíñ adj. celuilalt; celeilalte.
+obîlgan adj. supt; sorbit; absorbit; aspirat.
+obîlmak v.i. a fi supt; a fi sorbit; a se absorbi; a fi aspirat.
+obîr s., adj. 1. gurmand; mâncăcios. 2. căpcăun. 3. vampir.
+obîrlîk s. voracitate.
+oboy s. (muz.) oboi.
+obrîk I. adj. (fiz.) concav. II. s. (geogr.) pâlnie.
+observator s. (astr.) observator.
+obuw s. supt; sorbire; absorbire.
+obuwğî adj. absorbant.
+obuz s. (mil.) obuz; obuzier.
+oçan s. ocean.
+oda s. 1. cameră; încăpere; odaie; birou. 2. (pol.) cameră; adunare legislativă. // ●avam odasî — (pol.) cameră inferioară. ●bekáar odasî — garsonieră. ●bekleme odasî — anticameră. ●bo oda hem balaban hem de aydînlîk — această cameră este mare şi luminoasă. ●bo odanî tutağak bolaman — vreau să rețin această cameră. ●boş odañîz bar mî? — aveți camere libere?; ●ekí odalî dayire — apartament cu două camere. ●ğatak odasî — dormitor. ●ğuwunma odasî — cameră de baie. ●istihbarat odasî — birou de informații. ●kalem odasî — birou; cancelarie. ●karañgî oda — cameră obscură. ●kewteklí oda — cameră cu baie. ●kíyínme odasî — vestiar. ●kurutma odasî — uscătorie. ●lortlar odasî — (pol.) camera lorzilor. ●mísápír odasî — cameră de oaspeți. ●mobilalî oda — cameră mobilată. ●oda kaşînğî katta? — la ce etaj este camera?; ●oda kîzmetşísí — cameristă. ●oda muzikasî — muzică de cameră. ●odanî begenemen — îmi place camera. ●odanîñ anaktarî — cheia camerei. ●odanîñ ğîlînmasî — încălzirea camerei. ●odanîñ kóşesí — colțul camerei. ●odanîñ úğretí — costul camerei. ●tek ğataklî oda — cameră cu un singur pat. ●telefon/uzakses odasî — cabină telefonică. ●tiğaret odasî — cameră de comerț. ●urba odasî — garderobă. ●yemek odasî — sufragerie; popotă.
+odîn s. 1. lemn. 2. lemne (de foc).
+odînğî s. 1. vânzător de lemne. 2. tăietor de lemne.
+of interj. of!; uf!;
+ofis s. oficiu; birou; serviciu; secție.
+ofîldagan adj. care oftează.
+ofîldamak v.i. a ofta.
+ofîldatmak v.t. a determina să ofteze.
+ofîldaw s. oftat.
+oflagan adj. care oftează.
+oflamak v.i. a ofta.
+oflatmak v.t. a determina să ofteze.
+oflaw s. oftat.
+ofsayit s. (sport) ofsaid.
+ofset s. (tipogr.) ofset.
+oftalmoloğik adj. (med.) oftalmologic.
+oftalmoloğiya I. adj. (med.) oftalmologic. II. s. (med.) oftalmologie.
+oftalmoloğiyağî s. (med.) oftalmolog.
+oftalmoloğiyalî adj. (med.) oftalmologic.
+oftalmolok s. (med.) oftalmolog.
+oga pron. 1. spre el; spre ea; lui; ei. 2. spre acela; spre aceea; lui; ei. // ●kabaátní oga attîlar — au dat vina pe el. ●oga karşî ne hakksîzlîk — ce nedreptate față de el.
+ogaşîk adv. 1. până la el; până la ea. 2. până atunci.
+ogízí, tabîrga- s. (zool.) bou-moscat (lat., Ovibos moschatus).
+ogînatoplangan adj. axat.
+ogînatoplaw s. axare.
+ogînayerleşken adj. axat.
+ogînayerleştírme s. axare.
+ogîr s. noroc; şansă; fericire.
+ogîrak s. loc frecventat.
+ogîralga s. invitație.
+ogîralgağíbermesí s. invitare.
+ogîralgalî adj. poftit; invitat.
+ogîralgasîz adj. nepoftit; neinvitat.
+ogîrama s. abatere.
+ogîramak v.i. 1. a se abate pe la…; a se opri în treacăt la…; a trece pe la…; 2. a nimeri în…; a da în…; a da peste…; // ●belaga ogîramak — a intra într-o belea; a da de necaz; a o păți rău. ●bîzgînga ogîramak — a fi înfrânt. ●bîzîntîga ogîramak — a se zăpăci; a se încurca. ●felaketke ogîramak — a se nenoroci. ●felşke ogîramak — a paraliza. ●inkitaga ogîramak — a se întrerupe; a înceta. ●íşkíge ogîramak — a da în patima beției. ●kazaga ogîramak — a se accidenta. ●kuwuşturmaga ogîramak — a fi urmărit pe cale judiciară. ●maglúbiyetke ogîramak — a suferi o înfrângere. ●oñmazlîkka ogîramak — a se pauperiza. ●papîr kazasîna ogîramak — a naufragia. ●saldîrmaga/taaruzga ogîramak — a fi atacat; a fi agresat. ●súre aşîmîna ogîramak — a se prescrie. // ●gemí limanga ogîrayğak — vasul va face escală în port. ●gene ogîrarmîz — mai trecem.
+ogîraşkanlar s., pl. muncitorii; muncitorime.
+ogîraşma s. 1. străduință; silință; osteneală; efort; trudă. 2. ocupație; îndeletnicire. 3. patimă; suferință; chin. 4. tachinare; sâcâire.
+ogîraşmak A. v.i. 1. a se strădui; a-şi da silința; a-şi da osteneala; a se lupta; a depune efort; a trudi. 2. a se ocupa cu…; a se îndeletnici cu…; 3. a pătimi; a suferi; a îndura. B. v.t. a tachina; a sâcâi; a nu lăsa în pace; a se lua de…; // ●boşî-boşîna ogîraşmak — a călca în piatră seacă; a se osteni zadarnic. ●úy íşlerí/úynepísí men ogîraşmak — a face menajul; a gospodări.
+ogîraştîrgan adj. chinuitor.
+ogîraştîrma s. chinuire; caznă.
+ogîraştîrmak v.t. a chinui; a canoni; a căzni; a sili.
+ogîraştîruw s. chinuire; caznă.
+ogîraşuw s. 1. străduință; silință; osteneală; efort; trudă. 2. ocupație; îndeletnicire. 3. patimă; suferință; chin. 4. tachinare; sâcâire.
+ogîratmak v.t. a determina să se abată pe la…; a face să dea în…; // ●bîzgînga ogîratmak — a stârni panică. ●dermansîzlîkka ogîratmak — a deprima. ●kabîzga ogîratmak — a constipa. ●kazaga ogîratmak — a accidenta. ●memnuniyetsízlíkke ogîratmak — a nemulțumi. ●sektege ogîratmak — a zădărnici. ●zaruriyetke ogîratmak — a deveni grabnic; a deveni urgent.
+ogîraw s. abatere.
+ogîrkazangan s. propăşire; propăşit; prosper; prosperitate.
+ogîrlama s. 1. conducere; însoțire; călăuzire. 2. binecuvântare; felicitare.
+ogîrlamak v.t. 1. a conduce; a însoți; a călăuzi. 2. a binecuvânta; a felicita.
+ogîrlaw s. 1. conducere; însoțire; călăuzire. 2. binecuvântare; felicitare.
+ogîrlawğî s. conducător; însoțitor; călăuzitor.
+ogîrlî adj. norocos; fericit; de bun augur. // ●ogîrlî ğollar! — drum bun!;
+ogîrlîk s. fericire.
+ogîrolla adv. cu bine; în mod prielnic.
+ogîrsíz adj. nenorocos; nefericit; nefast.
+ogîrsízlîk s. nenoroc; ghinion; neşansă; semn rău.
+oglan s. 1. băiat. 2. valet (carte de joc).
+ogrî I. adj. hoțesc. II. s. hoție; tâlhar; bandit; haiduc; pirat.
+ogrîday adv. ca hoții; hoțeşte; hoțiş.
+ogrîğa adv. ca hoții; hoțeşte; hoțiş.
+ogrîlamak v.t. a fura; a şterpeli.
+ogrîlar s., pl. bandă de hoți; hoțime.
+ogrîlîk s. hoție; tâlhărie; banditism; haiducie; piraterie.
+ogrîlîkkaogîragan adj. prădat; tâlhărit.
+oh interj. oh!; vai!; ah!;
+oja s. lac de unghii; ojă.
+ok s. 1. săgeată. 2. (tehn.) ax; osie; arbore. // ●araba ogî — oişte. ●ok ğawunî — ploaie de săgeți. ●ok sadagî — tolbă cu săgeți.
+oka s. şiret; şnur; firet; ceapraz; găitan.
+okadar I. adj. atât; atâta; atâți; atâtea. II. adv. atât; atât de…;
+okadarşîk adj., adv. atâtica.
+okalama s. frecare.
+okalamak v.t. a freca; a masa. // ●kollarîn okalamak — a-şi freca mâinile; a se bucura. ●tírsek okalamak — a-şi roade coatele.
+okalangan adj. frecat; masat.
+okalanmak v.i. a se freca; a se masa. // ●duwarga okalanma! — nu te freca de perete!;
+okalaşmak v.i. a se freca unul de altul; a se masa reciproc.
+okalatmak v.i. a determina să frece; a determina să maseze.
+okalaw s. frecare; masare.
+okatkan s. arcaş.
+okçipital s., adj. (anat.) occipital.
+okçipital súyegí s. (anat.) os occipital.
 `;
