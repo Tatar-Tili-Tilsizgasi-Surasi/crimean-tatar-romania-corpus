dce1fb66508f80cf8ef0648a4ca8b11c241605ef
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-14 21:51:07 +0100

    Update g.ts

--- data/dictionary/g.ts
+++ data/dictionary/g.ts
@@ -162,4 +162,120 @@ gemaneğí s. violonist; scripcar; lăutar.
 gemí s. vas; vapor; navă; corabie. //●alîştîrma/talim gemísí — (nav.) navă şcoală. ●bo gemíde kaş kayîk bar? — câte bărci sunt pe acest vas? ; ●ğelkeñ gemísí — (nav.) corabie cu pânze. ●gemí ğalpa wura — vasul are tangaj. ●gemí inşaatî — construcŃia de nave. ●gemí limanga ogîrayğak — vasul va face escală în port. ●harbiye gemísí — (nav.) navă de război. ●uzay gemísí — navă cosmică. ●zîrhlî gemí — (mil.) crucişător; cuirasat. 
 gemíğí s. (nav.) marinar; navigator. 
 gemíğílík s. (nav.) marină; navigaŃie. // ●kîyuw boyî gemíğílígí — (nav.) cabotaj.
+gemíğílíkkeyaragan adj. (mar.) navigabil.
+gemílík s. şantier naval.
+gene I. adv. 1. iar; iarăşi; din nou. 2. încă; mai. II. conj. totuşi; cu toate acestea; orice s-ar zice. // ●gene de — totuşi; cu toate acestea; orice s-ar zice; pentru orice eventualitate. ●gene konîşa — încă vorbeşte. ●gene ogîrarmîz — mai trecem. ●o gene keldí — el a venit din nou. ●sízní gene bekliymíz — vă aşteptăm din nou.
+genel adj. 1. general; total. 2. public. // ●genel bibliyografiya — bibliografie generală. ●genel hukuk — (jur.) drept comun. ●genel kóríníş — vedere de ansamblu. ●genel sekreter — (pol.) secretar general. ●genel tewúke — istorie generală. ●genel tílbílímí — (lingv.) lingvistică generală. ●genel toplantî — adunare generală. ●genel túşúnğeler — considerații generale.
+genelaffuw s. amnistie generală.
+genelkana s. bordel; casă de toleranță.
+genelkurmay s. (mil.) stat-major.
+genelleme s. generalizare.
+genellemek v.t. a generaliza.
+genelleşken adj. generalizat.
+genelleşme s. generalizare.
+genelleşmek v.i. a se generaliza. // ●liçew úyretímí genelleşmekte — învățământul liceal este în curs de generalizare.
+genelleştírme s. generalizare.
+genelleştírmek v.t. a generaliza.
+genelleştírúw s. generalizare.
+genelleşúw s. generalizare.
+genellík s. generalitate; caracter general. // ●genellík men — în general; în linii mari; grosso modo.
+generastlangan adj. reîntâlnit.
+generastlaw s. reîntâlnire.
+genetuwgan adj. regenerat.
+genetuwuw s. regenerare.
+geñíş adj. 1. larg; amplu; vast; întins. 2. lat. 3. încăpător; spațios; cuprinzător. // ●ğúregí geñíş bolmak — a avea o inimă largă; a fi generos. // ●ğeket terakay geñíş — haina e puțin cam largă. ●geñíş aşî/poşmak/şat — (mat.) unghi obtuz. ●geñíş aşîlî úşpoşmak — (mat.) triunghi obtuz. ●geñíş ekseriyet — majoriate zdrobitoare. ●geñíş geşmíşlík — (gram.) preterit. ●geñíş mañlay — frunte lată. ●geñíş omîzlî — spătos. ●geñíş sozîkawaz/seslí/únlí — (lingv.) vocală deschisă. ●geñíş zaman — (gram.) aorist. ●ğol geñíş — drumul este lat.
+geñíşekranlî adj. panoramic.
+geñíşğayîlgan adj. extins; predominant.
+geñíşğayîlma s. extindere; predominare.
+geñíşğayîluw s. extindere; predominare.
+geñíşğúrek(lí) adj. generos; mărinimos; milostiv; darnic.
+geñíşğúreklík s. dărnicie; generozitate; milostivire; mărinimie.
+geñíşiygen adj. lărgit; mărit; extins; lățit.
+geñíşiyme s. lărgire; mărire; extindere; lățire.
+geñíşiymek v.i. a se lărgi; a se mări; a se extinde; a se lăți.
+geñíşiytmek v.t. a lărgi; a mări; a extinde; a lăți.
+geñíşiyúw s. lărgire; mărire; extindere; lățire.
+geñíşleme s. expansiune.
+geñíşlengen adj. lărgit; mărit; extins; lățit.
+geñíşlenme s. lărgire; mărire; extindere; lățire.
+geñíşlenmek v.i. a se lărgi; a se mări; a se extinde; a se lăți.
+geñíşlenúw s. lărgire; mărire; extindere; lățire.
+geñíşletmek v.t. a lărgi; a mări; a extinde; a lăți.
+geñíşlew s. expansiune.
+geñíşliy adv. lățiş; în sensul lățimii.
+geñíşlík s. 1. extindere; mărime; amplitudine. 2. lățime; lărgime. 3. (fig.) bunăstare; prosperitate. // ●azamiy geñíşlík — lățime maximă permisă. ●kumaşnîñ geñíşlígí — lățimea stofei. ●kumluklarnîñ geñíşlígí — lățimea plajelor. ●şólníñ geñíşlígí — lățimea câmpului.
+geñíz I. adj. nazal. II. s. (anat.) fosă nazală. // ●geñízden konîşmak — a vorbi pe nas; a fonfăi. // ●geñíz tartîkawazî — (lingv.) consoană nazală.
+geñízden adv. nazal.
+geñízdenkonîşuw s. fonfăială.
+geñízğe adv. nazal.
+geñízleşken adj. nazalizat.
+geñízleşme s. nazalizare.
+geñízleşmek v.i. a se nazaliza.
+geñízleştírmek v.t. a nazaliza.
+geñízleşúw s. nazalizare.
+geñízlí adj. nazal; nazalizat.
+gerdek s. 1. cameră nupțială. 2. ecran; cortină.
+gerdeklí adj. ecranat.
+gergedan s. (zool.) rinocer (lat., Rhinoceros).
+gergedanbóğek s. (ent.) băligar-cu-corn; caraban; nasicorn (lat., Oryctes nasicornis).
+gergedan-tezíkşí s. (ent.) caraban; nasicorn; băligar-cu-corn (lat., Oryctes nasicornis).
+gergef s. (text.) gherghef. // ●gergef íşlemek — a broda la gherghef.
+gerila s. gherilă; război de partizani.
+gerp s. emblemă; stemă; blazon.
+gerpbílímğí adj. eraldic.
+gerpbílímí s. eraldică.
+gerplí adj. emblematic.
+geş interj. (la diferite jocuri de cărți) pas! ;
+geşe postp. (pt. exprimarea orei) şi. // ●onbírní şerík geşe — unsprezece şi un sfert. ●sáát onnî yígírím geşe tren keldí — trenul a sosit la ora zece şi douăzeci.
+geşek s. pasarelă.
+geşen adj. trecut; precedent; premergător. // ●ğenábílerí geşen pazar keldíler — dumnealor au venit duminica trecută. ●geşen hapta ğolga şîktîk — am pornit la drum săptămâna trecută. ●geşen kúnlerde — zilele trecute. ●geşen sefer — rândul trecut. ●geşen sene — anul trecut. ●geşen sene bírínğílíkní aldî — anul trecut a luat premiul întâi. ●geşen zemanege nispetí men — comparativ cu timpul trecut.
+geşende adv. deunăzi; recent; nu demult; acum câteva zile.
+geşenden adv. din trecut.
+geşenek s. coridor; culoar; hol; galerie.
+geşenge adv. pentru trecut.
+geşengekaragan adj. retrospectiv.
+geşengekullanîlgan adj. retroactiv.
+geşengí adj. retro.
+geşenlerde adv. deunăzi; recent; nu demult; acum câteva zile.
+geşer adj. 1. (fin.) curent; în curs. 2. (econ.) căutat; cerut. 3. uzual; obişnuit. 4. valabil; valid. // ●geşer para — monedă în curs.
+geşerlí adj. valabil; valid.
+geşerlígípítken adj. expirat; nul.
+geşerlík s. 1. (fin.) curs. 2. (econ.) căutare; cerere. 3. valabilitate; validitate. // ●bilet geşerlígí — valabilitatea biletului.
+geşerlíkberílgen adj. validat.
+geşerlíkberúw s. validare.
+geşerlíktenşîgaruw s. (jur.) prescripție; invalidare.
+geşerlíktenşîkkan adj. (jur.) prescris; invalidat.
+geşersíz adj. (d. legi, dispoziții) fără valabilitate; ieşit din vigoare; expirat; invalidat; nul.
+geşersízlík s. (jur.) nulitate.
+geşiyatîrganda adv. în trecere; în treacăt.
+geşílealgan adj. accesibil.
+geşílír adj. penetrabil.
+geşílmek v.i. a se trece; a se sări; a se traversa; a se parcurge; a se străbate. // ●aytîp geşílgen — în treacăt spuse. ●múren geşílgen yer — vad.
+geşílmez adj. impenetrabil.
+geşílmiykalîr adj. impenetrabil.
+geşírgen adj. 1. (fiz.) permeabil. 2. (fiz.) bun conducător; conductibil; conductor. 3. (gram.) tranzitiv. // ●geşírgen fiil — (gram.) verb tranzitiv.
+geşírílgen adj. 1. petrecut; trăit. 2. condus; petrecut; însoțit; călăuzit. 3. pus; introdus; băgat. 4. trecut; transmis; pasat. 5. (tehn., lingv.) decalcat; calchiat.
+geşírílme s. transmitere; perpetuare.
+geşírílmek v.i. 1. a se petrece; a fi trăit. 2. a fi pus; a fi introdus; a fi băgat. 3. a se trece; a se transmite; a se pasa. 4. a se conduce; a se petrece.
+geşírílúw s. transmitere; perpetuare.
+geşírímlí adj. (fiz.) permeabil.
+geşírímlílík s. (fiz.) permeabilitate.
+geşírímsíz adj. (fiz.) impermeabil.
+geşírímsízlík s. (fiz.) impermeabilitate.
+geşírme s. 1. punere; introducere; băgare. 2. (tehn., lingv.) calc; decalcare; calchiere. 3. trecere; transmitere; pasare. 4. conducere; petrecere; însoțire; călăuzire. 5. (gram.) tranzitivitate. // ●geşírme káátí — hârtie de calc. ●nezaretten geşírme — recenzie.
+geşírmek v.t. 1. a trece; a transmite; a pasa. 2. a pune; a introduce; a băga. 3. a conduce; a petrece; a însoți; a călăuzi. 4. a petrece; a trăi (o perioadă de timp). 5. (tehn., lingv.) a decalca; a calchia. // ●balalîgîn geşírmek — a copilări. ●baygînlîk geşírmek — a leşina. ●bír taa kózden geşírmek — a reexamina; a revizui; a recapitula. ●defterge geşírmek — a consemna. ●elekten geşírmek — a trece prin ciur; a cerne. ●ğinnet geşírmek — a-şi pierde facultățile mintale; a-şi pierde mințile. ●ğúregínden geşírmek — a-i cădea cu drag; a-i cădea la inimă; a-l îndrăgi. ●hayatka geşírmek — a transpune în viață. ●hesabîna geşírmek — a pune în socoteala cuiva; a-l ține bun de plată; a-i imputa. ●kabaátínden geşírmek — a grația pe cineva; a-l ierta. ●kîlîştan geşírmek — a trece prin foc şi sabie. ●kîşnî geşírmek — a ierna. ●kókírek geşírmek — a suspina; a ofta. ●koldan geşírmek — a lua la mână; a lucra manual; a manipula. ●kolga geşírmek — a pune mâna pe ceva; a-şi însuşi; a pune mâna pe cineva; a-l prinde; a-l înhăța; a-l aresta. ●kózden geşírmek — a cerceta cu privirea; a revizui. ●kúnlerín geşírmek — a-şi petrece zilele; a trăi. ●kúpeştege geşírmek — a transborda. ●muruntak geşírmek — a pune botniță. ●ómírín geşírmek — a-şi duce traiul; a-şi petrece viața; a trăi. ●oyberúwden/sesberúwden geşírmek — a supune la vot. ●siğilge geşírmek — a înregistra; a înscrie. ●súzgeşten geşírmek — a trece prin strecurătoare; a strecura. ●teğrúbeden geşírmek — a practica. ●topnî geşírmek — (sport) a pasa mingea. ●ústúne geşírmek — a trece ceva pe numele cuiva. ●wakîtîn geşírmek — a-şi petrece timpul. ●yazuwnî başka káátke geşírmek — a transcrie. ●zamanîn geşírmek — a-şi petrece vremea. // ●men sízní geşíriyím — să vă arăt eu drumul.
+geşírmeme s. (gram.) intranzitivitate.
+geşírmez adj. (gram.) intranzitiv. // ●geşírmez fiil — (gram.) verb intranzitiv.
+geşírmezlík s. (gram.) intranzitivitate.
+geşírúw s. 1. (fiz.) permeabilitate. punere; introducere; băgare. 2. (fiz.) conductibilitate. (tehn., lingv.) calc; decalcare; calchiere. 3. trecere; transmitere; pasare. 4. conducere; petrecere; însoțire; călăuzire. 5. (gram.) tranzitivitate.
+geşírúwğí s., adj. conductor; transmițător. // ●geşírúwğí tegerşík — (tehn.) roată de transmisie.
+geşíş s. 1. trecere; tranziție. 2. trecere; culoar; pasaj; cale de acces. 3. (geogr.) trecătoare; pas. // ●tar geşíş — defileu.
+geşíşme s. osmoză.
+geşken I. adj. 1. trecut; vechi. 2. (d. timp) precedent; anterior; trecut. 3. (med.) molipsitor; contagios; transmisibil. II. s. trecut. // ●bírewníñ yeríne geşken — succesor. ●dúşman yagîna geşken kaşak — transfug. ●geşken zaman — (gram.) timpul trecut.
+geşkenden adv. din trecut.
+geşkín adj. 1. (d. oameni) trecut; îmbătrânit. 2. (d. plante) ofilit; veştejit; trecut.
+geşme s. 1. trecere; traversare; pasaj. 2. depăşire; penetrație. // ●geşme parmaklîgî — barieră. ●kósteríş tórení — paradă; ceremonie. ●resmiy geşme — defilare; paradă.
+geşmeden adv. fără să treacă. // ●kóp geşmeden — nu mult după aceea.
+geşmegen adj. 1. care nu trece; care nu are trecere. 2. (d. legi, dispoziții) fără valabilitate; ieşit din vigoare; expirat; invalidat; nul. // ●sózí geşmegen — fără trecere; fără influență.
+geşmek A. v.i. 1. a trece pe la; a merge; a circula. 2. a se abate (oprindu-se undeva). 3. (d. mărfuri) a avea trecere; a avea căutare. 4. a fi pomenit; a fi menționat. 5. a se termina; a lua sfârşit. 6. (şcol.) a promova; a trece. 7. (d. timp) a se scurge; a trece. 8. (d. oameni) a îmbătrâni. 9. (d. plante) a se veşteji; a se ofili; a se usca. 10. (d. fructe) a fi prea copt; a fi trecut. 11. a fi considerat; a fi luat drept… ; B. v.t. 1. a trece; a sări (peste ceva). 2. a traversa; a parcurge; a străbate. 3. a întrece; a depăşi. 4. a pătrunde; a răzbate. 5. a omite; a trece cu vederea; a uita. 6. a lăsa; a abandona; a renunța (la…). 7. a molipsi; a contamina. // ●akîlîndan geşmek — a-i trece prin minte; a-i da prin gând. ●aldga geşmek — a avansa; a înainta; a trece în frunte; a o lua înainte; a călăuzi; a conduce. ●añlatmadan geşmemek — a preciza; a specifica. ●atî geşmek — a i se pomeni numele; a fi nominalizat. ●atlap geşmek — a sări peste…; a omite; a escalada. ●başîndan geşmek — a cădea pe capul cuiva; a i se întâmpla; a păți. ●eñ yúksek noktadan geşmek — a culmina. ●ğarmaşîp geşmek — a escalada. ●ğaşînîp geşmek — a se furişa; a se strecura; a se fofila. ●ğeríne geşmek — a trece în locul cuiva; a-l înlocui. ●ğerníñ túbúne geşmek — a-i veni cuiva să intre în pământ. ●ilerí geşmek — a avansa; a înainta; a trece în frunte; a trece în față. ●imtannî geşmemek — a pica examenul. ●íş íşten geşmek — a nu mai fi de nicio treabă; a deveni inutil; a fi prea târziu. ●íşínden geşmek — a avea un presentiment; a presimți. ●íşíne geşmek — a-l pătrunde. ●kelíp geşmek — a trece. ●kertesín geşmek — a depăşi măsura. ●kulagîndan geşmek — a-i trece cuiva pe la ureche. ●kuwup geşmek — a se succeda; a se perinda; a se înşira. ●makbulga geşmek — a fi admis; a fi acceptat. ●modasî geşmek — a se demoda; a nu mai fi la modă. ●ózínden geşmek — a-şi pierde cunoştința; a nu mai şti de sine; a leşina. ●píkírínden geşmek — a-i trece prin minte. ●sózí geşmek — a avea cuvânt greu; a avea trecere; a fi prețuit. ●taaruzga geşmek — a trece la ofensivă. ●teşíp geşmek — a străpunge; a perfora. ●ústúnden geşmek — a trece peste…; a necinsti; a viola. ●uşup geşmek — a trece în zbor. ●zamanî geşmek — a-i trece vremea; a se învechi; a îmbătrâni. // ●aylanîşta aldîma geşeğeklí boldî — a vrut să mă depăşească în curbă. ●bilet geşmiy — biletul nu este valabil. ●bír kaş yîl geştí — au trecut câțiva ani. ●ğaz kurî geştí — vara a fost uscată. ●geşmege iğaze beríñíz! — permiteți-mi să trec! ; ●kópírní geşíñíz! — treceți podul! ; ●korkma, bonlar geşer! — nu-ți fie teamă, acestea vor trece! ; ●sózí geşer — (d. oameni) influent; cu trecere.
 `;
