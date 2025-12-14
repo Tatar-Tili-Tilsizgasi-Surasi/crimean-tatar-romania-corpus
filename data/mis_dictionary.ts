b9fb13271422b3a14fd6664e1490f14c802a6391
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-14 09:20:31 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -6,11 +6,13 @@ Fukuşima s. (topon.) Fukushima, Japan.
 Ğadîlar Bayramî s. Halloween.
 Ğorğiye s. Georgia, USA.
 Kaliforniye s. (topon.) California.
+koktayil s. cocktail.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
-malayiz s., adj. malaez.
+malayiz s., adj. malaez/malaezian.
 Malayiziye s. (topon.) Malaezia.
-malayiziyelí s., adj. malaez.
+malayiziyelí s., adj. malaez/malaezian.
 ok s. (abreviere pentru „okka” = kilogram) kg 
+partiy s. parte; party.
 Şînğa s. (topon.) Xinjiang, China.
 Teksas s. (topon.) Texas.
 Virjiniye s. (topon.) Virginia.
