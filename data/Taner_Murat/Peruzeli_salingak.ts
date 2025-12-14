94718409cee5d71d4cb7076af2756a47fd4b8ac3
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-14 12:11:44 +0100

    Update Peruzeli_salingak.ts

--- data/Taner_Murat/Peruzeli_salingak.ts
+++ data/Taner_Murat/Peruzeli_salingak.ts
@@ -1,2 +1,88 @@
 export const peruzeliSalingakEntries: string = `
+# Perúzelí salînğak Kîrîm Tatar manzúmeler ğîyîntîgî — Leagănul cu peruzea zbornic liric tătar crimean
+# Perúzelí salînğak Kîrîm Tatar manzumeler ğîyîntîgî Taner Murat’nîñ terğúmesínde — zbornic liric tătar crimean EdíŃie bilingvă tradusă în limba română de Taner Murat
+# Perúzelí salînğak Solmasîn súygí
+Solmasîn súygí
+Isa Abduraman (1937)
+Sen mení súydúñ, Men sení súydúm. Sen maga síñdíñ, Men saga síñdím... Kara sen endí, Kîzgînsîñ sen de, Kîzgînman men de... Ot ğana tende. Túşúnemen kúnde Kabaát kímde?... Kabaát sende, Kabaát mende. Şîdamay ğúkke Uşamîz kókke... Yeter, en endí! Mení de endír! Kúlúp kír úyge Solmasîn súygí!... Ekewmíz bírge Ketiyík ókkee. 1985 s.
+—  Leagănul cu peruzea Iubirea să nu se stingă
+Iubirea să nu se stingă M-ai plăcut tu Te-am plăcut eu. Te-ai legat tu M-am legat eu. Iată că acu' Supărată tu Supărat şi eu Ne-ncingem mereu. Mă gândesc într-una La cine-o fi vina? Vina e la tine Vina e la mine. Neducând poveri Ne-avântăm spre cer… De ajuns, coboară Dă-mi şi mie-o scară! Râzi, casa-mi lumină Dragostea să Ńină! Noi doi mână-n mână Mereu împreună. 1985
+# Perúzelí salînğak Ana tílím
+
+Mambet Ablálim (Ablálimov)
+
+Ana tílím Anayîmnîñ tílí, ey, Ana tílím! Anamnîñ sútí men kírdíñ kanîma, Ayneniy aytkansîñ maga múlayim Kundakta ğatkanda, turup yanîmda. Bo tílde hep yaşay eğdatlarîmnîñ Destanî, masalî, şîñî, manesí Bo tíldír kadmiy, azíz kalkîmnîñ Altînga bergísíz bay hazinesí. Dalgalî deñízníñ dalgalarîndan, Daklar, kayalarnîñ yúkseklígínden, Keñ şóller, bakşalar sesínden her an Ahenk algan, tesírlí bolgansîñ sen. ... Bízlerge mirastîr kadímíyetten Emekdar kalkîmnîñ bo tílí, Ğaşlîknî mektepke-ílímge yetken Bo lísan súydúrdí ğan Ana-Ílní! Anayîmnîñ tílí, ey, Ana tílím, Anayîmnîñ sútí men kírdíñ kanîma, Ayneniy aytkansîñ maga mulayim, Şo kúnden síñdíñ sen mením anîma. — Leagănul cu peruzea Limba maternă
+
+Limba maternă Oh, limbă maternă, limba mamei dragi Mi-ai pătruns în sânge prin lapte matern. De pe când cu “nani” mă vegheai duios, Din crudă pruncie cântece discern. În această limbă supravieŃuiesc Străbune legende, poezie, vers – E limba straveche a neamului meu Nobilul tezaur din aur emers. Din grandoarea mării, din zbucium de val, Din creştet de munte, din semeŃe stânci, Din vastele stepe, triluri din grădini, łi-ai tras armonia, unduiri adânci. … Tu eşti moştenirea harnicului neam Sevă din trecutul imemorial Tu ne-nveŃi ştiinŃa din pagini de cărŃi Să ne iubim Ńara, pământul natal. Oh, limbă maternă, limba mamei dragi, Mi-ai pătruns în sânge prin lapte matern. De pe când cu “nani” mă vegheai duios Mi-ai pătruns în suflet, ode îŃi aştern.
+# Perúzelí salînğak Tapsam da anamnî
+
+Çerkez Alí (1925)
+
+Tapsam da anamnî Búrúşúk yanaklar Búrúşúk mañlay Tapsam da men sízní Bír óbíp toysam. Tapsam da tízleríñ Ústúne anay Bír sabiy baladay Başîmnî salsam. Tapsam da múlayim Kózíñe baksam Sabiylík şagîmnî Aşk man añsam Sózíñní seslemiy Ózenge kaşkan Kúnlerím úşún de Helallîk alsam. Tapsam da anamnî Nazlansam bíraz Sîypatsam başîmnî Eríşsem, kúlsem… Bír şamar aşasam (Eh, maga, bo az) Soñra kózyaşîmnî Înğînmay sílsem. — Leagănul cu peruzea De mi-aş găsi măicuŃa
+
+De mi-aş găsi măicuŃa RidaŃi obraji Ridată frunte De v-aş găsi săruturi V-aş da pe săturate. De te-aş găsi, măicuŃă, M-aş aşeza în poală Ca pruncii inocenŃi M-aş legăna pe spate. De te-aş găsi şi-n ochii Duioşi de m-aş uita De era inocenŃei Să-mi amintesc cu drag Şi zilele în care Călcând cuvântul tău Fugeam către pârâu Zâmbind de mi-ai ierta. De mi-aş găsi măicuŃa Să mă alint puŃin S-o fac să mă dezmierde Să mă-îmbufnez, să râd… Să-mi dea la fund o palmă (Aş merita mai mult) Şi-apoi să îmi şterg ochii Fără să fac chip hâd.
+# Perúzelí salînğak Tapsam da anamnî
+
+Ne tapsañ óglígíñ Astînda saklap "Dadîna bak sen” dep Kaptîrgan anam Men seníñ arz etken Aşîñdan fakat Kaptîrîp bol-almadîm Oga bek ğanaman. Ğanaman da, tapsaydîm Anamnî diymen Yok, tabîp bolmayman Ólgenler kaytmay. Bo hayat kanunî – Şare yok, ka-tiyím, Anasîn kaybetken Bír taa tapmay. — Leagănul cu peruzea De mi-aş găsi măicuŃa
+
+- Orice-ai găsi ascunde În gură de pofteşti Mă păcăleai cu vorba Şi îmi dădeai gustări. Eu n-am putut bucate Pe-alese să îŃi dau Nu Ńi-am făcut pe voie De-aceea am mustrări. Am remuşcări şi caut MăicuŃa să-mi găsesc Dar nimeni nu revine De-i mort printre cei vii. Aceasta-i legea firii Nu pot să o prefac Mi-am rătăcit măicuŃa N-o voi mai regăsi.
+# Perúzelí salînğak Ana tílím
+
+Ana tílím
+
+Ismail Alimgeray
+
+Ayneniy aytîlsa, nazlanîp díñledím, Aşîksam: “Mamay” - dep, mugayîp íñledím. Yukusuz keşeler beşígím terbettí, Asîrnîñ gayesí maga tíl úyrettí. Anayîm bo tílde ayneniy aytkan, Ruhumda tesírí, tamgasî kalgan. Ya, ka-típ súymiyím men sení, tílím, Bo tílde konîşkan tuwganman, ílím. Anadan tuwganman kúneş batkanda, "Vatanîm!” - degenmen kîzîl tañ atkanda. Ğîrîmnîñ sesíne dawuşlar koşîldî, Mílletler dostlîgî, serbest Watanda. Şayirler, kedaylar bo tílde ğîrlasa, Ğúregím ğîmşala írígen mayday. Súygen ana tílím ruhumnîñ gîdasî, Síñírímden geşíp kanîma ğayray. Valîska, çardaşka men de oynayman, "Kaytarma” deseler ğúrekten ğoşaman. Şo man pekíşken ellerím we belím, Şo man sezíle barlîgîm mením. 1968 — Leagănul cu peruzea Graiul meu matern
+
+Graiul meu matern Ascultam în răsfăŃ cântecul de “nani”, Îngăimam când pofteam lapte “papa, mami”. În nopŃi lungi de nesomn mă dădea în leagăn, Mi-a dat Ńel, m-anvăŃat graiul fără seamăn. Grai de leagăn în care măicuŃa-mi cânta Mi-a rămas imprimat în inimă, spirit. Cum să nu te iubesc grai matern, nu-i aşa, Când tot neamul vorbeşte de pe slova ta? De la naşterea mea, primul răsărit, De la “Patrie!”, primul cuvânt gângurit, Cântul meu a cules necurmat vibraŃii Pentru neatârnare, pace între naŃii. Când recită poeŃii în iubitul meu grai Inima se înmoaie topindu-se, vai! Grai matern, grai iubit, hrană pentru suflet, Străbătându-mi fibra se răsfiră-n cuget. Dansez ceardaş sau vals şi eu ca oricine, Dar la o “kaytarma” nu mai ştiu de mine, Contopite-s în ea mâinile, spinarea, Eu aşa îmi exprim existenŃa, starea. 1968
+# Perúzelí salînğak Kayda ketemíz?
+
+Şeriyan Aliy (Aliyev) (1937)
+
+Kayda ketemíz?
+
+Diyler “kóp kópírler geşeğekmíz bíz,” Tírenímíz tura yegerlí attay. Úykenler soraylar: “Kayda ketemíz, Ógímízde ne bar taa hayatta?” Suwallar, suwallar ğewapsîz kala, Deñízge atîlgan taşlarday şóge. Tílekler tílene: “Sen asra, Allam!...” Bazîlar tílínde şamata, ófke. Vagonlar ortadan bólíngen takta man Turganda, tawanî başîña tiye Ketemíz ğutup toz we azar man Nefes almasî kúş, kózden yaş úye. Kapîlar kapatîldî, çabaklar wurup, Hálímíz ne yakka, nedír ya niyet? Pek seskendík bíz, gudok ókíríp, Vagon bír sílkíníp kóşken saniye. Karanlîk aranga kapawlî koyday, Hop túşken delíkke telmíre herkez. Ğurt man sawlîklaşmak túşúnğe-oyda, Úlkem, sawlîk man kal! ... Bonday seda, ses!... … — Leagănul cu peruzea Dar unde-am plecat?
+
+Dar unde-am plecat? Vom pleca spre zare poduri mii trecând, Ca un cal ce aşteptă, trenul e garat. Bătrânii întreabă: “Dar unde-am plecat, Unde ne veŃi duce, ce aveŃi de gând?” Întrebări, întrebări fără de ecou Ca piatra în apă se afundă-n hău. “Ajută-ne, Doamne!” se-aud implorări, Unii au pe limbă şoapte, indignări. E-mpărŃit vagonul cu rampe din lemn Le loveşti cu capul nu poŃi să stai demn. Respirăm în pulberi, jalea-i în puseu, Ochii sunt în lacrimi, aerul e greu. Uşile-s închise, li s-a tras zăvor, Oare unde mergem, avem viitor? Când porni vagonul zguduind răstit, Unii suspinară, alŃii-au hohotit. Ca oile-n strungă, în beznă potop, În hurducăială la macaz, la hop ToŃi aveam în minte un cuget comun: - Patria mea dragă, dulce rămas bun!... …
+# Perúzelí salînğak
+
+Şúkriy Appaz (Appazov) (1951)
+
+Dostîma
+
+Dostîma Karlî boran uluganda Kaálpní gamga toldîrîp, O kózleríñ ğumulganda Awur íşten boldîrîp, Karalamak ístegende Namussuzlar atîñnî, Takdir darbe endírgende Bekle eskí dostîñnî! Kuwanşîña kuwanş koşîp Ğurtuñda kúlse kúñeş, Kadelerden şarap taşîp Aşîktîrsa dost-eşní, Sakîn mení kóp bekleme Dostîm, ber-almam vade. Kel-almasam ópkeleme, Sení bírew húrmet etmez Dostîm, menden ziyade. — Leagănul cu peruzea Prietenului meu
+
+Prietenului meu Când vijelia troieni-va urlând În pieptul tău tristeŃi, amar, Când pleoapele îŃi vor cădea După o muncă de calvar, Când vor dori denigratorii Numele tău să-l facă mic, Când soarta crunt te va lovi, Tu să-Ńi aştepŃi vechiul amic! Daca abundă fericirea, soarele râde pe la voi, Iar vinul se revarsă din pahare Prilejuind relaŃii noi, Te rog să nu m-astepŃi prea mult, Prietene, nu ştiu când pot veni. Iar de nu vin, să fii încrezător mereu, Căci nimeni n-are pentru tine deferență, Amice, câtă stimă îŃi port eu.
+# Perúzelí salînğak Adetímde yok egílmek
+
+Adetímde yok egílmek
+
+Idris Asan (Asanin) (1927)
+
+Adetímde yok egílmek, Egílmez bír şînarman men. Afat kopsa baş ústúmde Egílmeden turarman men. Men kalkîma ters zatlar man Ómír boyî barîşmadîm. Ğelge kóre yúz şewúrúp Egílmege alîşmadîm. Inan, şayir egílse, Ğurtnî basar soñsuz ağğî. Ener bútún bír milletníñ Memleketí taktî-tağî. Akîlsîzlîkka boyîn egse Şayir ğoytar gururun. Afat kobar, ğaşîn wurar, Millet ğoytar ogîrîn. Men kalkîmnîñ nefesínden Ateş algan bír oşakman. Takdir otka atsa bíle, ''Milletím'' - dep yanağakman. Kóp sînadîm bo dúnyanî, Sefasîndan kop ğefasî. ''Egílmiyğekmen!'' - dedím kene Hakk kúreşí - ğan sefasî. 12.06.1999 senesí, Bakşasaray — Leagănul cu peruzea Nu-mi stă în fire să plec
+
+Nu-mi stă în fire să plec Nu-mi stă în fire să mă plec Sunt un platan neînduplecat. De s-ar isca urgiile Rămâne-voi neînchinat. Cu cei opuşi neamului meu N-am păciuit în viaŃa mea. Nici în bătaia vântului Să mă întorc nu-i firea mea. Poetul capul de-ar pleca Ar cădea Ńara în amar. S-ar duce naŃia de râpă, Simbolurile în zadar. Prostiei dacă s-ar pleca, Şi-ar pune bardul fala-n foc, Ar fi dezastre, trăsnete, Ar păşi neamu-n nenoroc. Sunt flacără în vâlvătăi Din răsuflare de popor, Aş spune “Neam” de-ar fi să pier Prins în pârjol mistuitor. Am contemplat acest pământ, Vesel câtva, mai mult inept, Şi totuşi “Nu mă voi pleca!", Sublimă-i lupta pentru drept. 12.06.1999, Bakceasaray
+# Perúzelí salînğak
+
+Valeriy Basîr (Basîrov) (1947)
+
+Ómrímízníñ ferasetí...
+
+Ómrímízníñ ferasetí... Belkí, bízím omrímízníñ Ferasetí Súygímní ğîkmaga ğol bermez. Penğereñní aş kírsín yîldîzlarnîñ Saf nurî Kaálbíñní temízlesín. Bír andan soñ tañ atar, Tîlsîmlarîñ sóndírer. Penğeremní keñ aştîm. Barsîn şo yîldîzlarnîñ Saf nurlarî Kaálbímní Temízlesín. — Leagănul cu peruzea RaŃiunea existenŃei noastre…
+
+RaŃiunea existenŃei noastre… Poate că raŃiunea existenŃei noastre va împiedica distrugerea iubirii mele. Deschide fereastra să intre lumina pură a stelelor, să-Ńi limpezească inima. Peste o clipă se vor ivi zorile înăbuşindu-Ńi farmecele. Am deschis ferestrele larg. Să intre lumina pură a stelelor, inima să îmi limpezească.
+# Perúzelí salînğak Tatarlîgîm
+
+Tatarlîgîm
+
+Şewkiy Bektóre (1988-1961)
+
+Tatarlîgîm, tuwgan yerím, Balalîktan súyemen, Olar úşún kóp wakîtlar Ğîrlay-ğana kúyemen. Kayda barsam men kóremen Karíp Tatar şaşîlgan, Óz bagînda koklamaga Yok bír gúlí aşîlgan. Ka-teğeksíñ, óz bagîñdan, Óz tílíñden pek karíp, Lákin kímge aytağaksîñ Sen bolarnî tíl ğarîp. Kattî ğel men atîlganlar Tawga-taşka ya ğarga, Ğartî dúnya mezar bolgan Tatarlîkka-Tatarga. Her mezarnîñ baş uşunda Toktap tóktím kózyaşîm, Herbíríne şîñlarîmdan Yasap tíktím baş taşîn. Kol kóterip duwa ettím Ğúregímden Kudayga, Uzun-kutlî ómír bersín Bariy óksíz anayga! — Leagănul cu peruzea Tătărimea mea
+
+Tătărimea mea În tătărime m-am născut, O îndrăgesc de mic copil, Doar pentru ea necontenit Cântând mă mistuiesc umil. Unde-aş umbla tot întâlnesc Sărmani tătari împrăştiaŃi, Din raiul lor de-aromele De trandafir deposedaŃi. Ce poŃi să faci de elizeu, De limba ta de-Ńi este dor? Oare pe cine să conjuri Jelindu-te stăruitor? AbandonaŃi în vânt tăios, Genuni, deşert, pădure rea, Cosmosul stâmb prinde-n mormânt Tătarii, tătărimea mea. Oprindu-mă am lăcrimat La fiece orfan mormânt Montându-le la căpătâi Un monument din vers şi cânt. Din suflet rugi am înălŃat Spre cer, spre Domnul iertător, Măcar s-aline-acele maici Rămase fără pruncii lor.
+# Perúzelí salînğak Ayt, Şatîrtaw!
+
+Ayt, Şatîrtaw! Ayt, Şatîrtaw, sîrîñnî, kóp mí dertíñ? Bekşísísíñ bo yerde bír kudretníñ. Súyrep şîktîñ kóklerge, ak bulutka, Tanîşsîñ sen bo eskí, óksíz ğurtka. Kóp yaşadîñ, kóp kórdíñ, kóp şektíñ belkí, Sen bír tílsíz tarihsíñ - eñ ewelkí. Neler óttí başîñdan, kóp yaşîñdan, Kaysî ízní sorayîm ak taşîñdan? Agaş kesíp tawuñdan ğurtlar kurgan, Kímdí sende burungî íş buyurgan? Kózyaşîñ mî başîñda agargan tuman, Yazda-kîşta múñsírep, búrkúrep turgan? Başîñ-şáşíñ agargan, ne keder kórdíñ? Kayday kaygî, kay dert men dertleníp ğúrdúñ? Kay bulutlar karartîp ğúregíñ ğaktî? Kay kollar man sílíndí başîñnîñ bahtî? Kím baltalap tawuñnî ğagîp ğîktî? Kurt-bóríñní kím kîstap targa tîktî? Ğúregíñde ot bar mî? Tútúnúñ şîga... Árúwúñ yok mî keñ kókte, ne ğanîñ sîga? Kúntuwuştan kóş etíp tóbeñden ótken, Kólgeñe konîp kóp wakît tînîşlap ketken, Pítsín dertíñ, túkensín, bír nefes al, Mením dertlí kalkîmnîñ bawurunda kal! — Leagănul cu peruzea Hai, spune, Munte Neumblat!
+
+Hai, spune, Munte Neumblat! Hai, spune, Munte Neumblat, ce cazne treci? Tu eşti străjer acestei lumi, tu ne petreci. Te-ai târâit până la cer, până la nori, Cunoşti de mult plaiul rămas fără feciori. Mult ai trăit, multe-ai văzut, ai pătimit, Eşti martor mut de pe când timpul s-a stârnit. Prin ce-ai trecut în lung răstimp, ce chinuri reci, Ce-albite stânci pot să întreb şi ce poteci? Cu lemnul din pădurea ta dulce cămin Cine sunt primii ce-au durat, cine-i străin? E lacrimă pe creasta ta fumul ceŃos Ce vara-iarna este trist şi burniŃos? Uite-ai albit de-a binelea, ai cărunŃit. Câte mâhniri ai suferit, câte-ai păŃit? Ce nori umbresc inima ta într-un suspin, Ce mâini au şters norocul din al tău destin? Cine tăind codrul fălos te-a dezfrunzit? Cine gonind lupii hoinari i-a încolŃit? ÎŃi arde sufletul cumva? Că-Ńi iese fum… Nu ai o dragă-n cerul larg nici pân-acum? Născută-n zori s-a aciuat pe creasta ta La loc umbros se odihnea, se răsfăŃa… Vom dovedi tristeŃea rea, ia un răgaz, Strânge la piept poporul meu plin de necaz!
 `;
