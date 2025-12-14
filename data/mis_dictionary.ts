80373a076a2c4e4833e073c4a1ffda02515c5643
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-14 09:46:51 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -7,6 +7,7 @@ Fukuşima s. (topon.) Fukushima, Japan.
 Ğañî Sene s. Anul Nou. // ● Ğañî Sene keşesí — Revelion.
 Ğorğiye s. Georgia, USA.
 Kaliforniye s. (topon.) California.
+Kîş-Babay s. 1. „Tatăl Iernii” (un bărbat care aduce cadouri în ajunul Anului Nou). 2. Moș Crăciun.
 koktayil s. cocktail.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
 malayiz s., adj. malaez/malaezian.
@@ -17,4 +18,5 @@ partiy s. parte; party.
 Şînğa s. (topon.) Xinjiang, China.
 Teksas s. (topon.) Texas.
 Virjiniye s. (topon.) Virginia.
+zombiy s. zombi.
 `;
