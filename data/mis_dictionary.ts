fa7ea6dd99b3a96b8fe2ac53b237d1335460d21b
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2025-12-18 19:44:53 +0100

    Update mis_dictionary.ts

--- data/mis_dictionary.ts
+++ data/mis_dictionary.ts
@@ -2,6 +2,7 @@ export const misDictionaryRawText: string = `
 Awustin s. (topon.) Austin.
 Çikago s. Chicago.
 çm s. (abreviere pentru „çentimetre” = centimetru) cm
+Ferduwsiy s. (antrop. m., prsn.) nume legendar persan.
 Fukuşima s. (topon.) Fukushima, Japan.
 gayîn s. numele literei «غ» din scrierea arabă.
 Ğadîlar Bayramî s. Halloween.
@@ -11,16 +12,22 @@ Kaliforniye s. (topon.) California.
 kaf s. numele literei «ق» din scrierea arabă.
 Kîş-Babay s. 1. „Tatăl Iernii” (un bărbat care aduce cadouri în ajunul Anului Nou). 2. Moș Crăciun.
 koktayil s. cocktail.
+Korasan s. (antrop. m., prsn.) nume legendar persan.
+Korezm s. (antrop. m., prsn.) nume legendar persan.
 Korezmiy, El- s. (antrop. m., prsn.) nume legendar persan. // ● El-Korezmiy ifadesí — (fon.) predictibilitate (termen special folosit în fonetica limbii tătare).
 malayiz s., adj. malaez/malaezian.
 Malayiziye s. (topon.) Malaezia.
 malayiziyelí s., adj. malaez/malaezian.
 ok s. (abreviere pentru „okka” = kilogram) kg 
+pars s., adj. persan; iranian. 
+parslî adj. persan; iranian.
+parsşa adv. în limba persană.
 pisiy s. pisică.
 partiy s. petrecere; party.
 Şînğa s. (topon.) Xinjiang, China.
 şîşkan s. şoarece.
 Teksas s. (topon.) Texas.
 Virjiniye s. (topon.) Virginia.
+zeban s. (lingv., prsn.) limbă; vorbire. //● zeban-î farsiy/parsiy — iz.prsn. limbă persană.
 zombiy s. zombi.
 `;
