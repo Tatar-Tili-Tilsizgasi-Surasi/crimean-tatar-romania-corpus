e13422f6fb9efc0ad016e055cc4f330b0c6fa8ba
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2026-03-17 17:42:26 +0100

    Populate rawDictionaryText with dictionary entries
    Added a comprehensive list of terms and definitions to the raw dictionary text.

--- data/dictionary/h.ts
+++ data/dictionary/h.ts
@@ -1,1 +1,95 @@
-export const rawDictionaryText = ``;
+export const rawDictionaryText = `
+ha interj. ha!; hei!; // ●ha kîz! — fato!; fetiŃo!; copilo!;
+Haat s. (antrop. m., arab.) "Călăuzitorul pe calea întemeiată" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+habaset s. 1. rău; răutate. 2. viciu.
+habasetlí adj. 1. vicios; viciat. 2. rău; răutăcios.
+habbe s. bob; grăunte.
+Habibe s. (antrop. f., arab.) “Cea iubită”.
+Habip s. (antrop. m., arab.) "Cel iubit" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+haç s. (relig.) cruce; crucifix; semnul crucii. // ●haç ğúrúşí — cruciadă.
+haççî s. (ist.) cruciat.
+haçlama s. răstignire; crucificare.
+haçlamak v.t. a răstigni; a crucifica.
+haçlangan adj. răstignit; crucificat.
+haçlaw s. răstignire; crucificare.
+hadde s. laminor.
+haddedengeşken adj. laminat.
+haddedengeşúw s. laminare.
+hadeka s. (anat.) pupilă.
+hademe s. om de serviciu; servitor.
+hadise s. întâmplare; eveniment.
+Hadiy s. (antrop. m., arab.) "Călăuzitorul pe calea întemeiată" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Hadiy, El- s. (relig., arab.) "Călăuzitorul"; “Cel care îi îndrumă pe supuşii Săi pe calea cea dreaptă; bunăstare şi belşug” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+Hadiye s. (antrop. f., arab.) “Călăuzitoarea pe calea înŃelepată”.
+hadîm s. (ist.) eunuc.
+hafakan s. palpitaŃie.
+hafiye s. spion; agent secret; informator; detectiv. // ●polisiya hafiyesi — detectiv.
+hafiyelík s. spionaj; informaŃii.
+hafiyelíkyapkan adj. iscoditor.
+Hafiz s. (antrop. m., arab.) "Depozitarul credinŃei" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Hafiz, El- s. (relig., arab.) "Depozitarul"; “Cel care păstrează lucrurile aşa cum sunt ele în întreg şi în amănunt, apărându-le de urgii şi nenorociri” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+Hafize s. (antrop. f., arab.) “Cea care memorează”.
+hafîz I. adj. prudent; precaut; conştient de pericole. II. s. (la musulmani) păstrătorul credinŃei; cel care ştie pe de rost Kuranul.
+hafîza s. memorie.
+haft num. (prsn.) şapte.
+hafta s. (cron.) săptămână.
+hafta-bahafta adv. din săptămână în săptămână; săptămânal.
+haftada adv. pe săptămână; săptămânal.
+haftadan-haftaga adv. din săptămână în săptămână; săptămânal.
+haftalîk s., adj. săptămânal.
+haftat num. (prsn.) şaptezeci.
+haftdah num. (prsn.) şaptesprezece.
+hağalet s. ruşine; sfială; timiditate. // ●hağalet kelmek — a se ruşina; a se sfii.
+hağaletlí adj. ruşinos; sfios; timid.
+hağaletsíz adj. fără ruşine; neruşinat; obraznic; impertinent; cinic.
+hağaletsízlík s. neruşinare; obrăznicie; impertinenŃă; cinism.
+hağet s. necesitate; nevoie; trebuinŃă.
+hağetlí adj. necesar; trebuincios.
+hağim s. (fiz.) volum.
+hağimlí adj. încăpător; voluminos.
+hağimsíz adj. neîncăpător.
+hağir s. interdicŃie.
+hağiz s. sechestru; poprire.
+hağizlí adj. sechestrat; poprit.
+hağizlík s. sechestrare.
+hağí s. (la musulmani) hagiu; pelerin; pelegrin.
+hağí-akay s. (la musulmani) hagiu; pelerin; pelegrin.
+Hağí-Avat s. (topon., localitate desfiinŃată) Hagi Avat (jud. Tulcea).
+Hağí-Kabul s. (hidron., pârâu lângă Cuza Vodă) Agi Cabul (jud. ConstanŃa).
+hağí-legelek s. (orn.) barză neagră; cocostârc negru (lat., Ciconia nigra).
+Hağíler-Ğolî s. (astr.) calea lactee; drumul robilor.
+hağílík s. pelerinaj; pelegrinaj; hagialâc.
+hağîm s. (fiz.) volum.
+hajdah num. (prsn.) optsprezece.
+hak s. gravare. // ●watandaş hakklarî — (jur.) drepturi civile.
+hakaan s. suveran; monarh.
+hakaanlîk s. monarhie.
+Hakem, El- s. (relig., arab.) "Judecătorul" (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+Hakim s. (antrop. m., arab.) "ÎnŃeleptul" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Hakim, El- s. (relig., arab.) "Cel preaînŃelept" (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+hakk s. 1. dreptate; echitate. 2. drept; privilegiu; merit. 3. (fig.) salariu; leafă; retribuŃie; onorariu. 4. adevăr; realitate; veridicitate. // ●hakk bermek — a-i da cuiva dreptate. ●hakk kazanmak — (jur.) a avea câştig de cauză. ●hakkî bolmak — a avea dreptate; a avea dreptul să…; a fi îndreptăŃit să…; ●hakkîna aşamak — a-şi face păcate cu cineva; a-l nedreptăŃi. ●hakkîn ida etmek — a-şi aroga dreptul de…; a revendica ceva. ●hakkîn tanîmak — a recunoaşte drepturile cuiva; a-i plăti osteneala; a-l retribui. ●hakkîndan kelmek — a-i veni cuiva de hac. // ●hakkî bolîp — de drept. ●hakkîñîz bar — aveŃi dreptate. ●hakkîñîz yok — nu aveŃi dreptate. ●ida etemen ke hakkîm bar — afirm că am dreptate. ●iğare hakkî — rentă. ●kaytarma hakkî — (pol.) drept de veto. ●kíríş hakkî — permis de intrare. ●kîzmet hakkî — salariu; leafă; retribuŃie; plată. ●maga kóre hakkîñîz bar — după părerea mea aveŃi dreptate. ●medeniy hakklar — drepturi civile. ●oy/ses hakkî — drept de vot. ●saylamak/ayîrmak hakkî — dreptul de a alege. ●siyasiy hakklar — drepturi politice. ●sóz hakkî — dreptul la cuvânt. ●takaddúm hakkî — prioritate. ●telif hakkî — drepturi de autor. ●terğih hakkî — drept de opŃiune. ●túm tanîş hakkî — referendum.
+Hakk s. (antrop. m., arab.) "Adevărul credinŃei" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Hakk, El- s. (relig., arab.) "Adevărul"; “Cel a cărui existenŃă rămâne neschimbătoare” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+hakkaret s. insultă; jignire. // ●hakkaret etmek — a insulta; a jigni. ●hakkaret kórmek — a suporta insulte; a fi jignit.
+hakkaretlí adj. insultat; jignit.
+hakkaretşí adj. insultător; jignitor.
+hakkaşamasî s. defavorizare; privare.
+hakkem s. arbitru (şi sport). // ●hakkem kararî man ğendí — a învins prin decizia arbitrului. ●hakkemníñ kararî — (sport) decizia arbitrului. ●kenar hakkemí — (sport) arbitru de linie.
+hakkemlík s. (sport) arbitraj.
+hakketílgen adj. meritat.
+hakketken adj. merituos.
+hakketme s. merit.
+hakketmek v.t. a merita.
+hakketmez adj. nevrednic.
+hakkikiy adj. 1. adevărat; veridic. 2. real; adevărat; veritabil; autentic. // ●hakkikiy arkadaş — prieten adevărat. ●hakkikiy elmaz — diamant veritabil. ●hakkikiy kîymet — (econ.) valoare reală.
+hakkikiyet s. 1. adevăr; veridicitate. 2. realitate; adevăr; valabilitate; autenticitate.
+hakkikiyetíkoplangan adj. autentificat.
+hakkikiyetkoplamasî s. autentificare.
+hakkikiylík s. 1. adevăr; veridicitate. 2. realitate; adevăr; valabilitate; autenticitate.
+hakkim I. adj. dominant; predominant; preponderent. II. s. magistrat; judecător. III. s., adj. înŃelept; preaînŃelept.
+hakkimiyet s. 1. suveranitate; autoritate suverană. 2. dominare; predominare; preponderenŃă.
+hakkimlík s. magistratură.
+hakkîaşalgan adj. 1. defavorizat; nedreptăŃit. 2. lipsit de…; privat.
+hakkîkaytarîlgan adj. plătit; răsplătit; remunerat; retribuit.
+hakkînda postp. despre; în legătură cu…; în privinŃa…; cu privire la…; // ●hakkînda konîşmak — a discuta despre…; a relata despre…; a se referi la…; // ●o kíşí hakkînda túrlí kaberler ğúre — în legătură cu omul acela circulă felurite zvonuri. ●o kitap hakkînda kóp tenkîytlar okîdîm — am citit multe critici cu privire la cartea aceea. ●o kîl hakkînda bír makale — un articol despre şcoală. ●seníñ hakkînda sade árúw sózler bar — în privinŃa ta sunt numai cuvinte bune.
+`;
