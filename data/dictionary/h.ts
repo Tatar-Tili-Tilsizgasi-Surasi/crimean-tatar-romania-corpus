fca807d1e53de437ff53fee0c485b459d70c83fb
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2026-03-17 18:07:10 +0100

    Add new entries to dictionary in h.ts

--- data/dictionary/h.ts
+++ data/dictionary/h.ts
@@ -92,4 +92,232 @@ hakkimlík s. magistratură.
 hakkîaşalgan adj. 1. defavorizat; nedreptăŃit. 2. lipsit de…; privat.
 hakkîkaytarîlgan adj. plătit; răsplătit; remunerat; retribuit.
 hakkînda postp. despre; în legătură cu…; în privinŃa…; cu privire la…; // ●hakkînda konîşmak — a discuta despre…; a relata despre…; a se referi la…; // ●o kíşí hakkînda túrlí kaberler ğúre — în legătură cu omul acela circulă felurite zvonuri. ●o kitap hakkînda kóp tenkîytlar okîdîm — am citit multe critici cu privire la cartea aceea. ●o kîl hakkînda bír makale — un articol despre şcoală. ●seníñ hakkînda sade árúw sózler bar — în privinŃa ta sunt numai cuvinte bune.
+hakkîşîkkan adj. (fin.) dedus.
+hakkîtan îlgan adj. 1. despăgubit; plătit; răsplătit; remunerat; retribuit. 2. (d. un copil născut în afara căsătoriei) recunoscut; legitimat.
+hakkîykat s. 1. adevăr; realitate; veridicitate. 2. fidelitate; devotament. // ●hakkîykatka karşî — împotriva realităŃii.
+hakkîykatîn adv. într-adevăr; în realitate.
+hakkîykatlî adj. fidel; devotat; credincios.
+hakkîykatsîz adj. infidel; lipsit de devotament; necredincios.
+hakkkaytarmasî s. plată; răsplată; remuneraŃie; retribuŃie; salarizare.
+hakklaşkan adj. împăcat.
+hakklaşma s. împăcare.
+hakklaşmagan adj. neîmpăcat.
+hakklaşmak v.i. a se împăca; a se reconcilia; a se pune de acord; a fi chit; a deveni chit (cu cineva). // ●hakklaşmak ístiymen — vreau să mă împac.
+hakklaşmama s. antagonism.
+hakklaşmaykalgan adj. antagonic; discordant; discrepant; ireconciliabil.
+hakklaşmaykalîr adj. ireconciliabil.
+hakklaşmaykalma s. antagonism; dezacord; discordanŃă; discordie; discrepanŃă; neînŃelegere.
+hakklaşmaykaluw s. antagonism; dezacord; discordanŃă; discordie; discrepanŃă; neînŃelegere.
+hakklaşmaz adj. antagonic; ireconciliabil; de neîmpăcat.
+hakklaşmazlîk s. discordie; neînŃelegere; zâzanie.
+hakklaştîrgan adj. conciliant.
+hakklaştîrma s. pacificare.
+hakklaştîrmak v.t. a împăca; a reconcilia.
+hakklaştîruw s. negociere; împăciuire; pacificare.
+hakklaştîruwğî s. împăciuitorist.
+hakklaştîruwğuluk s. împăciuitorism.
+hakklaşuw s. împăcare.
+hakklî adj. 1. drept; echitabil; just; justificat; îndreptăŃit. 2. adevărat; real; veridic. // ●dawada hakklî şîkmak (jur.) — a i se da câştig de cauză. ●ózín hakklî kóstermek — a se arăta îndreptăŃit; a se justifica. // ●hakklî ferit (jur.) — persoană fizică.
+hakklîşîgarma s. (jur.) achitare.
+hakklîşîgaruw s. (jur.) achitare.
+hakklîşîkkan adj. (jur.) achitat.
+hakksîz adj. nedrept; inechitabil; injust.
+hakksîzlîk s. nedreptate; inechitate; injustiŃie. // ●oğa karşî ne hakksîzlîk — ce nedreptate faŃă de el.
+hakkşîgarmasî s. (fin.) decont; deducere.
+hakkşúnas adj. drept; echitabil; just; îndreptăŃit; justificat; imparŃial; nepărtinitor.
+hakkşúnaslîk adj. dreptate; echitate; justeŃe; îndreptăŃire; justificare; imparŃialitate; nepărtinire.
+hakkta postp. despre; în legătură cu…; în privinŃa…; cu privire la…;
+hakktanîma s. dreptate; echitate; justeŃe; îndreptăŃire; justificare; imparŃialitate; nepărtinire.
+hakktanîr adj. drept; echitabil; just; îndreptăŃit; justificat; imparŃial; nepărtinitor.
+haklamak v.t. 1. a grava. 2. a răzui; a şterge.
+hakşî s. gravor.
+hal s. 1. (chim.) dizolvare; topire. 2. rezolvare; realizare; îndeplinire. // ●hal etmek — a soluŃiona; a rezolva; a aranja. // ●herşiy hal bolağak — totul se va rezolva.
+hala s. mătuşă paternală.
+hala s. hală.
+halakîzî s. verişoară primară; fiica mătuşii paternale.
+halat s. funie; frânghie; odgon.
+halaulî s. văr primar; fiul mătuşii paternale.
+halay s. (dim., fam.) mătuşică paternală; tanti.
+halazade s. văr după mătuşa paternală; verişoară după mătuşa paternală.
+halá s. vid; gol.
+halás s. eliberare; punere în libertate. // ●halás etmek — a elibera; a pune în libertate. ●halás tapmak — a se elibera; a scăpa.
+halástapkan adj. eliberat; dezrobit.
+halbolgan adj. rezolvat; dezlegat; tranşat; descurcat.
+halbolmagan adj. indecis; nedecis; nerezolvat.
+halef s. urmaş; succesor.
+halekáat s. 1. comportare; conduită. 2. (mil.) mişcări de trupe; manevre. // ●tabiye halekáatlarî (mil.) — manevre tactice.
+haleket s. 1. (fiz.) dinamică; mişcare. 2. acŃiune; activitate. 3. comportare; conduită; atitudine; gest. 4. pornire; plecare. 5. cutremur; seism. 6. (mil.) operaŃiune; acŃiune. 7. (în transp.) mers; trafic; mişcare; circulaŃie. 8. (med.) şoc; ictus. // ●arzîlanmaz haleketler — gesturi nedorite. ●bîzgîn haleket — comportare imorală. ●el-kol haleketí — gest; gesticulare. ●haleket dayiresí — birou de mişcare. ●haleket kayidelerí — reglementarea circulaŃiei. ●haleket kíşísí — om de acŃiune. ●haleket noktasî — punct de plecare. ●kanunga karşî haleket etken (jur.) — infractor; contravenient. ●kanungazît haleket — acŃiune ilegală. ●namútenahiy haleket — mobil perpetuu. ●sîğak-suwuk haleketí (med.) — şoc termic; termoplegie. ●telege haleketíne kapalî yol — circulaŃie interzisă din ambele sensuri.
+haleketalgan adj. antrenat; angrenat; pus în mişcare; activizat; impulsionat.
+haleketberílgen adj. stimulat.
+haleketberúw s. 1. stimulare; imbold; apel; îndemn. 2. antrenare; angrenare; punere în mişcare; activizare; impuls.
+haleketberúwğí I. adj. 1. stimulant; antrenant. 2. (tehn.) motric; motrice; conducător. II. s. stimulent. // ●haleketberúwğî kuwat — imbold.
+haleketlámbasî s. semafor; stop.
+haleketlendírgen adj. dinamizant.
+haleketlendírme adj. (tehn.) motrice.
+haleketlendírmek v.t. a dinamiza.
+haleketlengen adj. (fiz.) mobil; motric; mişcător.
+haleketlenme s. (fiz.) mobilitate; mişcare.
+haleketlí adj. 1. mobil; în mişcare; activ; vioi. 2. (fiz.) dinamic.
+haleketlík s. dinamism.
+haleketsíz adj. 1. (fiz.) static. 2. imobil; în repaus; inactiv. // ●haleketsíz kalmak — a încremeni. ●haleketsíz otîrmak — a lâncezi.
+haleketsízbîrakma s. Ńinere; imobilizare.
+haleketsízlík s. 1. imobilitate; inactivitate; nemişcare. 2. (fiz.) statică.
+haleketşîrağî s. (auto.) stop.
+halekette adv. mobil; în mişcare; activ. // ●turmadan halekette — mereu în mişcare.
+halel s. stricăciune; deranjament.
+halğî s. dizolvant.
+haliç s. (geogr.) golf.
+halife s. calif.
+halifelík s. califat.
+Halik/Kalik, El- s. (relig., arab.) "Întemeietorul"; “Cel care a creat totul din nimic şi a atribuit destinele" (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+halil s. 1. prieten. 2. iubit.
+Halil/Kalil s. (antrop. m., arab.) "Prietenul sincer" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Halile/Kalile s. (antrop. f., arab.) “Prietena sinceră”.
+halim adj. (d. oameni) blând; blajin; răbdător; calm; domol; liniştit; paşnic; potolit; stăpânit; temperat; tihnit.
+Halim s. (antrop. m., arab.) "Cel blând" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Halim, El- s. (relig., arab.) "Preabunul "; “Cel blând şi îngăduitor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+Halime s. (antrop. f., arab.) “Cea blândă”.
+halis adj. 1. curat; pur; imaculat; neîntinat. 2. (fig.) sincer.
+Halis s. (antrop. m., arab.) "Cel neîntinat" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Halise s. (antrop. f., arab.) "Cea neîntinată".
+halita s. (chim.) aliaj.
+halitalî adj. (d. metale) aliat.
+halî s. covor; covoraş; carpetă. // ●yeşíl halî — covor verde.
+halîğî s. Ńesător.
+Hağîlar s. (topon.) Hagieni (jud. ConstanŃa).
+halîk adj. 1. creator; care crează. 2. bine crescut; educat; manierat.
+halîtabîlgan adj. rezolvat; dezlegat; tranşat; descurcat.
+halka s. 1. verigă; zală. 2. inel. 3. cerc. // ●níşan halkasî — verighetă. ●piston halkasî (tehn.) — segment. ●şáş halkasî — zuluf. ●şînğîr halkasî — verigă.
+halkalî adj. inelat.
+halka-tumşuklî-kagay s. (orn.) pescăruş sur mare (lat., Larus delawarensis).
+halka-tumşuklî-şaklay s. (orn.) pescăruş sur mare (lat., Larus delawarensis).
+hallî adj. 1. rezolvat; realizat; îndeplinit. 2. (chim.) dizolvat; topit.
+halsîz adj. nerezolvat; nerealizat; neîndeplinit.
+halta s. 1. zgardă. 2. (fig.) subjugare; jug. // ●halta takmak — a-i pune zgardă; a-l supune; a-l subjuga.
+haltalamak v.t. a pune zgardă.
+haltalanmak v.i. a i se pune zgardă.
+haltalatmak v.t. a determina să pună zgardă.
+haltera s. (sport) halteră.
+halterağî s. (sport) halterofil.
+halvet s. singurătate; pustietate.
+ham adj. 1. (d. fructe, legume) crud; necopt. 2. (d. materiale) brut; neprelucrat. 3. (fig., d. idei) absurd; ireal. 4. (fig., d. oameni) grosolan; bădăran. ● hammade — materie primă.
+hamak s. hamac.
+hamal s. hamal. ● maga bír hamal kerek — am nevoie de un hamal.
+hamam s. baie publică. ● hamam yapmak — a face baie. ● buw hamamî — baie de aburi. ● kudret hamamî — izvor termal.
+hamamelis s. (bot.) hamamelis (lat., Hamamelis mollis).
+hamamğî s. băieş; proprietar al unei băi publice.
+hamamkana s. baie publică.
+haman ● haman yapmak — a face baie. ● buw hamanî — baie de aburi. ● kudret hamanî — izvor termal.
+hamanğî s. băieş; proprietar al unei băi publice.
+Hamanğî s. (topon.) Baia; Hamangia (jud. Tulcea).
+hamankana s. baie publică.
+hamarat adj. (d. femei) harnică; vrednică; gospodină.
+hamaset s. vitejie; eroism.
+Hamdiy s. (antrop. m., arab.) “Cel ce slăveşte”.
+Hamel s. (astr.) zodia berbecului.
+Hamide s. (antrop. f., arab.) “Cea ce slăveşte”.
+hamil adj. tolerant; îngăduitor.
+hamile adj. (d. femei) însărcinată; gravidă.
+hamilelík s. (med.) sarcină; graviditate.
+hamiş s. post-scriptum.
+Hamit s. (antrop. m., arab.) "Slăvitorul" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Hamit, El- s. (relig., arab.) "Cel preaslăvit"; “Cel ce este singurul vrednic de slava tuturor viețuitoarelor” (unul din cele 99 preafrumoase nume atribuite în Coran lui Dumnezeu/Allah).
+hamiy adj. apărător; protector.
+hamiyet s. patriotism.
+hamiyetlí s., adj. patriot.
+hamlaş s. (fiz.) suflai.
+hamle s. 1. salt (şi fig.). 2. năvală; atac. 3. (la şah) mutare.
+hammade s. materie primă.
+Hammet s. (antrop. m., arab.) "Slăvitorul" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+hamse num. (arab.) cinci.
+hamse-aşar num. (arab.) cincisprezece.
+hamsiy s. (iht.) hamsie (lat., Engraulis encrasicholus).
+hamsîn num. (arab.) cincizeci.
+hamule s. încărcătură; sarcină; povară. ● hamule senetí (transp.) — conosament.
+Hamza s. (antrop. m., arab.) “Leul”.
+Hamzaşî/Amzaşî s. (topon.) Amzacea (jud. Constanța).
+han s. 1. (ist.) han. 2. han; loc de popas.
+hanbiyke s. regină (soția hanului).
+handbal s. handbal. ● handbal meçí (sport) — meci de handbal.
+handbalğî s. handbalist.
+hane s. 1. cameră; casă; gospodărie. 2. (la tablă de şah) careu. 3. (text.) pătrățel; careu. 4. digit; celulă.
+hanedan I. adj. 1. generos; ospitalier. 2. nobil; aristocrat. II. s. dinastie.
+hanedanlîk s. 1. generozitate; ospitalitate. 2. nobilime; noblețe; aristocrație. 3. dinastie.
+hanelí adj. digital; celular.
+hanende s. cântăreț.
+hangar s. (av.) hangar.
+hanğî s. hangiu.
+Hanif s. (antrop. m., arab.) "Credinciosul devotat" (unul din cele 400 de nume atribuite în Coran Profetului Muhammed Aliyselam).
+Hanife s. (antrop. f., arab.) “Credincioasa devotată”.
+hanis adj. neparolist; care nu se ține de cuvânt.
+hanîm s. 1. doamnă. 2. soție. ● doktor hanîm — doamna doctor. ● úyretmen hanîm — doamna învățătoare.
+hanîmkîz s. prințesă; domniță; domnişoară.
+hanîş s. fiica hanului; prințesă.
+hanîşa s. soția hanului; regină.
+hanlîk s. monarhie; regat.
+hanorak s. hanorac.
+hantal adj. 1. (d. obiecte) gros; butucănos; diform. 2. (d. oameni) greoi; încet la treabă.
+hantallîk s. diformitate.
+hap s. 1. pastilă; pilulă; comprimat. 2. bulină. ● hapnî ğutmak — a înghiți hapul; a se păcăli. ● yuklatkan hap — somnifer.
+hapşîr interj. hapciu!
+hapşîrîk s. strănut.
+hapşîrma s. strănut.
+hapşîrmak v.i. a strănuta.
+hapşîruw s. strănut.
+hapta s. (cron.) săptămână. ● ekí hapta arasînda imtanlarnîñ hepísín bergen — şi-a susținut toate examenele în răstimp de două săptămâni. ● geşen hapta ğolga şîktîk — am pornit la drum săptămâna trecută. ● hapta arasînda — în cursul săptămânii. ● hapta soñî tatilí — week-end. ● haptanîñ başî — începutul săptămânii. ● keleğek hapta — săptămâna viitoare. ● obír hapta — săptămâna cealaltă.
+hapta-bahapta adv. din săptămână în săptămână; săptămânal.
+haptada adv. pe săptămână; săptămânal.
+haptadan-haptaga adv. din săptămână în săptămână; săptămânal.
+haptalîk s., adj. săptămânal. ● ekí haptalîk — bilunar. ● haptalîk dergí — revistă săptămânală. ● haptalîk gázáta — ziar săptămânal.
+harabe s. ruină; dărâmătură.
+haraç s. (ist.) haraci; tribut.
+harak s. arac.
+haraklamak v.t. a arăci.
+Haraklar s. (topon., localitate desființată) Araclar (jud. Tulcea).
+haram adj. 1. interzis de religie; nepermis; tabu; spurcat. 2. ilicit; prohibit.
+harameglenúw s. orgie.
+haramiy s. haiduc.
+haramiylík s. haiducie.
+haramlama s. spurcare; pângărire; întinare; murdărire.
+haramlamak v.t. a spurca; a pângări; a întina; a murdări.
+haramlangan adj. spurcat; pângărit; întinat; murdărit.
+haramlanmak v.i. a se spurca; a se pângări; a se întina; a se murdări.
+haramlaw s. spurcare; pângărire; întinare; murdărire.
+haramlîk s. mizerie; porcărie.
+haramzade s. copil nelegitim; bastard.
+hararet s. 1. căldură; fierbințeală. 2. (med.) temperatură. 3. sete; însetare. 4. (fig.) înflăcărare; entuziasm.
+hararetlendírmek v.t. a exalta.
+hararetlengen adj. exaltat.
+hararetlenme s. exaltare.
+hararetlenmek v.i. a se înflăcăra; a se entuziasma; a se exalta.
+hararetlí adj. fierbinte; înflăcărat; entuziasmat; exaltat.
+harbe s. lance; halebardă.
+harbeğí s. lăncier; sulițaş.
+harbetmek v.i. a se război.
+harbiy adj. de război; militar; marțial. ● harbiy deñíz donanmasî (mil.) — flotă militară.
+harbiyaldatuw s. stratagemă.
+harbiye I. adj. de război; milităresc; militar; marțial. II. s. 1. şcoală de război; cătănie. 2. militărie; bătălie; luptă; război. ● azatlanma harbiyesi (pol.) — război de eliberare. ● harbiye belletmesí — caz de război; casus belli. ● harbiye biyanî (pol.) — declarație de război. ● harbiye esírí (mil.) — prizonier de război. ● harbiye gemísí (nav.) — navă de război. ● harbiye kaşkînî (mil.) — dezertor. ● harbiye malúlí — invalid de război. ● istiklal harbiyesi (pol.) — război de independență. ● milliy kutuluş harbiyesi (pol.) — război de eliberare națională.
+harbiyedenewel adj. antebelic.
+harbiyedensoñ adj. postbelic.
+harbiyedenşîgarma s. (mil.) eliberarare; lăsare la vatră; demobilizare.
+harbiyedenşîkkan adj. (mil.) eliberat; lăsat la vatră; demobilizat.
+harbiyedenşîkma s. liberare; lăsare la vatră; demobilizare.
+harbiyegealîngan adj. (mil.) înrolat; încorporat; recrutat; activat.
+harbiyegealuw s. (mil.) înrolare; încorporare; recrutare; activare.
+harbiyegekarşî adj. antirăzboinic.
+harbiyegekaytarîlgan adj. (mil.) reactivat.
+harbiyegekaytaruw s. (mil.) reactivare.
+harbiyeğí s. militar; ostaş; oştean; războinic. ● fidiyelí harbiyeğí (mil.) — mercenar. ● harbiyeğí torbasî (mil.) — raniță. ● taze harbiyeğí (mil.) — recrut. ● tekmílğí harbiyeğí (mil.) — rezervist.
+harbiyeğílík s. militarism.
+harbiyelerarasî adj. interbelic.
+harbiyelí I. adj. belicos. II. s. absolvent al şcolii de război.
+harbiyesíz adj. demilitarizat.
+harbiyesízleşken adj. demilitarizat.
+harbiyesízleştírme s. demilitarizare.
+harbiyesízleştírmek v.t. a demilitariza.
+harbiykandîrma s. stratagemă.
+harbiyleşken adj. militarizat.
+harbiyleştírme s. militarizare.
+harbiyleştírmek v.t. a militariza.
+harb-î mahkeme iz.prsn. (mil.) curte marțială.
 `;
