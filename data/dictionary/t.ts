289aabba93ff55e06af295d730658d6a6d4816f5
Tatar Tílí Tílsîzgasî Şurasî <tatartilintilsizgasurasi@gmail.com>
2026-03-18 21:47:05 +0100

    Expand dictionary entries in t.ts
    Added new entries to the dictionary in t.ts, expanding vocabulary with various terms and definitions.

--- data/dictionary/t.ts
+++ data/dictionary/t.ts
@@ -7,4 +7,117 @@ taadútşí s. (mat.) — factor; înmulțitor; multiplicator.
 taafún s. — putrezire; alterare. // ●taafún bolmak — a putrezi; a se altera.
 taafúnlí adj. — putrezit; alterat.
 taağúp s. — mirare; uimire; nedumerire. // ●taağúp bolmak — a se mira; a fi nedumerit. // ●taağúp etmek — a mira; a uimi; a ului.
+taağúplendírílmek v.i. a se mira; a fi uimit; a fi uluit; a fi nedumerit.
+taağúplendírmek v.t. a determina să se mire; a uimi; a ului.
+taağúplendírúwğí adj. de mirare; uimitor; uluitor.
+taağúplengen adj. mirat; uimit; uluit; nedumerit.
+taağúplenmek v.i. a se mira; a fi uimit; a fi uluit; a fi nedumerit.
+taağúplenúw s. mirare; uimire; uluire; nedumerire.
+taağúplí adj. mirat; uimit; uluit; nedumerit.
+taahhút s. obligaŃie; îndatorire; angajament. // ●taahhút bolmak — a se obliga; a se angaja. // ●taahhút etmek — a obliga; a angaja.
+taahhútlí adj. 1. (d. scrisori etc.) recomandat. 2. obligat; îndatorat; angajat. // ●taahhútlí mektúp — scrisoare recomandată.
+taahhútşí s. angajator; patron; exploatator.
+taahhútşúler s., pl. patronat.
+taaiyí adv. mai bine.
+taallîk s. legătură; raport; relaŃie. // ●taallîk etmek — a se referi la…; a fi în legătură cu…;
+taallúl s. scuză; motivaŃie; justificare; pretext. // ●taallúl bolmak — a se pretexta; a se justifica. // ●taallúl etmek — a pretexta; a justifica.
+taallúllí adj. scuzabil; motivat; justificat.
+taam s. mâncare; aliment. // ●taam etmek — a mânca.
+taammúm s. răspândire; propagare; difuzare. // ●taammúm bolmak — a se răspândi; a se propaga; a se difuza. // ●taammúm etmek — a răspândi; a propaga; a difuza.
+taammúmlí adj. răspândit; propagat; difuzat.
+taammút s. (jur.) premeditare. // ●taammút bolmak — a fi premeditat. // ●taammút etmek — a premedita.
+taammútlí adj. premeditat.
+taamúden adv. (jur.) cu premeditare.
+taammútşe adv. (jur.) cu premeditare.
+taaruz s. ofensivă; atac; agresiune. // ●taaruz bolmak — a fi atacat; a fi agresat. // ●taaruz etmek — a ataca; a agresa. // ●taaruzga geşmek — a trece la ofensivă. // ●taaruzga ogîramak — a fi atacat; a fi agresat.
+taaruzğî s. atentatator; atacant; violator; agresor.
+taaruziy adj. ofensiv; de atac; agresiv.
+taassup s. fanatism; intoleranŃă.
+taasupkáar s., adj. fan; fanatic; intolerant.
+taayiy adv. mai bine.
+taayyúş s. viaŃă; existenŃă; trai. // ●taayyúş etmek — a trăi; a exista.
+taayyúşlí adj. în viaŃă; existent.
+tababet s. medicină.
+tabahat s. (gastron.) artă culinară; gastronomie; bucătărie; gătit.
+tabak s. 1. farfurie. 2. tabac; tutun măcinat. // ●deren tabak — farfurie adâncă. // ●kayîk tabak — platou; tavă. // ●salata tabagî — salatieră. // ●sigara tabagî — scrumieră. // ●tabak takîmî — serviciu de masă. // ●túkúrşúk tabagî — scuipătoare. // ●túz tabak — farfurie întinsă. // ●yemek tabagî — platou.
+tabaka s. 1. strat; film; peliculă. 2. (fig.) categorie socială; pătură socială; strat social; tagmă; castă. 3. tabacheră. // ●úst tabaka — elită.
+tabakalî adj. stratiform; stratificat.
+tabaka-tabaka adj. stratiform; stratificat.
+tabakbórek s. (gastron.) specialitate tătărească de colŃunaşi la tavă.
+tabakkana s. tăbăcărie; argăsitorie (atelier).
+tabaklama s. tăbăcire; argăsire.
+tabaklamak v.t. a tăbăci; a argăsi.
+tabaklangan adj. tăbăcit; argăsit.
+tabaklîk s. raft (pt. farfurii).
+tabaklîk s. tăbăcărie; argăsitorie.
+taban s. 1. (anat.) talpă; laba piciorului. 2. (la încălŃăminte) talpă; pingea. 3. planşeu; podea. 4. (mat.) bază. 5. suport; postament; soclu. 6. (geogr.) platformă. // ●taban almak — a-şi lua tălpăşiŃa. // ●tabanlarîn kótermek — a-şi lua tălpăşiŃa. // ●alîm tabanî — bază de impozitare. // ●ayak tabanî — (anat.) talpă. // ●kalîn tabanlî — cu talpă groasă. // ●kondîra tabanî — pingea.
+taban postp. spre; către. // ●akşamga taban kúneş kîzdîrmay — spre seară soarele nu dogoreşte. // ●ányakka taban — mai încolo.
+tabana s. sulă.
+tabanalgan s., adj. care şi-a luat tălpăşiŃa; evadat.
+tabanaluw s. fugă; goană; scăpare; evadare.
+tabanastî s. (la încălŃăminte) talpă; pingea.
+tabanbasa adv. imediat; îndată; numaidecât; deodată.
+tabanbasma adv. imediat; îndată; numaidecât; deodată.
+tabanbuwunî I. adj. (anat.) tarsian. II. s. (anat.) tars.
+tabanbuwunî súyegí s. (anat.) os tarsian.
+tabanğa s. pistol; revolver.
+tabanîğeñgíl adj. norocos; baftă.
+tabanîğeñgíllík s. noroc; băftos.
+tabanîkara adj. ghinionist; nenorocos.
+tabankóterúw s. fugă; goană; scăpare; evadare.
+tabankuwatî s. (mat.) logaritm. // ●tabankuwatî kóstergesí — (mat.) tabel de logaritmi.
+tabanşîk s. tablă; planşetă.
+taban-tabanga adv. în opoziŃie directă; de neîmpăcat; diametral. // ●taban-tabanga zît — diametral opus.
+tabantaragî I. adj. (anat.) metatarsian. II. s. (anat.) metatars.
+tabantaragî súyegí s. (anat.) os metatarsian.
+tabanteşkerlegen s. urmăritor; cercetaş.
+tabasbus s. linguşire; flatare; măgulire. // ●tabasbus bolmak — a fi linguşit. // ●tabasbus etmek — a se linguşi.
+tabasbuslî adj. flatat; măgulit; linguşit.
+tabasbusşî adj. flatant; măgulitor; linguşitor.
+tabbat s. tipărire; imprimare; editare.
+tabbatlî adj. tipărit; imprimat; editat.
+tabetílgen adj. tipărit; imprimat; editat.
+tabetílmek v.i. a se tipări; a se imprima; a fi editat.
+tabetme s. tipărire; imprimare; editare.
+tabetmek v.t. a tipări; a imprima; a edita.
+tabin num. (mong.) cincizeci.
+tabip s. medic; doctor. // ●adliy tabip — (med.) medic legist.
+tabiy I. adj. natural; normal; obişnuit. II. adv. desigur; fireşte. III. s. tipograf; editor. // ●tabiy koşîllarda — în condiŃii normale. // ●tabiy ólím — moarte naturală. // ●tabiy ómír — viaŃă normală. // ●tabiy, bonday taa árúw bolîr — fireşte, aşa va fi mai bine.
+tabiya s. (mil.) fortificaŃie; val.
+tabiyat s. 1. natură. 2. caracter; caracteristică; însuşire; calitate. 3. fire; natură; temperament. 4. gust; discernământ. 5. (med.) necesităŃi fiziologice. 6. comportament; conduită; purtare; obicei; obişnuinŃă; moravuri; apucături. // ●tabiyatîn deñíştírmek — a denatura. // ●ekewmízníñ tabiyatîmîz bír — amândoi avem acelaşi caracter. // ●ğansîz tabiyat — natură moartă. // ●tabiyat sabîsî bír kíşí — un om cu gust.
+tabiyatalgan adj. adaptat; deprins; domesticit; îmblânzit.
+tabiyatalîr adj. adaptabil.
+tabiyatalma s. adaptabilitate.
+tabiyatayagan adj. ecologic.
+tabiyatayamasîbílímğísí s. ecolog.
+tabiyatayamasîbílímí s. ecologie.
+tabiyatayamasîbílímlí adj. ecologic.
+tabiyatbergen s. antrenor; dresor; îmblânzitor.
+tabiyatbílímí s. ştiinŃe naturale. // ●tabiyat bílímí muzewí — muzeu de ştiinŃe naturale.
+tabiyatîdeñíşken adj. denaturat.
+tabiyatkayidesí s. formalitate; normă de conduită.
+tabiyatkoşîlî s. formalitate; normă de conduită.
+tabiyatkórgen adj. adaptat; deprins; domesticit; îmblânzit.
+tabiyatkórmegen adj. nedeprins; nedresat; neîmblânzit.
+tabiyatlaşkan adj. adaptat; deprins; domesticit; îmblânzit.
+tabiyatlaştîrma s. adaptare; deprindere; domesticire; îmblânzire.
+tabiyatlaştîrmak v.t. a adapta; a deprinde; a domestici; a îmblânzi.
+tabiyatlî adj. cu bun-simŃ; decent; respectuos; cuviincios; manierat.
+tabiyatlîk s. naturaleŃe.
+tabiyatsîz adj. nesimŃit; indecent; nerespectuos; necuviincios; nemanierat; nedeprins.
+tabiyatşa adv. obişnuit; din obişnuinŃă.
+tabiyatşî s. naturalist.
+tabiyatşîlîk s. naturalism.
+tabiyattan adv. obişnuit; din obişnuinŃă.
+tabiyattanîma s. adaptabilitate.
+tabiyattanîmaz s. inadaptabil.
+tabiyattanîr adj. adaptabil.
+tabiyattanústúnlúk s. supranatural; transcendentalism.
+tabiyatústí adj. supranatural; transcendental.
+tabiybolmagan adj. nefiresc.
+tabiye s. (mil.) tactică. // ●tabiye halekáatlarî — (mil.) manevre tactice. // ●tabiye yañğîşî — (mil.) greşeală tactică.
+tabiyeğí s. (mil.) tactician.
+tabiyelí adj. (mil.) cu tactică; tactic.
+tabiyet s. (jur.) cetăŃenie.
+tabiyeuzmanî s. (mil.) tactician.
 `;
